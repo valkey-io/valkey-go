redis/rueidis

feat: send DISCARD command when conn recycled (#580)
* feat: send DISCARD command when conn recycled

* feat: add tests

* fix: add suggestion

Co-authored-by: Rueian <rueiancsie@gmail.com>

* fix: DISCARD should reply a OK message instead of a push message.

---------

Co-authored-by: Anuragkillswitch <70265851+Anuragkillswitch@users.noreply.github.com>
Co-authored-by: Rueian <rueiancsie@gmail.com>

pipe_test.go
@@ -4119,7 +4119,7 @@ func TestPipe_CleanSubscriptions_6(t *testing.T) {
 	go func() {
 		p.CleanSubscriptions()
 	}()
-	mock.Expect("UNSUBSCRIBE").Expect("PUNSUBSCRIBE").Reply(
+	mock.Expect("UNSUBSCRIBE").Expect("PUNSUBSCRIBE").Expect("DISCARD").Reply(
 		RedisMessage{typ: '>', values: []RedisMessage{
 			{typ: '+', string: "unsubscribe"},
 			{typ: '_'},
@@ -4129,7 +4129,9 @@ func TestPipe_CleanSubscriptions_6(t *testing.T) {
 			{typ: '+', string: "punsubscribe"},
 			{typ: '_'},
 			{typ: ':', integer: 2},
-		}})
+		}},
+		RedisMessage{typ: '+', string: "OK"},
+	)
 }
 
 func TestPipe_CleanSubscriptions_7(t *testing.T) {
@@ -4141,7 +4143,7 @@ func TestPipe_CleanSubscriptions_7(t *testing.T) {
 	go func() {
 		p.CleanSubscriptions()
 	}()
-	mock.Expect("UNSUBSCRIBE").Expect("PUNSUBSCRIBE").Expect("SUNSUBSCRIBE").Reply(
+	mock.Expect("UNSUBSCRIBE").Expect("PUNSUBSCRIBE").Expect("SUNSUBSCRIBE").Expect("DISCARD").Reply(
 		RedisMessage{typ: '>', values: []RedisMessage{
 			{typ: '+', string: "unsubscribe"},
 			{typ: '_'},
@@ -4155,8 +4157,10 @@ func TestPipe_CleanSubscriptions_7(t *testing.T) {
 		RedisMessage{typ: '>', values: []RedisMessage{
 			{typ: '+', string: "sunsubscribe"},
 			{typ: '_'},
-			{typ: ':', integer: 2},
-		}})
+			{typ: ':', integer: 3},
+		}},
+		RedisMessage{typ: '+', string: "OK"},
+	)
 }
 
 func TestPingOnConnError(t *testing.T) {

valkey-io/valkey-go

pipe_test.go
@@ -4119,7 +4119,7 @@ func TestPipe_CleanSubscriptions_6(t *testing.T) {
 	go func() {
 		p.CleanSubscriptions()
 	}()
-	mock.Expect("UNSUBSCRIBE").Expect("PUNSUBSCRIBE").Reply(
+	mock.Expect("UNSUBSCRIBE").Expect("PUNSUBSCRIBE").Expect("DISCARD").Reply(
 		ValkeyMessage{typ: '>', values: []ValkeyMessage{
 			{typ: '+', string: "unsubscribe"},
 			{typ: '_'},
@@ -4129,7 +4129,9 @@ func TestPipe_CleanSubscriptions_6(t *testing.T) {
 			{typ: '+', string: "punsubscribe"},
 			{typ: '_'},
 			{typ: ':', integer: 2},
-		}})
+		}},
+		ValkeyMessage{typ: '+', string: "OK"},
+	)
 }
 
 func TestPipe_CleanSubscriptions_7(t *testing.T) {
@@ -4141,7 +4143,7 @@ func TestPipe_CleanSubscriptions_7(t *testing.T) {
 	go func() {
 		p.CleanSubscriptions()
 	}()
-	mock.Expect("UNSUBSCRIBE").Expect("PUNSUBSCRIBE").Expect("SUNSUBSCRIBE").Reply(
+	mock.Expect("UNSUBSCRIBE").Expect("PUNSUBSCRIBE").Expect("SUNSUBSCRIBE").Expect("DISCARD").Reply(
 		ValkeyMessage{typ: '>', values: []ValkeyMessage{
 			{typ: '+', string: "unsubscribe"},
 			{typ: '_'},
@@ -4155,8 +4157,10 @@ func TestPipe_CleanSubscriptions_7(t *testing.T) {
 		ValkeyMessage{typ: '>', values: []ValkeyMessage{
 			{typ: '+', string: "sunsubscribe"},
 			{typ: '_'},
-			{typ: ':', integer: 2},
-		}})
+			{typ: ':', integer: 3},
+		}},
+		ValkeyMessage{typ: '+', string: "OK"},
+	)
 }
 
 func TestPingOnConnError(t *testing.T) {