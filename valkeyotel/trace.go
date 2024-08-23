191301d5094fa0369803b8fd1698d515a0bc8b21
Mikhail Mazurskiy <126021+ash2k@users.noreply.github.com>
2024-08-24 00:25:39 +1000

    OpenTelemetry client optimizations (#614)
    * Use metric.WithAttributeSet()
    
    metric.WithAttributeSet(attribute.NewSet()) is more efficient than metric.WithAttributes() as it doesn't make a copy of the argument slice.
    
    * Allocate vararg slices once, not on each call
    
    See https://go.dev/ref/spec#Passing_arguments_to_..._parameters
    
    * Use field directly in otelclient
    
    * Use field directly in dedicated
    
    Signed-off-by: Rueian <rueiancsie@gmail.com>

--- valkeyotel/trace.go
+++ valkeyotel/trace.go
@@ -69,7 +69,8 @@ type otelclient struct {
 	meter           metric.Meter
 	cscMiss         metric.Int64Counter
 	cscHits         metric.Int64Counter
-	mAttrs          metric.MeasurementOption
+	addOpts         []metric.AddOption
+	recordOpts      []metric.RecordOption
 	tAttrs          trace.SpanStartEventOption
 	histogramOption HistogramOption
 	dbStmtFunc      StatementFunc
@@ -80,7 +81,7 @@ func (o *otelclient) B() valkey.Builder {
 }
 
 func (o *otelclient) Do(ctx context.Context, cmd valkey.Completed) (resp valkey.ValkeyResult) {
-	ctx, span := o.start(ctx, first(cmd.Commands()), sum(cmd.Commands()), o.tAttrs)
+	ctx, span := o.start(ctx, first(cmd.Commands()), sum(cmd.Commands()))
 	if o.dbStmtFunc != nil {
 		span.SetAttributes(dbstmt.String(o.dbStmtFunc(cmd.Commands())))
 	}
@@ -91,14 +92,14 @@ func (o *otelclient) Do(ctx context.Context, cmd valkey.Completed) (resp valkey.
 }
 
 func (o *otelclient) DoMulti(ctx context.Context, multi ...valkey.Completed) (resp []valkey.ValkeyResult) {
-	ctx, span := o.start(ctx, multiFirst(multi), multiSum(multi), o.tAttrs)
+	ctx, span := o.start(ctx, multiFirst(multi), multiSum(multi))
 	resp = o.client.DoMulti(ctx, multi...)
 	o.end(span, firstError(resp))
 	return
 }
 
 func (o *otelclient) DoStream(ctx context.Context, cmd valkey.Completed) (resp valkey.ValkeyResultStream) {
-	ctx, span := o.start(ctx, first(cmd.Commands()), sum(cmd.Commands()), o.tAttrs)
+	ctx, span := o.start(ctx, first(cmd.Commands()), sum(cmd.Commands()))
 	if o.dbStmtFunc != nil {
 		span.SetAttributes(dbstmt.String(o.dbStmtFunc(cmd.Commands())))
 	}
@@ -109,14 +110,14 @@ func (o *otelclient) DoStream(ctx context.Context, cmd valkey.Completed) (resp v
 }
 
 func (o *otelclient) DoMultiStream(ctx context.Context, multi ...valkey.Completed) (resp valkey.MultiValkeyResultStream) {
-	ctx, span := o.start(ctx, multiFirst(multi), multiSum(multi), o.tAttrs)
+	ctx, span := o.start(ctx, multiFirst(multi), multiSum(multi))
 	resp = o.client.DoMultiStream(ctx, multi...)
 	o.end(span, resp.Error())
 	return
 }
 
 func (o *otelclient) DoCache(ctx context.Context, cmd valkey.Cacheable, ttl time.Duration) (resp valkey.ValkeyResult) {
-	ctx, span := o.start(ctx, first(cmd.Commands()), sum(cmd.Commands()), o.tAttrs)
+	ctx, span := o.start(ctx, first(cmd.Commands()), sum(cmd.Commands()))
 	if o.dbStmtFunc != nil {
 		span.SetAttributes(dbstmt.String(o.dbStmtFunc(cmd.Commands())))
 	}
@@ -124,9 +125,9 @@ func (o *otelclient) DoCache(ctx context.Context, cmd valkey.Cacheable, ttl time
 	resp = o.client.DoCache(ctx, cmd, ttl)
 	if resp.NonValkeyError() == nil {
 		if resp.IsCacheHit() {
-			o.cscHits.Add(ctx, 1, o.mAttrs)
+			o.cscHits.Add(ctx, 1, o.addOpts...)
 		} else {
-			o.cscMiss.Add(ctx, 1, o.mAttrs)
+			o.cscMiss.Add(ctx, 1, o.addOpts...)
 		}
 	}
 	o.end(span, resp.Error())
@@ -134,14 +135,14 @@ func (o *otelclient) DoCache(ctx context.Context, cmd valkey.Cacheable, ttl time
 }
 
 func (o *otelclient) DoMultiCache(ctx context.Context, multi ...valkey.CacheableTTL) (resps []valkey.ValkeyResult) {
-	ctx, span := o.start(ctx, multiCacheableFirst(multi), multiCacheableSum(multi), o.tAttrs)
+	ctx, span := o.start(ctx, multiCacheableFirst(multi), multiCacheableSum(multi))
 	resps = o.client.DoMultiCache(ctx, multi...)
 	for _, resp := range resps {
 		if resp.NonValkeyError() == nil {
 			if resp.IsCacheHit() {
-				o.cscHits.Add(ctx, 1, o.mAttrs)
+				o.cscHits.Add(ctx, 1, o.addOpts...)
 			} else {
-				o.cscMiss.Add(ctx, 1, o.mAttrs)
+				o.cscMiss.Add(ctx, 1, o.addOpts...)
 			}
 		}
 	}
@@ -171,7 +172,7 @@ func (o *otelclient) Dedicate() (valkey.DedicatedClient, func()) {
 }
 
 func (o *otelclient) Receive(ctx context.Context, subscribe valkey.Completed, fn func(msg valkey.PubSubMessage)) (err error) {
-	ctx, span := o.start(ctx, first(subscribe.Commands()), sum(subscribe.Commands()), o.tAttrs)
+	ctx, span := o.start(ctx, first(subscribe.Commands()), sum(subscribe.Commands()))
 	if o.dbStmtFunc != nil {
 		span.SetAttributes(dbstmt.String(o.dbStmtFunc(subscribe.Commands())))
 	}
@@ -186,14 +187,15 @@ func (o *otelclient) Nodes() map[string]valkey.Client {
 	for addr, client := range nodes {
 		nodes[addr] = &otelclient{
 			client:          client,
-			mAttrs:          o.mAttrs,
-			tAttrs:          o.tAttrs,
 			meterProvider:   o.meterProvider,
 			tracerProvider:  o.tracerProvider,
 			tracer:          o.tracer,
 			meter:           o.meter,
 			cscMiss:         o.cscMiss,
 			cscHits:         o.cscHits,
+			addOpts:         o.addOpts,
+			recordOpts:      o.recordOpts,
+			tAttrs:          o.tAttrs,
 			histogramOption: o.histogramOption,
 			dbStmtFunc:      o.dbStmtFunc,
 		}
@@ -219,7 +221,7 @@ func (d *dedicated) B() valkey.Builder {
 }
 
 func (d *dedicated) Do(ctx context.Context, cmd valkey.Completed) (resp valkey.ValkeyResult) {
-	ctx, span := d.start(ctx, first(cmd.Commands()), sum(cmd.Commands()), d.tAttrs)
+	ctx, span := d.start(ctx, first(cmd.Commands()), sum(cmd.Commands()))
 	if d.dbStmtFunc != nil {
 		span.SetAttributes(dbstmt.String(d.dbStmtFunc(cmd.Commands())))
 	}
@@ -230,14 +232,14 @@ func (d *dedicated) Do(ctx context.Context, cmd valkey.Completed) (resp valkey.V
 }
 
 func (d *dedicated) DoMulti(ctx context.Context, multi ...valkey.Completed) (resp []valkey.ValkeyResult) {
-	ctx, span := d.start(ctx, multiFirst(multi), multiSum(multi), d.tAttrs)
+	ctx, span := d.start(ctx, multiFirst(multi), multiSum(multi))
 	resp = d.client.DoMulti(ctx, multi...)
 	d.end(span, firstError(resp))
 	return
 }
 
 func (d *dedicated) Receive(ctx context.Context, subscribe valkey.Completed, fn func(msg valkey.PubSubMessage)) (err error) {
-	ctx, span := d.start(ctx, first(subscribe.Commands()), sum(subscribe.Commands()), d.tAttrs)
+	ctx, span := d.start(ctx, first(subscribe.Commands()), sum(subscribe.Commands()))
 	if d.dbStmtFunc != nil {
 		span.SetAttributes(dbstmt.String(d.dbStmtFunc(subscribe.Commands())))
 	}
@@ -340,16 +342,16 @@ func multiCacheableFirst(multi []valkey.CacheableTTL) string {
 	return sb.String()
 }
 
-func (o *otelclient) start(ctx context.Context, op string, size int, attrs trace.SpanStartEventOption) (context.Context, trace.Span) {
-	return startSpan(o.tracer, ctx, op, size, attrs)
+func (o *otelclient) start(ctx context.Context, op string, size int) (context.Context, trace.Span) {
+	return startSpan(o.tracer, ctx, op, size, o.tAttrs)
 }
 
 func (o *otelclient) end(span trace.Span, err error) {
 	endSpan(span, err)
 }
 
-func (d *dedicated) start(ctx context.Context, op string, size int, attrs trace.SpanStartEventOption) (context.Context, trace.Span) {
-	return startSpan(d.tracer, ctx, op, size, attrs)
+func (d *dedicated) start(ctx context.Context, op string, size int) (context.Context, trace.Span) {
+	return startSpan(d.tracer, ctx, op, size, d.tAttrs)
 }
 
 func (d *dedicated) end(span trace.Span, err error) {
