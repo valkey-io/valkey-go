redis/rueidis

Add option to disable making `CLIENT SETINFO` command on initialization (#622)
* Disable client setinfo

Add DisableClientSetInfo path and test

Co-authored-by: Rahul Katre <rkatre@amazon.com>

* Use count instead of len(resp.s)-2

Co-authored-by: Rueian <rueiancsie@gmail.com>

* Add RESP2 pipe test

---------

Co-authored-by: Rahul Katre <rkatre@amazon.com>
Co-authored-by: Rueian <rueiancsie@gmail.com>

pipe_test.go
@@ -491,6 +491,36 @@ func TestNewPipe(t *testing.T) {
 		n1.Close()
 		n2.Close()
 	})
+	t.Run("With DisableClientSetInfo", func(t *testing.T) {
+		n1, n2 := net.Pipe()
+		mock := &redisMock{buf: bufio.NewReader(n2), conn: n2, t: t}
+		go func() {
+			mock.Expect("HELLO", "3").
+				Reply(RedisMessage{
+					typ: '%',
+					values: []RedisMessage{
+						{typ: '+', string: "proto"},
+						{typ: ':', integer: 3},
+					},
+				})
+			mock.Expect("CLIENT", "TRACKING", "ON", "OPTIN").
+				ReplyString("OK")
+		}()
+		p, err := newPipe(func() (net.Conn, error) { return n1, nil }, &ClientOption{
+			ClientSetInfo: DisableClientSetInfo,
+		})
+		go func() {
+			mock.Expect("PING").
+				ReplyString("OK")
+		}()
+		if err != nil {
+			t.Fatalf("pipe setup failed: %v", err)
+		}
+		p.Close()
+		mock.Close()
+		n1.Close()
+		n2.Close()
+	})
 }
 
 func TestNewRESP2Pipe(t *testing.T) {
@@ -768,6 +798,29 @@ func TestNewRESP2Pipe(t *testing.T) {
 		if err != io.ErrClosedPipe {
 			t.Fatalf("pipe setup should failed with io.ErrClosedPipe, but got %v", err)
 		}
+	})
+	t.Run("With DisableClientSetInfo", func(t *testing.T) {
+		n1, n2 := net.Pipe()
+		mock := &redisMock{buf: bufio.NewReader(n2), conn: n2, t: t}
+		go func() {
+			mock.Expect("HELLO", "3").
+				ReplyError("ERR unknown command `HELLO`")
+		}()
+		p, err := newPipe(func() (net.Conn, error) { return n1, nil }, &ClientOption{
+			DisableCache:  true,
+			ClientSetInfo: DisableClientSetInfo,
+		})
+		if err != nil {
+			t.Fatalf("pipe setup failed: %v", err)
+		}
+		if p.version >= 6 {
+			t.Fatalf("unexpected p.version: %v", p.version)
+		}
+		go func() { mock.Expect("PING").ReplyString("OK") }()
+		p.Close()
+		mock.Close()
+		n1.Close()
+		n2.Close()
 	})
 }
 

valkey-io/valkey-go

Signed-off-by: Rueian <rueiancsie@gmail.com>

pipe_test.go
@@ -491,6 +491,36 @@ func TestNewPipe(t *testing.T) {
 		n1.Close()
 		n2.Close()
 	})
+	t.Run("With DisableClientSetInfo", func(t *testing.T) {
+		n1, n2 := net.Pipe()
+		mock := &valkeyMock{buf: bufio.NewReader(n2), conn: n2, t: t}
+		go func() {
+			mock.Expect("HELLO", "3").
+				Reply(ValkeyMessage{
+					typ: '%',
+					values: []ValkeyMessage{
+						{typ: '+', string: "proto"},
+						{typ: ':', integer: 3},
+					},
+				})
+			mock.Expect("CLIENT", "TRACKING", "ON", "OPTIN").
+				ReplyString("OK")
+		}()
+		p, err := newPipe(func() (net.Conn, error) { return n1, nil }, &ClientOption{
+			ClientSetInfo: DisableClientSetInfo,
+		})
+		go func() {
+			mock.Expect("PING").
+				ReplyString("OK")
+		}()
+		if err != nil {
+			t.Fatalf("pipe setup failed: %v", err)
+		}
+		p.Close()
+		mock.Close()
+		n1.Close()
+		n2.Close()
+	})
 }
 
 func TestNewRESP2Pipe(t *testing.T) {
@@ -768,6 +798,29 @@ func TestNewRESP2Pipe(t *testing.T) {
 		if err != io.ErrClosedPipe {
 			t.Fatalf("pipe setup should failed with io.ErrClosedPipe, but got %v", err)
 		}
+	})
+	t.Run("With DisableClientSetInfo", func(t *testing.T) {
+		n1, n2 := net.Pipe()
+		mock := &valkeyMock{buf: bufio.NewReader(n2), conn: n2, t: t}
+		go func() {
+			mock.Expect("HELLO", "3").
+				ReplyError("ERR unknown command `HELLO`")
+		}()
+		p, err := newPipe(func() (net.Conn, error) { return n1, nil }, &ClientOption{
+			DisableCache:  true,
+			ClientSetInfo: DisableClientSetInfo,
+		})
+		if err != nil {
+			t.Fatalf("pipe setup failed: %v", err)
+		}
+		if p.version >= 6 {
+			t.Fatalf("unexpected p.version: %v", p.version)
+		}
+		go func() { mock.Expect("PING").ReplyString("OK") }()
+		p.Close()
+		mock.Close()
+		n1.Close()
+		n2.Close()
 	})
 }
 