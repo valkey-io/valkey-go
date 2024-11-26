redis/rueidis

feat: implement rueidislimiter module for distributed rate limiting (#661)
Signed-off-by: Ernesto Alejandro Santana Hidalgo <ernesto.alejandrosantana@gmail.com>

rueidislimiter/limiter.go
@@ -1,0 +1,153 @@
+package rueidislimiter
+
+import (
+	"context"
+	"errors"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/redis/rueidis"
+)
+
+var (
+	ErrInvalidTokens   = errors.New("number of tokens must be non-negative")
+	ErrInvalidResponse = errors.New("invalid response from Redis")
+)
+
+type Result struct {
+	Allowed   bool
+	Remaining int64
+	ResetAtMs int64
+}
+
+type RateLimiterClient interface {
+	Check(ctx context.Context, identifier string) (Result, error)
+	Allow(ctx context.Context, identifier string) (Result, error)
+	AllowN(ctx context.Context, identifier string, n int64) (Result, error)
+}
+
+const PlaceholderPrefix = "rueidislimiter"
+
+type rateLimiter struct {
+	client    rueidis.Client
+	keyPrefix string
+	limit     int
+	window    time.Duration
+}
+
+type RateLimiterOption struct {
+	ClientBuilder func(option rueidis.ClientOption) (rueidis.Client, error)
+	ClientOption  rueidis.ClientOption
+	KeyPrefix     string
+	Limit         int
+	Window        time.Duration
+}
+
+func NewRateLimiter(option RateLimiterOption) (RateLimiterClient, error) {
+	if option.Window < time.Millisecond {
+		option.Window = time.Millisecond
+	}
+	if option.Limit <= 0 {
+		option.Limit = 1
+	}
+	if option.KeyPrefix == "" {
+		option.KeyPrefix = PlaceholderPrefix
+	}
+
+	rl := &rateLimiter{
+		limit:  option.Limit,
+		window: option.Window,
+	}
+
+	var err error
+	if option.ClientBuilder != nil {
+		rl.client, err = option.ClientBuilder(option.ClientOption)
+	} else {
+		rl.client, err = rueidis.NewClient(option.ClientOption)
+	}
+	if err != nil {
+		return nil, err
+	}
+	rl.keyPrefix = option.KeyPrefix
+	return rl, nil
+}
+
+func (l *rateLimiter) Limit() int {
+	return l.limit
+}
+
+func (l *rateLimiter) Check(ctx context.Context, identifier string) (Result, error) {
+	return l.AllowN(ctx, identifier, 0)
+}
+
+func (l *rateLimiter) Allow(ctx context.Context, identifier string) (Result, error) {
+	return l.AllowN(ctx, identifier, 1)
+}
+
+func (l *rateLimiter) AllowN(ctx context.Context, identifier string, n int64) (Result, error) {
+	if n < 0 {
+		return Result{}, ErrInvalidTokens
+	}
+
+	now := time.Now().UTC()
+	keys := []string{l.getKey(identifier)}
+	args := []string{
+		strconv.FormatInt(n, 10),
+		strconv.FormatInt(now.Add(l.window).UnixMilli(), 10),
+		strconv.FormatInt(now.UnixMilli(), 10),
+	}
+
+	resp := rateLimitScript.Exec(ctx, l.client, keys, args)
+	if err := resp.Error(); err != nil {
+		return Result{}, err
+	}
+
+	data, err := resp.AsIntSlice()
+	if err != nil || len(data) != 2 {
+		return Result{}, ErrInvalidResponse
+	}
+
+	current := data[0]
+	remaining := int64(l.limit) - current
+	if remaining < 0 {
+		remaining = 0
+	}
+
+	allowed := current <= int64(l.limit)
+	if n == 0 {
+		allowed = current < int64(l.limit)
+	}
+
+	return Result{
+		Allowed:   allowed,
+		Remaining: remaining,
+		ResetAtMs: data[1],
+	}, nil
+}
+
+func (l *rateLimiter) getKey(identifier string) string {
+	sb := strings.Builder{}
+	sb.Grow(len(l.keyPrefix) + len(identifier) + 3)
+	sb.WriteString(l.keyPrefix)
+	sb.WriteString(":{")
+	sb.WriteString(identifier)
+	sb.WriteString("}")
+	return sb.String()
+}
+
+var rateLimitScript = rueidis.NewLuaScript(`
+local rate_limit_key = KEYS[1]
+local increment_amount = tonumber(ARGV[1])
+local next_expires_at = tonumber(ARGV[2])
+local current_time = tonumber(ARGV[3])
+local expires_at_key = rate_limit_key .. ":ex"
+local expires_at = tonumber(redis.call("get", expires_at_key))
+if not expires_at or expires_at < current_time then
+  redis.call("set", rate_limit_key, 0, "pxat", next_expires_at + 1000)
+  redis.call("set", expires_at_key, next_expires_at, "pxat", next_expires_at + 1000)
+  expires_at = next_expires_at
+end
+local current = redis.call("incrby", rate_limit_key, increment_amount)
+return { current, expires_at }
+`)

rueidislimiter/limiter_test.go
@@ -1,0 +1,209 @@
+package rueidislimiter_test
+
+import (
+	"context"
+	"encoding/binary"
+	"encoding/hex"
+	"math/rand"
+	"testing"
+	"time"
+	"unsafe"
+
+	"github.com/redis/rueidis"
+	"github.com/redis/rueidis/rueidislimiter"
+)
+
+func setup(t testing.TB) rueidis.Client {
+	client, err := rueidis.NewClient(rueidis.ClientOption{InitAddress: []string{"127.0.0.1:6379"}})
+	if err != nil {
+		t.Fatal(err)
+	}
+	return client
+}
+
+func TestRateLimiter(t *testing.T) {
+	client := setup(t)
+	t.Cleanup(client.Close)
+
+	now := time.Now()
+	window := 100 * time.Millisecond
+	limiter, err := rueidislimiter.NewRateLimiter(rueidislimiter.RateLimiterOption{
+		ClientBuilder: func(option rueidis.ClientOption) (rueidis.Client, error) {
+			return client, nil
+		},
+		Limit:  3,
+		Window: window,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("Check defaults", func(t *testing.T) {
+		limiter, err := rueidislimiter.NewRateLimiter(rueidislimiter.RateLimiterOption{
+			ClientBuilder: func(option rueidis.ClientOption) (rueidis.Client, error) {
+				return client, nil
+			},
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		result, err := limiter.Check(context.Background(), randStr())
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !result.Allowed || result.Remaining != 1 || result.ResetAtMs < now.UnixMilli() {
+			t.Fatalf("Expected Allowed=true, Remaining=1, ResetAt >= now; got Allowed=%v, Remaining=%v, ResetAt=%v", result.Allowed, result.Remaining, result.ResetAtMs)
+		}
+	})
+
+	t.Run("Check allowed within limit", func(t *testing.T) {
+		result, err := limiter.Check(context.Background(), randStr())
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !result.Allowed || result.Remaining != 3 || result.ResetAtMs < now.UnixMilli() {
+			t.Fatalf("Expected Allowed=true, Remaining=3, ResetAt >= now; got Allowed=%v, Remaining=%v, ResetAt=%v", result.Allowed, result.Remaining, result.ResetAtMs)
+		}
+	})
+
+	t.Run("Check denied after exceeding limit", func(t *testing.T) {
+		key := randStr()
+		generateLoad(t, limiter, key, 3)
+
+		result, err := limiter.Check(context.Background(), key)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if result.Allowed || result.Remaining != 0 || result.ResetAtMs < now.UnixMilli() {
+			t.Fatalf("Expected Allowed=false, Remaining=0, ResetAt >= now; got Allowed=%v, Remaining=%v, ResetAt=%v", result.Allowed, result.Remaining, result.ResetAtMs)
+		}
+	})
+
+	t.Run("Check allowed after window reset", func(t *testing.T) {
+		key := randStr()
+		generateLoad(t, limiter, key, 3)
+
+		// Sleep for slightly longer than window duration to ensure reset
+		time.Sleep(window * 2)
+		result, err := limiter.Check(context.Background(), key)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !result.Allowed || result.Remaining != 3 || result.ResetAtMs < now.UnixMilli() {
+			t.Fatalf("Expected Allowed=true, Remaining=3, ResetAt >= now after reset; got Allowed=%v, Remaining=%v, ResetAt=%v", result.Allowed, result.Remaining, result.ResetAtMs)
+		}
+	})
+
+	t.Run("AllowN defaults", func(t *testing.T) {
+		limiter, err := rueidislimiter.NewRateLimiter(rueidislimiter.RateLimiterOption{
+			ClientBuilder: func(option rueidis.ClientOption) (rueidis.Client, error) {
+				return client, nil
+			},
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		result, err := limiter.AllowN(context.Background(), randStr(), 1)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !result.Allowed || result.Remaining != 0 || result.ResetAtMs < now.UnixMilli() {
+			t.Fatalf("Expected Allowed=true, Remaining=0, ResetAt >= now; got Allowed=%v, Remaining=%v, ResetAt=%v", result.Allowed, result.Remaining, result.ResetAtMs)
+		}
+	})
+
+	t.Run("AllowN with tokens within limit", func(t *testing.T) {
+		key := randStr()
+		result, err := limiter.AllowN(context.Background(), key, 1)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !result.Allowed || result.Remaining != 2 || result.ResetAtMs < now.UnixMilli() {
+			t.Fatalf("Expected Allowed=true, Remaining=2, ResetAt >= now; got Allowed=%v, Remaining=%v, ResetAt=%v", result.Allowed, result.Remaining, result.ResetAtMs)
+		}
+	})
+
+	t.Run("AllowN denied after exceeding limit", func(t *testing.T) {
+		key := randStr()
+		generateLoad(t, limiter, key, 3)
+
+		result, err := limiter.AllowN(context.Background(), key, 1)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if result.Allowed || result.Remaining != 0 || result.ResetAtMs < now.UnixMilli() {
+			t.Fatalf("Expected Allowed=false, Remaining=0, ResetAt >= now; got Allowed=%v, Remaining=%v, ResetAt=%v", result.Allowed, result.Remaining, result.ResetAtMs)
+		}
+	})
+
+	t.Run("AllowN with zero tokens", func(t *testing.T) {
+		key := randStr()
+		result, err := limiter.AllowN(context.Background(), key, 0)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !result.Allowed {
+			t.Fatalf("Expected Allowed=true when allowing zero tokens, but got false")
+		}
+	})
+
+	t.Run("AllowN with negative tokens", func(t *testing.T) {
+		key := randStr()
+		result, err := limiter.AllowN(context.Background(), key, -1)
+		if err == nil {
+			t.Fatalf("Expected error for negative tokens, but got nil")
+		}
+		if result.Allowed {
+			t.Fatalf("Expected Allowed=false when allowing negative tokens, but got true")
+		}
+	})
+}
+
+func BenchmarkRateLimiter(b *testing.B) {
+	client := setup(b)
+	defer client.Close()
+
+	limiter, err := rueidislimiter.NewRateLimiter(rueidislimiter.RateLimiterOption{
+		ClientBuilder: func(option rueidis.ClientOption) (rueidis.Client, error) {
+			return client, nil
+		},
+	})
+	if err != nil {
+		b.Fatal(err)
+	}
+	key := randStr()
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	b.Run("Check", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			limiter.Check(context.Background(), key)
+		}
+	})
+
+	b.Run("AllowN", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			limiter.AllowN(context.Background(), key, 1)
+		}
+	})
+}
+
+func generateLoad(t *testing.T, limiter rueidislimiter.RateLimiterClient, key string, n int) {
+	for i := 0; i < n; i++ {
+		_, err := limiter.Allow(context.Background(), key)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
+// randStr generates a 24-byte long, random string.
+func randStr() string {
+	b := make([]byte, 24)
+	binary.LittleEndian.PutUint64(b[12:], rand.Uint64())
+	binary.LittleEndian.PutUint32(b[20:], rand.Uint32())
+	hex.Encode(b, b[12:])
+
+	return unsafe.String(unsafe.SliceData(b), len(b))
+}

valkey-io/valkey-go

Signed-off-by: Rueian <rueiancsie@gmail.com>

valkeylimiter/limiter.go
@@ -1,0 +1,153 @@
+package valkeylimiter
+
+import (
+	"context"
+	"errors"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/valkey-io/valkey-go"
+)
+
+var (
+	ErrInvalidTokens   = errors.New("number of tokens must be non-negative")
+	ErrInvalidResponse = errors.New("invalid response from Redis")
+)
+
+type Result struct {
+	Allowed   bool
+	Remaining int64
+	ResetAtMs int64
+}
+
+type RateLimiterClient interface {
+	Check(ctx context.Context, identifier string) (Result, error)
+	Allow(ctx context.Context, identifier string) (Result, error)
+	AllowN(ctx context.Context, identifier string, n int64) (Result, error)
+}
+
+const PlaceholderPrefix = "valkeylimiter"
+
+type rateLimiter struct {
+	client    valkey.Client
+	keyPrefix string
+	limit     int
+	window    time.Duration
+}
+
+type RateLimiterOption struct {
+	ClientBuilder func(option valkey.ClientOption) (valkey.Client, error)
+	ClientOption  valkey.ClientOption
+	KeyPrefix     string
+	Limit         int
+	Window        time.Duration
+}
+
+func NewRateLimiter(option RateLimiterOption) (RateLimiterClient, error) {
+	if option.Window < time.Millisecond {
+		option.Window = time.Millisecond
+	}
+	if option.Limit <= 0 {
+		option.Limit = 1
+	}
+	if option.KeyPrefix == "" {
+		option.KeyPrefix = PlaceholderPrefix
+	}
+
+	rl := &rateLimiter{
+		limit:  option.Limit,
+		window: option.Window,
+	}
+
+	var err error
+	if option.ClientBuilder != nil {
+		rl.client, err = option.ClientBuilder(option.ClientOption)
+	} else {
+		rl.client, err = valkey.NewClient(option.ClientOption)
+	}
+	if err != nil {
+		return nil, err
+	}
+	rl.keyPrefix = option.KeyPrefix
+	return rl, nil
+}
+
+func (l *rateLimiter) Limit() int {
+	return l.limit
+}
+
+func (l *rateLimiter) Check(ctx context.Context, identifier string) (Result, error) {
+	return l.AllowN(ctx, identifier, 0)
+}
+
+func (l *rateLimiter) Allow(ctx context.Context, identifier string) (Result, error) {
+	return l.AllowN(ctx, identifier, 1)
+}
+
+func (l *rateLimiter) AllowN(ctx context.Context, identifier string, n int64) (Result, error) {
+	if n < 0 {
+		return Result{}, ErrInvalidTokens
+	}
+
+	now := time.Now().UTC()
+	keys := []string{l.getKey(identifier)}
+	args := []string{
+		strconv.FormatInt(n, 10),
+		strconv.FormatInt(now.Add(l.window).UnixMilli(), 10),
+		strconv.FormatInt(now.UnixMilli(), 10),
+	}
+
+	resp := rateLimitScript.Exec(ctx, l.client, keys, args)
+	if err := resp.Error(); err != nil {
+		return Result{}, err
+	}
+
+	data, err := resp.AsIntSlice()
+	if err != nil || len(data) != 2 {
+		return Result{}, ErrInvalidResponse
+	}
+
+	current := data[0]
+	remaining := int64(l.limit) - current
+	if remaining < 0 {
+		remaining = 0
+	}
+
+	allowed := current <= int64(l.limit)
+	if n == 0 {
+		allowed = current < int64(l.limit)
+	}
+
+	return Result{
+		Allowed:   allowed,
+		Remaining: remaining,
+		ResetAtMs: data[1],
+	}, nil
+}
+
+func (l *rateLimiter) getKey(identifier string) string {
+	sb := strings.Builder{}
+	sb.Grow(len(l.keyPrefix) + len(identifier) + 3)
+	sb.WriteString(l.keyPrefix)
+	sb.WriteString(":{")
+	sb.WriteString(identifier)
+	sb.WriteString("}")
+	return sb.String()
+}
+
+var rateLimitScript = valkey.NewLuaScript(`
+local rate_limit_key = KEYS[1]
+local increment_amount = tonumber(ARGV[1])
+local next_expires_at = tonumber(ARGV[2])
+local current_time = tonumber(ARGV[3])
+local expires_at_key = rate_limit_key .. ":ex"
+local expires_at = tonumber(redis.call("get", expires_at_key))
+if not expires_at or expires_at < current_time then
+  redis.call("set", rate_limit_key, 0, "pxat", next_expires_at + 1000)
+  redis.call("set", expires_at_key, next_expires_at, "pxat", next_expires_at + 1000)
+  expires_at = next_expires_at
+end
+local current = redis.call("incrby", rate_limit_key, increment_amount)
+return { current, expires_at }
+`)

valkeylimiter/limiter_test.go
@@ -1,0 +1,209 @@
+package valkeylimiter_test
+
+import (
+	"context"
+	"encoding/binary"
+	"encoding/hex"
+	"math/rand"
+	"testing"
+	"time"
+	"unsafe"
+
+	"github.com/valkey-io/valkey-go"
+	"github.com/valkey-io/valkey-go/valkeylimiter"
+)
+
+func setup(t testing.TB) valkey.Client {
+	client, err := valkey.NewClient(valkey.ClientOption{InitAddress: []string{"127.0.0.1:6379"}})
+	if err != nil {
+		t.Fatal(err)
+	}
+	return client
+}
+
+func TestRateLimiter(t *testing.T) {
+	client := setup(t)
+	t.Cleanup(client.Close)
+
+	now := time.Now()
+	window := 100 * time.Millisecond
+	limiter, err := valkeylimiter.NewRateLimiter(valkeylimiter.RateLimiterOption{
+		ClientBuilder: func(option valkey.ClientOption) (valkey.Client, error) {
+			return client, nil
+		},
+		Limit:  3,
+		Window: window,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("Check defaults", func(t *testing.T) {
+		limiter, err := valkeylimiter.NewRateLimiter(valkeylimiter.RateLimiterOption{
+			ClientBuilder: func(option valkey.ClientOption) (valkey.Client, error) {
+				return client, nil
+			},
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		result, err := limiter.Check(context.Background(), randStr())
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !result.Allowed || result.Remaining != 1 || result.ResetAtMs < now.UnixMilli() {
+			t.Fatalf("Expected Allowed=true, Remaining=1, ResetAt >= now; got Allowed=%v, Remaining=%v, ResetAt=%v", result.Allowed, result.Remaining, result.ResetAtMs)
+		}
+	})
+
+	t.Run("Check allowed within limit", func(t *testing.T) {
+		result, err := limiter.Check(context.Background(), randStr())
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !result.Allowed || result.Remaining != 3 || result.ResetAtMs < now.UnixMilli() {
+			t.Fatalf("Expected Allowed=true, Remaining=3, ResetAt >= now; got Allowed=%v, Remaining=%v, ResetAt=%v", result.Allowed, result.Remaining, result.ResetAtMs)
+		}
+	})
+
+	t.Run("Check denied after exceeding limit", func(t *testing.T) {
+		key := randStr()
+		generateLoad(t, limiter, key, 3)
+
+		result, err := limiter.Check(context.Background(), key)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if result.Allowed || result.Remaining != 0 || result.ResetAtMs < now.UnixMilli() {
+			t.Fatalf("Expected Allowed=false, Remaining=0, ResetAt >= now; got Allowed=%v, Remaining=%v, ResetAt=%v", result.Allowed, result.Remaining, result.ResetAtMs)
+		}
+	})
+
+	t.Run("Check allowed after window reset", func(t *testing.T) {
+		key := randStr()
+		generateLoad(t, limiter, key, 3)
+
+		// Sleep for slightly longer than window duration to ensure reset
+		time.Sleep(window * 2)
+		result, err := limiter.Check(context.Background(), key)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !result.Allowed || result.Remaining != 3 || result.ResetAtMs < now.UnixMilli() {
+			t.Fatalf("Expected Allowed=true, Remaining=3, ResetAt >= now after reset; got Allowed=%v, Remaining=%v, ResetAt=%v", result.Allowed, result.Remaining, result.ResetAtMs)
+		}
+	})
+
+	t.Run("AllowN defaults", func(t *testing.T) {
+		limiter, err := valkeylimiter.NewRateLimiter(valkeylimiter.RateLimiterOption{
+			ClientBuilder: func(option valkey.ClientOption) (valkey.Client, error) {
+				return client, nil
+			},
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		result, err := limiter.AllowN(context.Background(), randStr(), 1)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !result.Allowed || result.Remaining != 0 || result.ResetAtMs < now.UnixMilli() {
+			t.Fatalf("Expected Allowed=true, Remaining=0, ResetAt >= now; got Allowed=%v, Remaining=%v, ResetAt=%v", result.Allowed, result.Remaining, result.ResetAtMs)
+		}
+	})
+
+	t.Run("AllowN with tokens within limit", func(t *testing.T) {
+		key := randStr()
+		result, err := limiter.AllowN(context.Background(), key, 1)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !result.Allowed || result.Remaining != 2 || result.ResetAtMs < now.UnixMilli() {
+			t.Fatalf("Expected Allowed=true, Remaining=2, ResetAt >= now; got Allowed=%v, Remaining=%v, ResetAt=%v", result.Allowed, result.Remaining, result.ResetAtMs)
+		}
+	})
+
+	t.Run("AllowN denied after exceeding limit", func(t *testing.T) {
+		key := randStr()
+		generateLoad(t, limiter, key, 3)
+
+		result, err := limiter.AllowN(context.Background(), key, 1)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if result.Allowed || result.Remaining != 0 || result.ResetAtMs < now.UnixMilli() {
+			t.Fatalf("Expected Allowed=false, Remaining=0, ResetAt >= now; got Allowed=%v, Remaining=%v, ResetAt=%v", result.Allowed, result.Remaining, result.ResetAtMs)
+		}
+	})
+
+	t.Run("AllowN with zero tokens", func(t *testing.T) {
+		key := randStr()
+		result, err := limiter.AllowN(context.Background(), key, 0)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !result.Allowed {
+			t.Fatalf("Expected Allowed=true when allowing zero tokens, but got false")
+		}
+	})
+
+	t.Run("AllowN with negative tokens", func(t *testing.T) {
+		key := randStr()
+		result, err := limiter.AllowN(context.Background(), key, -1)
+		if err == nil {
+			t.Fatalf("Expected error for negative tokens, but got nil")
+		}
+		if result.Allowed {
+			t.Fatalf("Expected Allowed=false when allowing negative tokens, but got true")
+		}
+	})
+}
+
+func BenchmarkRateLimiter(b *testing.B) {
+	client := setup(b)
+	defer client.Close()
+
+	limiter, err := valkeylimiter.NewRateLimiter(valkeylimiter.RateLimiterOption{
+		ClientBuilder: func(option valkey.ClientOption) (valkey.Client, error) {
+			return client, nil
+		},
+	})
+	if err != nil {
+		b.Fatal(err)
+	}
+	key := randStr()
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	b.Run("Check", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			limiter.Check(context.Background(), key)
+		}
+	})
+
+	b.Run("AllowN", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			limiter.AllowN(context.Background(), key, 1)
+		}
+	})
+}
+
+func generateLoad(t *testing.T, limiter valkeylimiter.RateLimiterClient, key string, n int) {
+	for i := 0; i < n; i++ {
+		_, err := limiter.Allow(context.Background(), key)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
+// randStr generates a 24-byte long, random string.
+func randStr() string {
+	b := make([]byte, 24)
+	binary.LittleEndian.PutUint64(b[12:], rand.Uint64())
+	binary.LittleEndian.PutUint32(b[20:], rand.Uint32())
+	hex.Encode(b, b[12:])
+
+	return unsafe.String(unsafe.SliceData(b), len(b))
+}