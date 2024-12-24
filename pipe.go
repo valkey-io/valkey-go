redis/rueidis

fix: protocol out of sync when mixing sunsubscribe requests and slot migrations (#691)
Signed-off-by: Rueian <rueiancsie@gmail.com>

pipe.go
@@ -25,6 +25,17 @@ const LibVer = "1.0.51"
 
 var noHello = regexp.MustCompile("unknown command .?(HELLO|hello).?")
 
+// See https://github.com/redis/rueidis/pull/691
+func isUnsubReply(msg *RedisMessage) bool {
+	// ex. NOPERM User limiteduser has no permissions to run the 'ping' command
+	if msg.typ == '-' && strings.Contains(msg.string, "'ping'") {
+		msg.typ = '+'
+		msg.string = "PONG"
+		return true
+	}
+	return msg.string == "PONG" || (len(msg.values) != 0 && msg.values[0].string == "pong")
+}
+
 type wire interface {
 	Do(ctx context.Context, cmd Completed) RedisResult
 	DoCache(ctx context.Context, cmd Cacheable, ttl time.Duration) RedisResult
@@ -438,6 +449,9 @@ func (p *pipe) _backgroundWrite() (err error) {
 		}
 		for _, cmd := range multi {
 			err = writeCmd(p.w, cmd.Commands())
+			if cmd.IsUnsub() { // See https://github.com/redis/rueidis/pull/691
+				err = writeCmd(p.w, cmds.PingCmd.Commands())
+			}
 		}
 	}
 	return
@@ -456,7 +470,10 @@ func (p *pipe) _backgroundRead() (err error) {
 		ver   = p.version
 		prply bool // push reply
 		unsub bool // unsubscribe notification
-		r2ps  = p.r2ps
+
+		skipUnsubReply bool // if unsubscribe is replied
+
+		r2ps = p.r2ps
 	)
 
 	defer func() {
@@ -516,9 +533,14 @@ func (p *pipe) _backgroundRead() (err error) {
 				// We should ignore them and go fetch next message.
 				// We also treat all the other unsubscribe notifications just like sunsubscribe,
 				// so that we don't need to track how many channels we have subscribed to deal with wildcard unsubscribe command
+				// See https://github.com/redis/rueidis/pull/691
 				if unsub {
 					prply = false
 					unsub = false
+					continue
+				}
+				if skipUnsubReply && isUnsubReply(&msg) {
+					skipUnsubReply = false
 					continue
 				}
 				panic(protocolbug)
@@ -555,7 +577,8 @@ func (p *pipe) _backgroundRead() (err error) {
 			// We should ignore them and go fetch next message.
 			// We also treat all the other unsubscribe notifications just like sunsubscribe,
 			// so that we don't need to track how many channels we have subscribed to deal with wildcard unsubscribe command
-			if unsub && (!multi[ff].NoReply() || !strings.HasSuffix(multi[ff].Commands()[0], "UNSUBSCRIBE")) {
+			// See https://github.com/redis/rueidis/pull/691
+			if unsub {
 				prply = false
 				unsub = false
 				continue
@@ -569,6 +592,16 @@ func (p *pipe) _backgroundRead() (err error) {
 			msg = RedisMessage{} // override successful subscribe/unsubscribe response to empty
 		} else if multi[ff].NoReply() && msg.string == "QUEUED" {
 			panic(multiexecsub)
+		} else if multi[ff].IsUnsub() && !isUnsubReply(&msg) {
+			// See https://github.com/redis/rueidis/pull/691
+			skipUnsubReply = true
+		} else if skipUnsubReply {
+			// See https://github.com/redis/rueidis/pull/691
+			if !isUnsubReply(&msg) {
+				panic(protocolbug)
+			}
+			skipUnsubReply = false
+			continue
 		}
 		resp := newResult(msg, err)
 		if resps != nil {

valkey-io/valkey-go

pipe.go
@@ -25,6 +25,17 @@ const LibVer = "1.0.46"
 
 var noHello = regexp.MustCompile("unknown command .?(HELLO|hello).?")
 
+// See https://github.com/redis/rueidis/pull/691
+func isUnsubReply(msg *ValkeyMessage) bool {
+	// ex. NOPERM User limiteduser has no permissions to run the 'ping' command
+	if msg.typ == '-' && strings.Contains(msg.string, "'ping'") {
+		msg.typ = '+'
+		msg.string = "PONG"
+		return true
+	}
+	return msg.string == "PONG" || (len(msg.values) != 0 && msg.values[0].string == "pong")
+}
+
 type wire interface {
 	Do(ctx context.Context, cmd Completed) ValkeyResult
 	DoCache(ctx context.Context, cmd Cacheable, ttl time.Duration) ValkeyResult
@@ -438,6 +449,9 @@ func (p *pipe) _backgroundWrite() (err error) {
 		}
 		for _, cmd := range multi {
 			err = writeCmd(p.w, cmd.Commands())
+			if cmd.IsUnsub() { // See https://github.com/redis/rueidis/pull/691
+				err = writeCmd(p.w, cmds.PingCmd.Commands())
+			}
 		}
 	}
 	return
@@ -456,7 +470,10 @@ func (p *pipe) _backgroundRead() (err error) {
 		ver   = p.version
 		prply bool // push reply
 		unsub bool // unsubscribe notification
-		r2ps  = p.r2ps
+
+		skipUnsubReply bool // if unsubscribe is replied
+
+		r2ps = p.r2ps
 	)
 
 	defer func() {
@@ -516,9 +533,14 @@ func (p *pipe) _backgroundRead() (err error) {
 				// We should ignore them and go fetch next message.
 				// We also treat all the other unsubscribe notifications just like sunsubscribe,
 				// so that we don't need to track how many channels we have subscribed to deal with wildcard unsubscribe command
+				// See https://github.com/redis/rueidis/pull/691
 				if unsub {
 					prply = false
 					unsub = false
+					continue
+				}
+				if skipUnsubReply && isUnsubReply(&msg) {
+					skipUnsubReply = false
 					continue
 				}
 				panic(protocolbug)
@@ -555,7 +577,8 @@ func (p *pipe) _backgroundRead() (err error) {
 			// We should ignore them and go fetch next message.
 			// We also treat all the other unsubscribe notifications just like sunsubscribe,
 			// so that we don't need to track how many channels we have subscribed to deal with wildcard unsubscribe command
-			if unsub && (!multi[ff].NoReply() || !strings.HasSuffix(multi[ff].Commands()[0], "UNSUBSCRIBE")) {
+			// See https://github.com/redis/rueidis/pull/691
+			if unsub {
 				prply = false
 				unsub = false
 				continue
@@ -569,6 +592,16 @@ func (p *pipe) _backgroundRead() (err error) {
 			msg = ValkeyMessage{} // override successful subscribe/unsubscribe response to empty
 		} else if multi[ff].NoReply() && msg.string == "QUEUED" {
 			panic(multiexecsub)
+		} else if multi[ff].IsUnsub() && !isUnsubReply(&msg) {
+			// See https://github.com/redis/rueidis/pull/691
+			skipUnsubReply = true
+		} else if skipUnsubReply {
+			// See https://github.com/redis/rueidis/pull/691
+			if !isUnsubReply(&msg) {
+				panic(protocolbug)
+			}
+			skipUnsubReply = false
+			continue
 		}
 		resp := newResult(msg, err)
 		if resps != nil {