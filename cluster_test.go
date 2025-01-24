redis/rueidis

feat: add AZ to replica info (#721)
* feat: add AZ to replica info

* refactor: alwyas send HELLO

* refactor: use hello 2 explicit

* perf: use only when replica exist

* feat: add enable az lookup

* refactor: add comments to az lookup

* refactor: do not use mutex

* refactor: rename option

cluster_test.go
@@ -1418,6 +1418,111 @@ func TestClusterClientInit(t *testing.T) {
 			t.Fatalf("unexpected node assigned to rslot 16383")
 		}
 	})
+
+	t.Run("Refresh cluster which has multi replicas with az", func(t *testing.T) {
+		primaryNodeConn := &mockConn{
+			DoFn: func(cmd Completed) RedisResult {
+				if strings.Join(cmd.Commands(), " ") == "CLUSTER SLOTS" {
+					return slotsMultiRespWithMultiReplicas
+				}
+				return RedisResult{
+					err: errors.New("unexpected call"),
+				}
+			},
+			AZFn: func() string {
+				return "us-west-1a"
+			},
+		}
+		replicaNodeConn1 := &mockConn{
+			DoFn: func(cmd Completed) RedisResult {
+				return RedisResult{
+					err: errors.New("unexpected call"),
+				}
+			},
+			AZFn: func() string {
+				return "us-west-1a"
+			},
+		}
+		replicaNodeConn2 := &mockConn{
+			DoFn: func(cmd Completed) RedisResult {
+				return RedisResult{
+					err: errors.New("unexpected call"),
+				}
+			},
+			AZFn: func() string {
+				return "us-west-1b"
+			},
+		}
+		replicaNodeConn3 := &mockConn{
+			DoFn: func(cmd Completed) RedisResult {
+				return RedisResult{
+					err: errors.New("unexpected call"),
+				}
+			},
+			AZFn: func() string {
+				return "us-west-1c"
+			},
+		}
+
+		client, err := newClusterClient(
+			&ClientOption{
+				InitAddress: []string{"127.0.0.1:0"},
+				SendToReplicas: func(cmd Completed) bool {
+					return true
+				},
+				ReplicaSelector: func(slot uint16, replicas []ReplicaInfo) int {
+					for i, replica := range replicas {
+						if replica.AZ == "us-west-1b" {
+							return i
+						}
+					}
+					return -1
+				},
+				EnableReplicaInfoAZ: true,
+			},
+			func(dst string, opt *ClientOption) conn {
+				switch {
+				case dst == "127.0.0.2:1" || dst == "127.0.1.2:1":
+					return replicaNodeConn1
+				case dst == "127.0.0.3:2" || dst == "127.0.1.3:2":
+					return replicaNodeConn2
+				case dst == "127.0.0.4:3" || dst == "127.0.1.4:3":
+					return replicaNodeConn3
+				default:
+					return primaryNodeConn
+				}
+			},
+			newRetryer(defaultRetryDelayFn),
+		)
+		if err != nil {
+			t.Fatalf("unexpected err %v", err)
+		}
+
+		if client.pslots[0] != primaryNodeConn {
+			t.Fatalf("unexpected node assigned to pslot 0")
+		}
+		if client.pslots[8192] != primaryNodeConn {
+			t.Fatalf("unexpected node assigned to pslot 8192")
+		}
+		if client.pslots[8193] != primaryNodeConn {
+			t.Fatalf("unexpected node assigned to pslot 8193")
+		}
+		if client.pslots[16383] != primaryNodeConn {
+			t.Fatalf("unexpected node assigned to pslot 16383")
+		}
+		if client.rslots[0] != replicaNodeConn2 {
+			t.Fatalf("unexpected node assigned to rslot 0")
+		}
+		if client.rslots[8192] != replicaNodeConn2 {
+			t.Fatalf("unexpected node assigned to rslot 8192")
+		}
+		if client.rslots[8193] != replicaNodeConn2 {
+			t.Fatalf("unexpected node assigned to rslot 8193")
+		}
+		if client.rslots[16383] != replicaNodeConn2 {
+			t.Fatalf("unexpected node assigned to rslot 16383")
+		}
+	})
 }
 
 //gocyclo:ignore

pipe.go
@@ -48,6 +48,7 @@ type wire interface {
 	DoMultiStream(ctx context.Context, pool *pool, multi ...Completed) MultiRedisResultStream
 	Info() map[string]RedisMessage
 	Version() int
+	AZ() string
 	Error() error
 	Close()
 
@@ -257,6 +258,7 @@ func _newPipe(connFn func() (net.Conn, error), option *ClientOption, r2ps, nobg 
 			return nil, ErrNoCache
 		}
 		init = init[:0]
+		init = append(init, []string{"HELLO", "2"})
 		if password != "" && username == "" {
 			init = append(init, []string{"AUTH", password})
 		} else if username != "" {
@@ -304,8 +306,14 @@ func _newPipe(connFn func() (net.Conn, error), option *ClientOption, r2ps, nobg 
 					continue
 				}
 				if err = r.Error(); err != nil {
+					if re, ok := err.(*RedisError); ok && noHello.MatchString(re.string) {
+						continue
+					}
 					p.Close()
 					return nil, err
+				}
+				if i == 0 {
+					p.info, err = r.AsMap()
 				}
 			}
 		}
@@ -840,6 +848,10 @@ func (p *pipe) Info() map[string]RedisMessage {
 
 func (p *pipe) Version() int {
 	return int(p.version)
+}
+
+func (p *pipe) AZ() string {
+	return p.info["availability_zone"].string
 }
 
 func (p *pipe) Do(ctx context.Context, cmd Completed) (resp RedisResult) {

pipe_test.go
@@ -211,6 +211,8 @@ func TestNewPipe(t *testing.T) {
 					values: []RedisMessage{
 						{typ: '+', string: "proto"},
 						{typ: ':', integer: 3},
+						{typ: '+', string: "availability_zone"},
+						{typ: '+', string: "us-west-1a"},
 					},
 				})
 			mock.Expect("CLIENT", "TRACKING", "ON", "OPTIN").
@@ -238,6 +240,9 @@ func TestNewPipe(t *testing.T) {
 			t.Fatalf("pipe setup failed: %v", err)
 		}
 		go func() { mock.Expect("PING").ReplyString("OK") }()
+		if p.AZ() != "us-west-1a" {
+			t.Fatalf("unexpected az: %v", p.AZ())
+		}
 		p.Close()
 		mock.Close()
 		n1.Close()
@@ -247,6 +252,16 @@ func TestNewPipe(t *testing.T) {
 		n1, n2 := net.Pipe()
 		mock := &redisMock{buf: bufio.NewReader(n2), conn: n2, t: t}
 		go func() {
+			mock.Expect("HELLO", "2").
+				Reply(RedisMessage{
+					typ: '*',
+					values: []RedisMessage{
+						{typ: '+', string: "proto"},
+						{typ: ':', integer: 2},
+						{typ: '+', string: "availability_zone"},
+						{typ: '+', string: "us-west-1a"},
+					},
+				})
 			mock.Expect("AUTH", "pa").
 				ReplyString("OK")
 			mock.Expect("CLIENT", "SETNAME", "cn").
@@ -276,6 +291,9 @@ func TestNewPipe(t *testing.T) {
 			t.Fatalf("pipe setup failed: %v", err)
 		}
 		go func() { mock.Expect("PING").ReplyString("OK") }()
+		if p.AZ() != "us-west-1a" {
+			t.Fatalf("unexpected az: %v", p.AZ())
+		}
 		p.Close()
 		mock.Close()
 		n1.Close()
@@ -312,6 +330,9 @@ func TestNewPipe(t *testing.T) {
 			t.Fatalf("pipe setup failed: %v", err)
 		}
 		go func() { mock.Expect("PING").ReplyString("OK") }()
+		if p.AZ() != "" {
+			t.Fatalf("unexpected az: %v", p.AZ())
+		}
 		p.Close()
 		mock.Close()
 		n1.Close()
@@ -578,6 +599,21 @@ func TestNewRESP2Pipe(t *testing.T) {
 					{typ: '+', string: "redis"},
 					{typ: '+', string: "proto"},
 					{typ: ':', integer: 2},
+					{typ: '+', string: "availability_zone"},
+					{typ: '+', string: "us-west-1a"},
+				}})
+			mock.Expect("CLIENT", "SETINFO", "LIB-NAME", LibName).
+				ReplyError("UNKNOWN COMMAND")
+			mock.Expect("CLIENT", "SETINFO", "LIB-VER", LibVer).
+				ReplyError("UNKNOWN COMMAND")
+			mock.Expect("HELLO", "2").
+				Reply(RedisMessage{typ: '*', values: []RedisMessage{
+					{typ: '+', string: "server"},
+					{typ: '+', string: "redis"},
+					{typ: '+', string: "proto"},
+					{typ: ':', integer: 2},
+					{typ: '+', string: "availability_zone"},
+					{typ: '+', string: "us-west-1a"},
 				}})
 			mock.Expect("CLIENT", "SETINFO", "LIB-NAME", LibName).
 				ReplyError("UNKNOWN COMMAND")
@@ -592,6 +628,9 @@ func TestNewRESP2Pipe(t *testing.T) {
 		}
 		if p.version >= 6 {
 			t.Fatalf("unexpected p.version: %v", p.version)
+		}
+		if p.AZ() != "us-west-1a" {
+			t.Fatalf("unexpected az: %v", p.AZ())
 		}
 		go func() { mock.Expect("PING").ReplyString("OK") }()
 		p.Close()
@@ -611,6 +650,8 @@ func TestNewRESP2Pipe(t *testing.T) {
 				ReplyError("UNKNOWN COMMAND")
 			mock.Expect("CLIENT", "SETINFO", "LIB-VER", LibVer).
 				ReplyError("UNKNOWN COMMAND")
+			mock.Expect("HELLO", "2").
+				ReplyError("ERR unknown command `HELLO`")
 			mock.Expect("AUTH", "pa").
 				ReplyString("OK")
 			mock.Expect("CLIENT", "SETNAME", "cn").
@@ -652,6 +693,8 @@ func TestNewRESP2Pipe(t *testing.T) {
 				ReplyError("UNKNOWN COMMAND")
 			mock.Expect("CLIENT", "SETINFO", "LIB-VER", LibVer).
 				ReplyError("UNKNOWN COMMAND")
+			mock.Expect("HELLO", "2").
+				ReplyError("ERR unknown command `HELLO`")
 			mock.Expect("AUTH", "ua", "pa").
 				ReplyString("OK")
 			mock.Expect("CLIENT", "SETNAME", "cn").
@@ -696,6 +739,8 @@ func TestNewRESP2Pipe(t *testing.T) {
 				ReplyError("UNKNOWN COMMAND")
 			mock.Expect("CLIENT", "SETINFO", "LIB-VER", LibVer).
 				ReplyError("UNKNOWN COMMAND")
+			mock.Expect("HELLO", "2").
+				ReplyError("ERR unknown command `HELLO`")
 			mock.Expect("AUTH", "pa").
 				ReplyString("OK")
 			mock.Expect("CLIENT", "SETNAME", "cn").
@@ -742,6 +787,8 @@ func TestNewRESP2Pipe(t *testing.T) {
 				ReplyError("UNKNOWN COMMAND")
 			mock.Expect("CLIENT", "SETINFO", "LIB-VER", LibVer).
 				ReplyError("UNKNOWN COMMAND")
+			mock.Expect("HELLO", "2").
+				ReplyError("ERR unknown command `HELLO`")
 			mock.Expect("AUTH", "pa").
 				ReplyString("OK")
 			mock.Expect("CLIENT", "SETNAME", "cn").
@@ -805,6 +852,8 @@ func TestNewRESP2Pipe(t *testing.T) {
 		mock := &redisMock{buf: bufio.NewReader(n2), conn: n2, t: t}
 		go func() {
 			mock.Expect("HELLO", "3").
+				ReplyError("ERR unknown command `HELLO`")
+			mock.Expect("HELLO", "2").
 				ReplyError("ERR unknown command `HELLO`")
 		}()
 		p, err := newPipe(func() (net.Conn, error) { return n1, nil }, &ClientOption{

valkey-io/valkey-go

Signed-off-by: Rueian <rueiancsie@gmail.com>

cluster_test.go
@@ -1418,6 +1418,111 @@ func TestClusterClientInit(t *testing.T) {
 			t.Fatalf("unexpected node assigned to rslot 16383")
 		}
 	})
+
+	t.Run("Refresh cluster which has multi replicas with az", func(t *testing.T) {
+		primaryNodeConn := &mockConn{
+			DoFn: func(cmd Completed) ValkeyResult {
+				if strings.Join(cmd.Commands(), " ") == "CLUSTER SLOTS" {
+					return slotsMultiRespWithMultiReplicas
+				}
+				return ValkeyResult{
+					err: errors.New("unexpected call"),
+				}
+			},
+			AZFn: func() string {
+				return "us-west-1a"
+			},
+		}
+		replicaNodeConn1 := &mockConn{
+			DoFn: func(cmd Completed) ValkeyResult {
+				return ValkeyResult{
+					err: errors.New("unexpected call"),
+				}
+			},
+			AZFn: func() string {
+				return "us-west-1a"
+			},
+		}
+		replicaNodeConn2 := &mockConn{
+			DoFn: func(cmd Completed) ValkeyResult {
+				return ValkeyResult{
+					err: errors.New("unexpected call"),
+				}
+			},
+			AZFn: func() string {
+				return "us-west-1b"
+			},
+		}
+		replicaNodeConn3 := &mockConn{
+			DoFn: func(cmd Completed) ValkeyResult {
+				return ValkeyResult{
+					err: errors.New("unexpected call"),
+				}
+			},
+			AZFn: func() string {
+				return "us-west-1c"
+			},
+		}
+
+		client, err := newClusterClient(
+			&ClientOption{
+				InitAddress: []string{"127.0.0.1:0"},
+				SendToReplicas: func(cmd Completed) bool {
+					return true
+				},
+				ReplicaSelector: func(slot uint16, replicas []ReplicaInfo) int {
+					for i, replica := range replicas {
+						if replica.AZ == "us-west-1b" {
+							return i
+						}
+					}
+					return -1
+				},
+				EnableReplicaInfoAZ: true,
+			},
+			func(dst string, opt *ClientOption) conn {
+				switch {
+				case dst == "127.0.0.2:1" || dst == "127.0.1.2:1":
+					return replicaNodeConn1
+				case dst == "127.0.0.3:2" || dst == "127.0.1.3:2":
+					return replicaNodeConn2
+				case dst == "127.0.0.4:3" || dst == "127.0.1.4:3":
+					return replicaNodeConn3
+				default:
+					return primaryNodeConn
+				}
+			},
+			newRetryer(defaultRetryDelayFn),
+		)
+		if err != nil {
+			t.Fatalf("unexpected err %v", err)
+		}
+
+		if client.pslots[0] != primaryNodeConn {
+			t.Fatalf("unexpected node assigned to pslot 0")
+		}
+		if client.pslots[8192] != primaryNodeConn {
+			t.Fatalf("unexpected node assigned to pslot 8192")
+		}
+		if client.pslots[8193] != primaryNodeConn {
+			t.Fatalf("unexpected node assigned to pslot 8193")
+		}
+		if client.pslots[16383] != primaryNodeConn {
+			t.Fatalf("unexpected node assigned to pslot 16383")
+		}
+		if client.rslots[0] != replicaNodeConn2 {
+			t.Fatalf("unexpected node assigned to rslot 0")
+		}
+		if client.rslots[8192] != replicaNodeConn2 {
+			t.Fatalf("unexpected node assigned to rslot 8192")
+		}
+		if client.rslots[8193] != replicaNodeConn2 {
+			t.Fatalf("unexpected node assigned to rslot 8193")
+		}
+		if client.rslots[16383] != replicaNodeConn2 {
+			t.Fatalf("unexpected node assigned to rslot 16383")
+		}
+	})
 }
 
 //gocyclo:ignore

pipe.go
@@ -48,6 +48,7 @@ type wire interface {
 	DoMultiStream(ctx context.Context, pool *pool, multi ...Completed) MultiValkeyResultStream
 	Info() map[string]ValkeyMessage
 	Version() int
+	AZ() string
 	Error() error
 	Close()
 
@@ -257,6 +258,7 @@ func _newPipe(connFn func() (net.Conn, error), option *ClientOption, r2ps, nobg 
 			return nil, ErrNoCache
 		}
 		init = init[:0]
+		init = append(init, []string{"HELLO", "2"})
 		if password != "" && username == "" {
 			init = append(init, []string{"AUTH", password})
 		} else if username != "" {
@@ -304,8 +306,14 @@ func _newPipe(connFn func() (net.Conn, error), option *ClientOption, r2ps, nobg 
 					continue
 				}
 				if err = r.Error(); err != nil {
+					if re, ok := err.(*ValkeyError); ok && noHello.MatchString(re.string) {
+						continue
+					}
 					p.Close()
 					return nil, err
+				}
+				if i == 0 {
+					p.info, err = r.AsMap()
 				}
 			}
 		}
@@ -840,6 +848,10 @@ func (p *pipe) Info() map[string]ValkeyMessage {
 
 func (p *pipe) Version() int {
 	return int(p.version)
+}
+
+func (p *pipe) AZ() string {
+	return p.info["availability_zone"].string
 }
 
 func (p *pipe) Do(ctx context.Context, cmd Completed) (resp ValkeyResult) {

pipe_test.go
@@ -211,6 +211,8 @@ func TestNewPipe(t *testing.T) {
 					values: []ValkeyMessage{
 						{typ: '+', string: "proto"},
 						{typ: ':', integer: 3},
+						{typ: '+', string: "availability_zone"},
+						{typ: '+', string: "us-west-1a"},
 					},
 				})
 			mock.Expect("CLIENT", "TRACKING", "ON", "OPTIN").
@@ -238,6 +240,9 @@ func TestNewPipe(t *testing.T) {
 			t.Fatalf("pipe setup failed: %v", err)
 		}
 		go func() { mock.Expect("PING").ReplyString("OK") }()
+		if p.AZ() != "us-west-1a" {
+			t.Fatalf("unexpected az: %v", p.AZ())
+		}
 		p.Close()
 		mock.Close()
 		n1.Close()
@@ -247,6 +252,16 @@ func TestNewPipe(t *testing.T) {
 		n1, n2 := net.Pipe()
 		mock := &valkeyMock{buf: bufio.NewReader(n2), conn: n2, t: t}
 		go func() {
+			mock.Expect("HELLO", "2").
+				Reply(ValkeyMessage{
+					typ: '*',
+					values: []ValkeyMessage{
+						{typ: '+', string: "proto"},
+						{typ: ':', integer: 2},
+						{typ: '+', string: "availability_zone"},
+						{typ: '+', string: "us-west-1a"},
+					},
+				})
 			mock.Expect("AUTH", "pa").
 				ReplyString("OK")
 			mock.Expect("CLIENT", "SETNAME", "cn").
@@ -276,6 +291,9 @@ func TestNewPipe(t *testing.T) {
 			t.Fatalf("pipe setup failed: %v", err)
 		}
 		go func() { mock.Expect("PING").ReplyString("OK") }()
+		if p.AZ() != "us-west-1a" {
+			t.Fatalf("unexpected az: %v", p.AZ())
+		}
 		p.Close()
 		mock.Close()
 		n1.Close()
@@ -312,6 +330,9 @@ func TestNewPipe(t *testing.T) {
 			t.Fatalf("pipe setup failed: %v", err)
 		}
 		go func() { mock.Expect("PING").ReplyString("OK") }()
+		if p.AZ() != "" {
+			t.Fatalf("unexpected az: %v", p.AZ())
+		}
 		p.Close()
 		mock.Close()
 		n1.Close()
@@ -578,6 +599,21 @@ func TestNewRESP2Pipe(t *testing.T) {
 					{typ: '+', string: "valkey"},
 					{typ: '+', string: "proto"},
 					{typ: ':', integer: 2},
+					{typ: '+', string: "availability_zone"},
+					{typ: '+', string: "us-west-1a"},
+				}})
+			mock.Expect("CLIENT", "SETINFO", "LIB-NAME", LibName).
+				ReplyError("UNKNOWN COMMAND")
+			mock.Expect("CLIENT", "SETINFO", "LIB-VER", LibVer).
+				ReplyError("UNKNOWN COMMAND")
+			mock.Expect("HELLO", "2").
+				Reply(ValkeyMessage{typ: '*', values: []ValkeyMessage{
+					{typ: '+', string: "server"},
+					{typ: '+', string: "redis"},
+					{typ: '+', string: "proto"},
+					{typ: ':', integer: 2},
+					{typ: '+', string: "availability_zone"},
+					{typ: '+', string: "us-west-1a"},
 				}})
 			mock.Expect("CLIENT", "SETINFO", "LIB-NAME", LibName).
 				ReplyError("UNKNOWN COMMAND")
@@ -592,6 +628,9 @@ func TestNewRESP2Pipe(t *testing.T) {
 		}
 		if p.version >= 6 {
 			t.Fatalf("unexpected p.version: %v", p.version)
+		}
+		if p.AZ() != "us-west-1a" {
+			t.Fatalf("unexpected az: %v", p.AZ())
 		}
 		go func() { mock.Expect("PING").ReplyString("OK") }()
 		p.Close()
@@ -611,6 +650,8 @@ func TestNewRESP2Pipe(t *testing.T) {
 				ReplyError("UNKNOWN COMMAND")
 			mock.Expect("CLIENT", "SETINFO", "LIB-VER", LibVer).
 				ReplyError("UNKNOWN COMMAND")
+			mock.Expect("HELLO", "2").
+				ReplyError("ERR unknown command `HELLO`")
 			mock.Expect("AUTH", "pa").
 				ReplyString("OK")
 			mock.Expect("CLIENT", "SETNAME", "cn").
@@ -652,6 +693,8 @@ func TestNewRESP2Pipe(t *testing.T) {
 				ReplyError("UNKNOWN COMMAND")
 			mock.Expect("CLIENT", "SETINFO", "LIB-VER", LibVer).
 				ReplyError("UNKNOWN COMMAND")
+			mock.Expect("HELLO", "2").
+				ReplyError("ERR unknown command `HELLO`")
 			mock.Expect("AUTH", "ua", "pa").
 				ReplyString("OK")
 			mock.Expect("CLIENT", "SETNAME", "cn").
@@ -696,6 +739,8 @@ func TestNewRESP2Pipe(t *testing.T) {
 				ReplyError("UNKNOWN COMMAND")
 			mock.Expect("CLIENT", "SETINFO", "LIB-VER", LibVer).
 				ReplyError("UNKNOWN COMMAND")
+			mock.Expect("HELLO", "2").
+				ReplyError("ERR unknown command `HELLO`")
 			mock.Expect("AUTH", "pa").
 				ReplyString("OK")
 			mock.Expect("CLIENT", "SETNAME", "cn").
@@ -742,6 +787,8 @@ func TestNewRESP2Pipe(t *testing.T) {
 				ReplyError("UNKNOWN COMMAND")
 			mock.Expect("CLIENT", "SETINFO", "LIB-VER", LibVer).
 				ReplyError("UNKNOWN COMMAND")
+			mock.Expect("HELLO", "2").
+				ReplyError("ERR unknown command `HELLO`")
 			mock.Expect("AUTH", "pa").
 				ReplyString("OK")
 			mock.Expect("CLIENT", "SETNAME", "cn").
@@ -805,6 +852,8 @@ func TestNewRESP2Pipe(t *testing.T) {
 		mock := &valkeyMock{buf: bufio.NewReader(n2), conn: n2, t: t}
 		go func() {
 			mock.Expect("HELLO", "3").
+				ReplyError("ERR unknown command `HELLO`")
+			mock.Expect("HELLO", "2").
 				ReplyError("ERR unknown command `HELLO`")
 		}()
 		p, err := newPipe(func() (net.Conn, error) { return n1, nil }, &ClientOption{