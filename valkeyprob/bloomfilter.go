redis/rueidis

perf(rueidisprob): reduce allocations in bloom filter operations (#722)
Optimizes memory allocations in bloom filter operations, resulting in:
- 99.13% fewer allocations in BloomFilter operations
- 98.54% fewer allocations in CountingBloomFilter operations
- 21.72% less memory usage in BloomFilter operations
- 17.25% less memory usage in CountingBloomFilter operations
- 2.41% faster BloomFilter operations
- 2.48% faster CountingBloomFilter operations

Signed-off-by: Ernesto Alejandro Santana Hidalgo <ernesto.alejandrosantana@gmail.com>

rueidisprob/bloomfilter.go
@@ -256,7 +256,10 @@ func (c *bloomFilter) AddMulti(ctx context.Context, keys []string) error {
 		return nil
 	}
 
-	indexes := c.indexes(keys)
+	buf := bytesPool.Get(0, len(keys)*int(c.hashIterations)*8)
+	defer bytesPool.Put(buf)
+
+	indexes := c.indexes(keys, &buf.s)
 
 	args := make([]string, 0, len(indexes)+1)
 	args = append(args, c.hashIterationString)
@@ -266,13 +269,15 @@ func (c *bloomFilter) AddMulti(ctx context.Context, keys []string) error {
 	return resp.Error()
 }
 
-func (c *bloomFilter) indexes(keys []string) []string {
+func (c *bloomFilter) indexes(keys []string, buf *[]byte) []string {
 	allIndexes := make([]string, 0, len(keys)*int(c.hashIterations))
 	size := uint64(c.size)
 	for _, key := range keys {
 		h1, h2 := hash([]byte(key))
 		for i := uint(0); i < c.hashIterations; i++ {
-			allIndexes = append(allIndexes, strconv.FormatUint(index(h1, h2, i, size), 10))
+			offset := len(*buf)
+			*buf = strconv.AppendUint(*buf, index(h1, h2, i, size), 10)
+			allIndexes = append(allIndexes, rueidis.BinaryString((*buf)[offset:]))
 		}
 	}
 	return allIndexes
@@ -292,7 +297,10 @@ func (c *bloomFilter) ExistsMulti(ctx context.Context, keys []string) ([]bool, e
 		return nil, nil
 	}
 
-	indexes := c.indexes(keys)
+	buf := bytesPool.Get(0, len(keys)*int(c.hashIterations)*8)
+	defer bytesPool.Put(buf)
+
+	indexes := c.indexes(keys, &buf.s)
 
 	args := make([]string, 0, len(indexes)+1)
 	args = append(args, c.hashIterationString)

rueidisprob/countingbloomfilter.go
@@ -228,7 +228,10 @@ func (f *countingBloomFilter) AddMulti(ctx context.Context, keys []string) error
 		return nil
 	}
 
-	indexes := f.indexes(keys)
+	buf := bytesPool.Get(0, len(keys)*int(f.hashIterations)*8)
+	defer bytesPool.Put(buf)
+
+	indexes := f.indexes(keys, &buf.s)
 
 	args := make([]string, 0, len(indexes)+1)
 	args = append(args, strconv.Itoa(len(keys)))
@@ -238,13 +241,15 @@ func (f *countingBloomFilter) AddMulti(ctx context.Context, keys []string) error
 	return resp.Error()
 }
 
-func (f *countingBloomFilter) indexes(keys []string) []string {
+func (f *countingBloomFilter) indexes(keys []string, buf *[]byte) []string {
 	allIndexes := make([]string, 0, len(keys)*int(f.hashIterations))
 	size := uint64(f.size)
 	for _, key := range keys {
 		h1, h2 := hash([]byte(key))
 		for i := uint(0); i < f.hashIterations; i++ {
-			allIndexes = append(allIndexes, strconv.FormatUint(index(h1, h2, i, size), 10))
+			offset := len(*buf)
+			*buf = strconv.AppendUint(*buf, index(h1, h2, i, size), 10)
+			allIndexes = append(allIndexes, rueidis.BinaryString((*buf)[offset:]))
 		}
 	}
 	return allIndexes
@@ -264,7 +269,10 @@ func (f *countingBloomFilter) ExistsMulti(ctx context.Context, keys []string) ([
 		return nil, nil
 	}
 
-	indexes := f.indexes(keys)
+	buf := bytesPool.Get(0, len(keys)*int(f.hashIterations)*8)
+	defer bytesPool.Put(buf)
+
+	indexes := f.indexes(keys, &buf.s)
 
 	resp := f.client.Do(
 		ctx,
@@ -317,7 +325,11 @@ func (f *countingBloomFilter) RemoveMulti(ctx context.Context, keys []string) er
 		return nil
 	}
 
-	indexes := f.indexes(keys)
+	buf := bytesPool.Get(0, len(keys)*int(f.hashIterations)*8)
+	defer bytesPool.Put(buf)
+
+	indexes := f.indexes(keys, &buf.s)
+
 	args := make([]string, 0, len(indexes)+1)
 	args = append(args, indexes...)
 	args = append(args, f.hashIterationString)
@@ -353,7 +365,10 @@ func (f *countingBloomFilter) ItemMinCountMulti(ctx context.Context, keys []stri
 		return nil, nil
 	}
 
-	indexes := f.indexes(keys)
+	buf := bytesPool.Get(0, len(keys)*int(f.hashIterations)*8)
+	defer bytesPool.Put(buf)
+
+	indexes := f.indexes(keys, &buf.s)
 
 	resp := f.client.Do(
 		ctx,

valkey-io/valkey-go

Signed-off-by: Rueian <rueiancsie@gmail.com>

valkeyprob/bloomfilter.go
@@ -256,7 +256,10 @@ func (c *bloomFilter) AddMulti(ctx context.Context, keys []string) error {
 		return nil
 	}
 
-	indexes := c.indexes(keys)
+	buf := bytesPool.Get(0, len(keys)*int(c.hashIterations)*8)
+	defer bytesPool.Put(buf)
+
+	indexes := c.indexes(keys, &buf.s)
 
 	args := make([]string, 0, len(indexes)+1)
 	args = append(args, c.hashIterationString)
@@ -266,13 +269,15 @@ func (c *bloomFilter) AddMulti(ctx context.Context, keys []string) error {
 	return resp.Error()
 }
 
-func (c *bloomFilter) indexes(keys []string) []string {
+func (c *bloomFilter) indexes(keys []string, buf *[]byte) []string {
 	allIndexes := make([]string, 0, len(keys)*int(c.hashIterations))
 	size := uint64(c.size)
 	for _, key := range keys {
 		h1, h2 := hash([]byte(key))
 		for i := uint(0); i < c.hashIterations; i++ {
-			allIndexes = append(allIndexes, strconv.FormatUint(index(h1, h2, i, size), 10))
+			offset := len(*buf)
+			*buf = strconv.AppendUint(*buf, index(h1, h2, i, size), 10)
+			allIndexes = append(allIndexes, valkey.BinaryString((*buf)[offset:]))
 		}
 	}
 	return allIndexes
@@ -292,7 +297,10 @@ func (c *bloomFilter) ExistsMulti(ctx context.Context, keys []string) ([]bool, e
 		return nil, nil
 	}
 
-	indexes := c.indexes(keys)
+	buf := bytesPool.Get(0, len(keys)*int(c.hashIterations)*8)
+	defer bytesPool.Put(buf)
+
+	indexes := c.indexes(keys, &buf.s)
 
 	args := make([]string, 0, len(indexes)+1)
 	args = append(args, c.hashIterationString)

valkeyprob/countingbloomfilter.go
@@ -228,7 +228,10 @@ func (f *countingBloomFilter) AddMulti(ctx context.Context, keys []string) error
 		return nil
 	}
 
-	indexes := f.indexes(keys)
+	buf := bytesPool.Get(0, len(keys)*int(f.hashIterations)*8)
+	defer bytesPool.Put(buf)
+
+	indexes := f.indexes(keys, &buf.s)
 
 	args := make([]string, 0, len(indexes)+1)
 	args = append(args, strconv.Itoa(len(keys)))
@@ -238,13 +241,15 @@ func (f *countingBloomFilter) AddMulti(ctx context.Context, keys []string) error
 	return resp.Error()
 }
 
-func (f *countingBloomFilter) indexes(keys []string) []string {
+func (f *countingBloomFilter) indexes(keys []string, buf *[]byte) []string {
 	allIndexes := make([]string, 0, len(keys)*int(f.hashIterations))
 	size := uint64(f.size)
 	for _, key := range keys {
 		h1, h2 := hash([]byte(key))
 		for i := uint(0); i < f.hashIterations; i++ {
-			allIndexes = append(allIndexes, strconv.FormatUint(index(h1, h2, i, size), 10))
+			offset := len(*buf)
+			*buf = strconv.AppendUint(*buf, index(h1, h2, i, size), 10)
+			allIndexes = append(allIndexes, valkey.BinaryString((*buf)[offset:]))
 		}
 	}
 	return allIndexes
@@ -264,7 +269,10 @@ func (f *countingBloomFilter) ExistsMulti(ctx context.Context, keys []string) ([
 		return nil, nil
 	}
 
-	indexes := f.indexes(keys)
+	buf := bytesPool.Get(0, len(keys)*int(f.hashIterations)*8)
+	defer bytesPool.Put(buf)
+
+	indexes := f.indexes(keys, &buf.s)
 
 	resp := f.client.Do(
 		ctx,
@@ -317,7 +325,11 @@ func (f *countingBloomFilter) RemoveMulti(ctx context.Context, keys []string) er
 		return nil
 	}
 
-	indexes := f.indexes(keys)
+	buf := bytesPool.Get(0, len(keys)*int(f.hashIterations)*8)
+	defer bytesPool.Put(buf)
+
+	indexes := f.indexes(keys, &buf.s)
+
 	args := make([]string, 0, len(indexes)+1)
 	args = append(args, indexes...)
 	args = append(args, f.hashIterationString)
@@ -353,7 +365,10 @@ func (f *countingBloomFilter) ItemMinCountMulti(ctx context.Context, keys []stri
 		return nil, nil
 	}
 
-	indexes := f.indexes(keys)
+	buf := bytesPool.Get(0, len(keys)*int(f.hashIterations)*8)
+	defer bytesPool.Put(buf)
+
+	indexes := f.indexes(keys, &buf.s)
 
 	resp := f.client.Do(
 		ctx,