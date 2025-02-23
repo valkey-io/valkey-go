redis/rueidis

add feature to cmd, give a choice to change execution from spool to auto pipeline (#773)
* add feature to cmd, when calling ToPipe(), change execution from spool to auto pipeline

* Add an example in Readme's Disable Auto Pipelining section, make some improvement in coding style

* add pipeTag to noRetTag

---------

Co-authored-by: zhaohuiliu <zhaohuiliu@tencent.com>

mux_test.go
@@ -233,6 +233,9 @@ func TestMuxReuseWire(t *testing.T) {
 		m, checkClean := setupMux([]*mockWire{
 			{
 				// leave first wire for pipeline calls
+				DoFn: func(cmd Completed) RedisResult {
+					return newResult(RedisMessage{typ: '+', string: "PIPELINED"}, nil)
+				},
 			},
 			{
 				DoFn: func(cmd Completed) RedisResult {
@@ -274,6 +277,13 @@ func TestMuxReuseWire(t *testing.T) {
 			t.Fatalf("unexpected response %v", val)
 		}
 
+		// this should use auto pipeline
+		if val, err := m.Do(context.Background(), cmds.NewCompleted([]string{"PING"}).ToPipe()).ToString(); err != nil {
+			t.Fatalf("unexpected error %v", err)
+		} else if val != "PIPELINED" {
+			t.Fatalf("unexpected response %v", val)
+		}
+
 		response <- newResult(RedisMessage{typ: '+', string: "BLOCK_RESPONSE"}, nil)
 		<-blocking
 	})
@@ -284,6 +294,9 @@ func TestMuxReuseWire(t *testing.T) {
 		m, checkClean := setupMux([]*mockWire{
 			{
 				// leave first wire for pipeline calls
+				DoMultiFn: func(cmd ...Completed) *redisresults {
+					return &redisresults{s: []RedisResult{newResult(RedisMessage{typ: '+', string: "PIPELINED"}, nil)}}
+				},
 			},
 			{
 				DoMultiFn: func(cmd ...Completed) *redisresults {
@@ -322,6 +335,13 @@ func TestMuxReuseWire(t *testing.T) {
 		if val, err := m.DoMulti(context.Background(), cmds.NewBlockingCompleted([]string{"PING"})).s[0].ToString(); err != nil {
 			t.Fatalf("unexpected error %v", err)
 		} else if val != "ACQUIRED" {
+			t.Fatalf("unexpected response %v", val)
+		}
+
+		// this should use auto pipeline
+		if val, err := m.DoMulti(context.Background(), cmds.NewCompleted([]string{"PING"}).ToPipe()).s[0].ToString(); err != nil {
+			t.Fatalf("unexpected error %v", err)
+		} else if val != "PIPELINED" {
 			t.Fatalf("unexpected response %v", val)
 		}
 

valkey-io/valkey-go

Signed-off-by: Rueian <rueiancsie@gmail.com>

mux_test.go
@@ -233,6 +233,9 @@ func TestMuxReuseWire(t *testing.T) {
 		m, checkClean := setupMux([]*mockWire{
 			{
 				// leave first wire for pipeline calls
+				DoFn: func(cmd Completed) ValkeyResult {
+					return newResult(ValkeyMessage{typ: '+', string: "PIPELINED"}, nil)
+				},
 			},
 			{
 				DoFn: func(cmd Completed) ValkeyResult {
@@ -274,6 +277,13 @@ func TestMuxReuseWire(t *testing.T) {
 			t.Fatalf("unexpected response %v", val)
 		}
 
+		// this should use auto pipeline
+		if val, err := m.Do(context.Background(), cmds.NewCompleted([]string{"PING"}).ToPipe()).ToString(); err != nil {
+			t.Fatalf("unexpected error %v", err)
+		} else if val != "PIPELINED" {
+			t.Fatalf("unexpected response %v", val)
+		}
+
 		response <- newResult(ValkeyMessage{typ: '+', string: "BLOCK_RESPONSE"}, nil)
 		<-blocking
 	})
@@ -284,6 +294,9 @@ func TestMuxReuseWire(t *testing.T) {
 		m, checkClean := setupMux([]*mockWire{
 			{
 				// leave first wire for pipeline calls
+				DoMultiFn: func(cmd ...Completed) *valkeyresults {
+					return &valkeyresults{s: []ValkeyResult{newResult(ValkeyMessage{typ: '+', string: "PIPELINED"}, nil)}}
+				},
 			},
 			{
 				DoMultiFn: func(cmd ...Completed) *valkeyresults {
@@ -322,6 +335,13 @@ func TestMuxReuseWire(t *testing.T) {
 		if val, err := m.DoMulti(context.Background(), cmds.NewBlockingCompleted([]string{"PING"})).s[0].ToString(); err != nil {
 			t.Fatalf("unexpected error %v", err)
 		} else if val != "ACQUIRED" {
+			t.Fatalf("unexpected response %v", val)
+		}
+
+		// this should use auto pipeline
+		if val, err := m.DoMulti(context.Background(), cmds.NewCompleted([]string{"PING"}).ToPipe()).s[0].ToString(); err != nil {
+			t.Fatalf("unexpected error %v", err)
+		} else if val != "PIPELINED" {
 			t.Fatalf("unexpected response %v", val)
 		}
 