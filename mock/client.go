redis/rueidis

feat: Add `Mode()` Method to `rueidis.Client` for Detecting Client Mode (#794)
* feat: client mode

* docs: add mode doc

Signed-off-by: Cheyu Wu <cheyu1220@gmail.com>

* chore: rm redundant comment

Signed-off-by: Cheyu Wu <cheyu1220@gmail.com>

* fix: client mode name

* fix: add missing implement func

* fix: wrong const variable name

* fix: rm mode from struct

Signed-off-by: Cheyu Wu <cheyu1220@gmail.com>

* test: add basic unit test

Signed-off-by: Cheyu Wu <cheyu1220@gmail.com>

---------

Signed-off-by: Cheyu Wu <cheyu1220@gmail.com>

mock/client.go
@@ -60,6 +60,10 @@ func (m *Client) B() rueidis.Builder {
 	return cmds.NewBuilder(m.slot)
 }
 
+func (m *Client) Mode() rueidis.ClientMode {
+	return rueidis.ClientModeStandalone
+}
+
 // Close mocks base method.
 func (m *Client) Close() {
 	m.ctrl.T.Helper()

rueidiscompat/tx.go
@@ -107,6 +107,10 @@ func (p *txproxy) Nodes() map[string]rueidis.Client {
 	panic("not implemented")
 }
 
+func (p *txproxy) Mode() rueidis.ClientMode {
+	panic("not implemented")
+}
+
 type Tx interface {
 	CoreCmdable
 	Watch(ctx context.Context, keys ...string) *StatusCmd

rueidishook/hook.go
@@ -82,6 +82,10 @@ func (c *hookclient) Nodes() map[string]rueidis.Client {
 	return nodes
 }
 
+func (c *hookclient) Mode() rueidis.ClientMode {
+	return c.client.Mode()
+}
+
 func (c *hookclient) Close() {
 	c.client.Close()
 }
@@ -148,6 +152,10 @@ func (e *extended) Dedicate() (client rueidis.DedicatedClient, cancel func()) {
 
 func (e *extended) Nodes() map[string]rueidis.Client {
 	panic("Nodes() is not allowed with rueidis.DedicatedClient")
+}
+
+func (e *extended) Mode() rueidis.ClientMode {
+	panic("Mode() is not allowed with rueidis.DedicatedClient")
 }
 
 type result struct {

rueidisotel/trace.go
@@ -203,6 +203,10 @@ func (o *otelclient) Nodes() map[string]rueidis.Client {
 	return nodes
 }
 
+func (o *otelclient) Mode() rueidis.ClientMode {
+	return o.client.Mode()
+}
+
 func (o *otelclient) Close() {
 	o.client.Close()
 }

valkey-io/valkey-go

Signed-off-by: Rueian <rueiancsie@gmail.com>

mock/client.go
@@ -60,6 +60,10 @@ func (m *Client) B() valkey.Builder {
 	return cmds.NewBuilder(m.slot)
 }
 
+func (m *Client) Mode() valkey.ClientMode {
+	return valkey.ClientModeStandalone
+}
+
 // Close mocks base method.
 func (m *Client) Close() {
 	m.ctrl.T.Helper()

valkeycompat/tx.go
@@ -107,6 +107,10 @@ func (p *txproxy) Nodes() map[string]valkey.Client {
 	panic("not implemented")
 }
 
+func (p *txproxy) Mode() valkey.ClientMode {
+	panic("not implemented")
+}
+
 type Tx interface {
 	CoreCmdable
 	Watch(ctx context.Context, keys ...string) *StatusCmd

valkeyhook/hook.go
@@ -82,6 +82,10 @@ func (c *hookclient) Nodes() map[string]valkey.Client {
 	return nodes
 }
 
+func (c *hookclient) Mode() valkey.ClientMode {
+	return c.client.Mode()
+}
+
 func (c *hookclient) Close() {
 	c.client.Close()
 }
@@ -148,6 +152,10 @@ func (e *extended) Dedicate() (client valkey.DedicatedClient, cancel func()) {
 
 func (e *extended) Nodes() map[string]valkey.Client {
 	panic("Nodes() is not allowed with valkey.DedicatedClient")
+}
+
+func (e *extended) Mode() valkey.ClientMode {
+	panic("Mode() is not allowed with valkey.DedicatedClient")
 }
 
 type result struct {

valkeyotel/trace.go
@@ -203,6 +203,10 @@ func (o *otelclient) Nodes() map[string]valkey.Client {
 	return nodes
 }
 
+func (o *otelclient) Mode() valkey.ClientMode {
+	return o.client.Mode()
+}
+
 func (o *otelclient) Close() {
 	o.client.Close()
 }