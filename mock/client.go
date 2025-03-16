redis/rueidis

feat: allow mocking Mode with the mock module
Signed-off-by: Rueian <rueiancsie@gmail.com>

mock/client.go
@@ -60,8 +60,18 @@ func (m *Client) B() rueidis.Builder {
 	return cmds.NewBuilder(m.slot)
 }
 
+// Mode mocks base method.
 func (m *Client) Mode() rueidis.ClientMode {
-	return rueidis.ClientModeStandalone
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "Mode")
+	ret0, _ := ret[0].(rueidis.ClientMode)
+	return ret0
+}
+
+// Mode indicates an expected call of Mode.
+func (mr *ClientMockRecorder) Mode() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Mode", reflect.TypeOf((*Client)(nil).Mode))
 }
 
 // Close mocks base method.

mock/client_test.go
@@ -18,6 +18,12 @@ func TestNewClient(t *testing.T) {
 	ctx := context.Background()
 	client := NewClient(ctrl)
 	{
+		client.EXPECT().Mode().Return(rueidis.ClientModeStandalone)
+		if mode := client.Mode(); mode != rueidis.ClientModeStandalone {
+			t.Fatalf("unexpected val %v", mode)
+		}
+	}
+	{
 		client.EXPECT().Do(ctx, Match("GET", "a")).Return(Result(RedisNil()))
 		if err := client.Do(ctx, client.B().Get().Key("a").Build()).Error(); !rueidis.IsRedisNil(err) {
 			t.Fatalf("unexpected err %v", err)

rueidishook/hook_test.go
@@ -78,6 +78,12 @@ func (w *wronghook) DoMultiStream(client rueidis.Client, ctx context.Context, mu
 func testHooked(t *testing.T, hooked rueidis.Client, mocked *mock.Client) {
 	ctx := context.Background()
 	{
+		mocked.EXPECT().Mode().Return(rueidis.ClientModeStandalone)
+		if mode := hooked.Mode(); mode != rueidis.ClientModeStandalone {
+			t.Fatalf("unexpected mode %v", mode)
+		}
+	}
+	{
 		mocked.EXPECT().Do(ctx, mock.Match("GET", "a")).Return(mock.Result(mock.RedisNil()))
 		if err := hooked.Do(ctx, hooked.B().Get().Key("a").Build()).Error(); !rueidis.IsRedisNil(err) {
 			t.Fatalf("unexpected err %v", err)
@@ -247,6 +253,12 @@ func TestForbiddenMethodForDedicatedClient(t *testing.T) {
 		fn  func(client rueidis.Client)
 		msg string
 	}{
+		{
+			fn: func(client rueidis.Client) {
+				client.Mode()
+			},
+			msg: "Mode() is not allowed with rueidis.DedicatedClient",
+		},
 		{
 			fn: func(client rueidis.Client) {
 				client.DoCache(context.Background(), client.B().Get().Key("").Cache(), time.Second)

rueidisotel/trace_test.go
@@ -114,6 +114,10 @@ func TestWithClient(t *testing.T) {
 func testWithClient(t *testing.T, client rueidis.Client, exp *tracetest.InMemoryExporter, mxp metric.Reader) {
 	ctx := context.Background()
 
+	if client.Mode() != rueidis.ClientModeStandalone {
+		t.Fatalf("client mode is not standalone")
+	}
+
 	// test empty trace
 	var emptyCompletedArr []rueidis.Completed
 	resps := client.DoMulti(ctx, emptyCompletedArr...)

valkey-io/valkey-go

mock/client.go
@@ -60,8 +60,18 @@ func (m *Client) B() valkey.Builder {
 	return cmds.NewBuilder(m.slot)
 }
 
+// Mode mocks base method.
 func (m *Client) Mode() valkey.ClientMode {
-	return valkey.ClientModeStandalone
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "Mode")
+	ret0, _ := ret[0].(valkey.ClientMode)
+	return ret0
+}
+
+// Mode indicates an expected call of Mode.
+func (mr *ClientMockRecorder) Mode() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Mode", reflect.TypeOf((*Client)(nil).Mode))
 }
 
 // Close mocks base method.

mock/client_test.go
@@ -18,6 +18,12 @@ func TestNewClient(t *testing.T) {
 	ctx := context.Background()
 	client := NewClient(ctrl)
 	{
+		client.EXPECT().Mode().Return(valkey.ClientModeStandalone)
+		if mode := client.Mode(); mode != valkey.ClientModeStandalone {
+			t.Fatalf("unexpected val %v", mode)
+		}
+	}
+	{
 		client.EXPECT().Do(ctx, Match("GET", "a")).Return(Result(ValkeyNil()))
 		if err := client.Do(ctx, client.B().Get().Key("a").Build()).Error(); !valkey.IsValkeyNil(err) {
 			t.Fatalf("unexpected err %v", err)

valkeyhook/hook_test.go
@@ -78,6 +78,12 @@ func (w *wronghook) DoMultiStream(client valkey.Client, ctx context.Context, mul
 func testHooked(t *testing.T, hooked valkey.Client, mocked *mock.Client) {
 	ctx := context.Background()
 	{
+		mocked.EXPECT().Mode().Return(valkey.ClientModeStandalone)
+		if mode := hooked.Mode(); mode != valkey.ClientModeStandalone {
+			t.Fatalf("unexpected mode %v", mode)
+		}
+	}
+	{
 		mocked.EXPECT().Do(ctx, mock.Match("GET", "a")).Return(mock.Result(mock.ValkeyNil()))
 		if err := hooked.Do(ctx, hooked.B().Get().Key("a").Build()).Error(); !valkey.IsValkeyNil(err) {
 			t.Fatalf("unexpected err %v", err)
@@ -247,6 +253,12 @@ func TestForbiddenMethodForDedicatedClient(t *testing.T) {
 		fn  func(client valkey.Client)
 		msg string
 	}{
+		{
+			fn: func(client valkey.Client) {
+				client.Mode()
+			},
+			msg: "Mode() is not allowed with valkey.DedicatedClient",
+		},
 		{
 			fn: func(client valkey.Client) {
 				client.DoCache(context.Background(), client.B().Get().Key("").Cache(), time.Second)

valkeyotel/trace_test.go
@@ -114,6 +114,10 @@ func TestWithClient(t *testing.T) {
 func testWithClient(t *testing.T, client valkey.Client, exp *tracetest.InMemoryExporter, mxp metric.Reader) {
 	ctx := context.Background()
 
+	if client.Mode() != valkey.ClientModeStandalone {
+		t.Fatalf("client mode is not standalone")
+	}
+
 	// test empty trace
 	var emptyCompletedArr []valkey.Completed
 	resps := client.DoMulti(ctx, emptyCompletedArr...)