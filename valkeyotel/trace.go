redis/rueidis

feat: emit metrics for command duration and errors (#826)

rueidisotel/trace.go
@@ -61,6 +61,32 @@ func WithDBStatement(f StatementFunc) Option {
 // StatementFunc is a the function that maps a command's tokens to a string to put in the db.statement attribute
 type StatementFunc func(cmdTokens []string) string
 
+type commandMetrics struct {
+	duration   metric.Float64Histogram
+	errors     metric.Int64Counter
+	addOpts    []metric.AddOption
+	recordOpts []metric.RecordOption
+	opAttr     bool
+}
+
+func (c *commandMetrics) recordDuration(ctx context.Context, op string, now time.Time) {
+	opts := c.recordOpts
+	if c.opAttr {
+		opts = append(c.recordOpts, metric.WithAttributeSet(attribute.NewSet(attribute.String("operation", op))))
+	}
+	c.duration.Record(ctx, time.Since(now).Seconds(), opts...)
+}
+
+func (c *commandMetrics) recordError(ctx context.Context, op string, err error) {
+	if err != nil && !rueidis.IsRedisNil(err) {
+		opts := c.addOpts
+		if c.opAttr {
+			opts = append(c.addOpts, metric.WithAttributeSet(attribute.NewSet(attribute.String("operation", op))))
+		}
+		c.errors.Add(ctx, 1, opts...)
+	}
+}
+
 type otelclient struct {
 	client          rueidis.Client
 	meterProvider   metric.MeterProvider
@@ -74,6 +100,7 @@ type otelclient struct {
 	addOpts         []metric.AddOption
 	recordOpts      []metric.RecordOption
 	histogramOption HistogramOption
+	commandMetrics
 }
 
 func (o *otelclient) B() rueidis.Builder {
@@ -81,43 +108,58 @@ func (o *otelclient) B() rueidis.Builder {
 }
 
 func (o *otelclient) Do(ctx context.Context, cmd rueidis.Completed) (resp rueidis.RedisResult) {
-	ctx, span := o.start(ctx, first(cmd.Commands()), sum(cmd.Commands()))
+	op := first(cmd.Commands())
+	defer o.recordDuration(ctx, op, time.Now())
+	ctx, span := o.start(ctx, op, sum(cmd.Commands()))
 	if o.dbStmtFunc != nil {
 		span.SetAttributes(dbstmt.String(o.dbStmtFunc(cmd.Commands())))
 	}
 
 	resp = o.client.Do(ctx, cmd)
 	o.end(span, resp.Error())
+	o.recordError(ctx, op, resp.Error())
 	return
 }
 
 func (o *otelclient) DoMulti(ctx context.Context, multi ...rueidis.Completed) (resp []rueidis.RedisResult) {
-	ctx, span := o.start(ctx, multiFirst(multi), multiSum(multi))
+	op := multiFirst(multi)
+	defer o.recordDuration(ctx, op, time.Now())
+	ctx, span := o.start(ctx, op, multiSum(multi))
 	resp = o.client.DoMulti(ctx, multi...)
-	o.end(span, firstError(resp))
+	err := firstError(resp)
+	o.end(span, err)
+	o.recordError(ctx, op, err)
 	return
 }
 
 func (o *otelclient) DoStream(ctx context.Context, cmd rueidis.Completed) (resp rueidis.RedisResultStream) {
-	ctx, span := o.start(ctx, first(cmd.Commands()), sum(cmd.Commands()))
+	op := first(cmd.Commands())
+	defer o.recordDuration(ctx, op, time.Now())
+	ctx, span := o.start(ctx, op, sum(cmd.Commands()))
 	if o.dbStmtFunc != nil {
 		span.SetAttributes(dbstmt.String(o.dbStmtFunc(cmd.Commands())))
 	}
 
 	resp = o.client.DoStream(ctx, cmd)
 	o.end(span, resp.Error())
+	o.recordError(ctx, op, resp.Error())
 	return
 }
 
 func (o *otelclient) DoMultiStream(ctx context.Context, multi ...rueidis.Completed) (resp rueidis.MultiRedisResultStream) {
-	ctx, span := o.start(ctx, multiFirst(multi), multiSum(multi))
+	op := multiFirst(multi)
+	defer o.recordDuration(ctx, op, time.Now())
+	ctx, span := o.start(ctx, op, multiSum(multi))
 	resp = o.client.DoMultiStream(ctx, multi...)
 	o.end(span, resp.Error())
+	o.recordError(ctx, op, resp.Error())
 	return
 }
 
 func (o *otelclient) DoCache(ctx context.Context, cmd rueidis.Cacheable, ttl time.Duration) (resp rueidis.RedisResult) {
-	ctx, span := o.start(ctx, first(cmd.Commands()), sum(cmd.Commands()))
+	op := first(cmd.Commands())
+	defer o.recordDuration(ctx, op, time.Now())
+	ctx, span := o.start(ctx, op, sum(cmd.Commands()))
 	if o.dbStmtFunc != nil {
 		span.SetAttributes(dbstmt.String(o.dbStmtFunc(cmd.Commands())))
 	}
@@ -131,11 +173,14 @@ func (o *otelclient) DoCache(ctx context.Context, cmd rueidis.Cacheable, ttl tim
 		}
 	}
 	o.end(span, resp.Error())
+	o.recordError(ctx, op, resp.Error())
 	return
 }
 
 func (o *otelclient) DoMultiCache(ctx context.Context, multi ...rueidis.CacheableTTL) (resps []rueidis.RedisResult) {
-	ctx, span := o.start(ctx, multiCacheableFirst(multi), multiCacheableSum(multi))
+	op := multiCacheableFirst(multi)
+	defer o.recordDuration(ctx, op, time.Now())
+	ctx, span := o.start(ctx, op, multiCacheableSum(multi))
 	resps = o.client.DoMultiCache(ctx, multi...)
 	for _, resp := range resps {
 		if resp.NonRedisError() == nil {
@@ -146,17 +191,20 @@ func (o *otelclient) DoMultiCache(ctx context.Context, multi ...rueidis.Cacheabl
 			}
 		}
 	}
-	o.end(span, firstError(resps))
+	err := firstError(resps)
+	o.end(span, err)
+	o.recordError(ctx, op, err)
 	return
 }
 
 func (o *otelclient) Dedicated(fn func(rueidis.DedicatedClient) error) (err error) {
 	return o.client.Dedicated(func(client rueidis.DedicatedClient) error {
 		return fn(&dedicated{
-			client:     client,
-			tAttrs:     o.tAttrs,
-			tracer:     o.tracer,
-			dbStmtFunc: o.dbStmtFunc,
+			client:         client,
+			tAttrs:         o.tAttrs,
+			tracer:         o.tracer,
+			dbStmtFunc:     o.dbStmtFunc,
+			commandMetrics: o.commandMetrics,
 		})
 	})
 }
@@ -164,10 +212,11 @@ func (o *otelclient) Dedicated(fn func(rueidis.DedicatedClient) error) (err erro
 func (o *otelclient) Dedicate() (rueidis.DedicatedClient, func()) {
 	client, cancel := o.client.Dedicate()
 	return &dedicated{
-		client:     client,
-		tAttrs:     o.tAttrs,
-		tracer:     o.tracer,
-		dbStmtFunc: o.dbStmtFunc,
+		client:         client,
+		tAttrs:         o.tAttrs,
+		tracer:         o.tracer,
+		dbStmtFunc:     o.dbStmtFunc,
+		commandMetrics: o.commandMetrics,
 	}, cancel
 }
 
@@ -198,6 +247,7 @@ func (o *otelclient) Nodes() map[string]rueidis.Client {
 			tAttrs:          o.tAttrs,
 			histogramOption: o.histogramOption,
 			dbStmtFunc:      o.dbStmtFunc,
+			commandMetrics:  o.commandMetrics,
 		}
 	}
 	return nodes
@@ -218,6 +268,7 @@ type dedicated struct {
 	tracer     trace.Tracer
 	tAttrs     trace.SpanStartEventOption
 	dbStmtFunc StatementFunc
+	commandMetrics
 }
 
 func (d *dedicated) B() rueidis.Builder {
@@ -225,20 +276,27 @@ func (d *dedicated) B() rueidis.Builder {
 }
 
 func (d *dedicated) Do(ctx context.Context, cmd rueidis.Completed) (resp rueidis.RedisResult) {
-	ctx, span := d.start(ctx, first(cmd.Commands()), sum(cmd.Commands()))
+	op := first(cmd.Commands())
+	defer d.recordDuration(ctx, op, time.Now())
+	ctx, span := d.start(ctx, op, sum(cmd.Commands()))
 	if d.dbStmtFunc != nil {
 		span.SetAttributes(dbstmt.String(d.dbStmtFunc(cmd.Commands())))
 	}
 
 	resp = d.client.Do(ctx, cmd)
 	d.end(span, resp.Error())
+	d.recordError(ctx, op, resp.Error())
 	return
 }
 
 func (d *dedicated) DoMulti(ctx context.Context, multi ...rueidis.Completed) (resp []rueidis.RedisResult) {
-	ctx, span := d.start(ctx, multiFirst(multi), multiSum(multi))
+	op := multiFirst(multi)
+	defer d.recordDuration(ctx, op, time.Now())
+	ctx, span := d.start(ctx, op, multiSum(multi))
 	resp = d.client.DoMulti(ctx, multi...)
-	d.end(span, firstError(resp))
+	err := firstError(resp)
+	d.end(span, err)
+	d.recordError(ctx, op, err)
 	return
 }
 

rueidisotel/trace_test.go
@@ -54,6 +54,7 @@ func (m *mockMeter) Int64UpDownCounter(name string, options ...metricapi.Int64Up
 	}
 	return nil, nil
 }
+
 func (m *mockMeter) Float64Histogram(name string, options ...metricapi.Float64HistogramOption) (metricapi.Float64Histogram, error) {
 	if m.testName == name {
 		return nil, fmt.Errorf("%w: %s", errMocked, m.testName)
@@ -272,6 +273,23 @@ func testWithClient(t *testing.T, client rueidis.Client, exp *tracetest.InMemory
 		client.DoMulti(ctx, client.B().Set().Key("key").Value("val").Build(), client.B().Set().Key("key").Value("val").Build())
 		validateTrace(t, exp, "SET SET", codes.Ok)
 	}
+
+	if err := mxp.Collect(ctx, &metrics); err != nil {
+		t.Fatalf("unexpected err %v", err)
+	}
+
+	validateMetrics(t, metrics, "rueidis_command_errors", 6)
+
+	validateMetricNumDataPoints(t, metrics, "rueidis_command_errors", 1)
+	validateMetricNumDataPoints(t, metrics, "rueidis_command_duration_seconds", 1)
+
+	duration := float64HistogramMetric(metrics, "rueidis_command_duration_seconds")
+	if duration == 0 {
+		t.Fatalf("rueidis_command_duration_seconds: got 0, expected > 0")
+	}
+
+	validateMetricHasNoAttribute(t, metrics, "rueidis_command_errors", "operation")
+	validateMetricHasNoAttribute(t, metrics, "rueidis_command_duration_seconds", "operation")
 }
 
 func validateTrace(t *testing.T, exp *tracetest.InMemoryExporter, op string, code codes.Code) {
@@ -341,6 +359,7 @@ func TestWithClientSimple(t *testing.T) {
 		WithTracerProvider(tracerProvider),
 		WithMeterProvider(meterProvider),
 		WithDBStatement(dbStmtFunc),
+		WithOperationMetricAttr(),
 	)
 	defer client.Close()
 
@@ -366,22 +385,99 @@ func TestWithClientSimple(t *testing.T) {
 	if !found {
 		t.Fatalf("expected attribute 'any: label' not found in span attributes")
 	}
+
+	metrics := metricdata.ResourceMetrics{}
+	if err := mxp.Collect(context.Background(), &metrics); err != nil {
+		t.Fatalf("unexpected err %v", err)
+	}
+
+	validateMetricNumDataPoints(t, metrics, "rueidis_command_duration_seconds", 1)
+	validateMetricHasAttributes(t, metrics, "rueidis_command_duration_seconds", "operation")
 }
 
 func validateMetrics(t *testing.T, metrics metricdata.ResourceMetrics, name string, value int64) {
-	for _, sm := range metrics.ScopeMetrics {
-		for _, m := range sm.Metrics {
-			if m.Name == name {
-				data := m.Data.(metricdata.Sum[int64])
-				metricdatatest.AssertHasAttributes(t, data, attribute.String("any", "label"))
-				if data.DataPoints[0].Value != value {
-					t.Fatalf("unexpected metric value %v", data.DataPoints[0].Value)
+	t.Helper()
+
+	data, ok := findMetric(metrics, name).(metricdata.Sum[int64])
+	if !ok {
+		t.Fatalf("metric %v not found", name)
+	}
+
+	metricdatatest.AssertHasAttributes(t, data, attribute.String("any", "label"))
+	var sum int64
+	for _, dp := range data.DataPoints {
+		sum += dp.Value
+	}
+	if sum != value {
+		t.Fatalf("unexpected value for %v: wanted %v, got %v", name, value, sum)
+	}
+}
+
+func validateMetricHasAttributes(t *testing.T, metrics metricdata.ResourceMetrics, name string, keys ...attribute.Key) {
+	t.Helper()
+
+	switch data := findMetric(metrics, name).(type) {
+	case metricdata.Sum[int64]:
+		for _, dp := range data.DataPoints {
+			for _, key := range keys {
+				_, ok := dp.Attributes.Value(key)
+				if !ok {
+					t.Fatalf("metric %v does not have attribute %v", name, key)
 				}
-				return
 			}
 		}
+	case metricdata.Histogram[float64]:
+		for _, dp := range data.DataPoints {
+			for _, key := range keys {
+				_, ok := dp.Attributes.Value(key)
+				if !ok {
+					t.Fatalf("metric %v does not have attribute %v", name, key)
+				}
+			}
+		}
+	default:
+		t.Fatalf("unexpected metric type for %v: %T", name, data)
 	}
-	t.Fatalf("metrics not found %v", name)
+}
+
+func validateMetricHasNoAttribute(t *testing.T, metrics metricdata.ResourceMetrics, name string, key attribute.Key) {
+	t.Helper()
+
+	switch data := findMetric(metrics, name).(type) {
+	case metricdata.Sum[int64]:
+		for _, dp := range data.DataPoints {
+			_, ok := dp.Attributes.Value(key)
+			if ok {
+				t.Fatalf("metric %v has attribute %v", name, key)
+			}
+		}
+	case metricdata.Histogram[float64]:
+		for _, dp := range data.DataPoints {
+			_, ok := dp.Attributes.Value(key)
+			if ok {
+				t.Fatalf("metric %v has attribute %v", name, key)
+			}
+		}
+	default:
+		t.Fatalf("unexpected metric type for %v: %T", name, data)
+	}
+}
+
+func validateMetricNumDataPoints(t *testing.T, metrics metricdata.ResourceMetrics, name string, num int) {
+	t.Helper()
+
+	switch data := findMetric(metrics, name).(type) {
+	case metricdata.Sum[int64]:
+		if len(data.DataPoints) != num {
+			t.Fatalf("unexpected number of data points for %v: got %d, expected %d", name, len(data.DataPoints), num)
+		}
+	case metricdata.Histogram[float64]:
+		if len(data.DataPoints) != num {
+			t.Fatalf("unexpected number of data points for %v: got %d, expected %d", name, len(data.DataPoints), num)
+		}
+	default:
+		t.Fatalf("unexpected metric type for %v: %T", name, data)
+	}
 }
 
 func ExampleWithClient_openTelemetry() {

valkey-io/valkey-go

Signed-off-by: Rueian <rueiancsie@gmail.com>

valkeyotel/trace.go
@@ -61,6 +61,32 @@ func WithDBStatement(f StatementFunc) Option {
 // StatementFunc is a the function that maps a command's tokens to a string to put in the db.statement attribute
 type StatementFunc func(cmdTokens []string) string
 
+type commandMetrics struct {
+	duration   metric.Float64Histogram
+	errors     metric.Int64Counter
+	addOpts    []metric.AddOption
+	recordOpts []metric.RecordOption
+	opAttr     bool
+}
+
+func (c *commandMetrics) recordDuration(ctx context.Context, op string, now time.Time) {
+	opts := c.recordOpts
+	if c.opAttr {
+		opts = append(c.recordOpts, metric.WithAttributeSet(attribute.NewSet(attribute.String("operation", op))))
+	}
+	c.duration.Record(ctx, time.Since(now).Seconds(), opts...)
+}
+
+func (c *commandMetrics) recordError(ctx context.Context, op string, err error) {
+	if err != nil && !valkey.IsValkeyNil(err) {
+		opts := c.addOpts
+		if c.opAttr {
+			opts = append(c.addOpts, metric.WithAttributeSet(attribute.NewSet(attribute.String("operation", op))))
+		}
+		c.errors.Add(ctx, 1, opts...)
+	}
+}
+
 type otelclient struct {
 	client          valkey.Client
 	meterProvider   metric.MeterProvider
@@ -74,6 +100,7 @@ type otelclient struct {
 	addOpts         []metric.AddOption
 	recordOpts      []metric.RecordOption
 	histogramOption HistogramOption
+	commandMetrics
 }
 
 func (o *otelclient) B() valkey.Builder {
@@ -81,43 +108,58 @@ func (o *otelclient) B() valkey.Builder {
 }
 
 func (o *otelclient) Do(ctx context.Context, cmd valkey.Completed) (resp valkey.ValkeyResult) {
-	ctx, span := o.start(ctx, first(cmd.Commands()), sum(cmd.Commands()))
+	op := first(cmd.Commands())
+	defer o.recordDuration(ctx, op, time.Now())
+	ctx, span := o.start(ctx, op, sum(cmd.Commands()))
 	if o.dbStmtFunc != nil {
 		span.SetAttributes(dbstmt.String(o.dbStmtFunc(cmd.Commands())))
 	}
 
 	resp = o.client.Do(ctx, cmd)
 	o.end(span, resp.Error())
+	o.recordError(ctx, op, resp.Error())
 	return
 }
 
 func (o *otelclient) DoMulti(ctx context.Context, multi ...valkey.Completed) (resp []valkey.ValkeyResult) {
-	ctx, span := o.start(ctx, multiFirst(multi), multiSum(multi))
+	op := multiFirst(multi)
+	defer o.recordDuration(ctx, op, time.Now())
+	ctx, span := o.start(ctx, op, multiSum(multi))
 	resp = o.client.DoMulti(ctx, multi...)
-	o.end(span, firstError(resp))
+	err := firstError(resp)
+	o.end(span, err)
+	o.recordError(ctx, op, err)
 	return
 }
 
 func (o *otelclient) DoStream(ctx context.Context, cmd valkey.Completed) (resp valkey.ValkeyResultStream) {
-	ctx, span := o.start(ctx, first(cmd.Commands()), sum(cmd.Commands()))
+	op := first(cmd.Commands())
+	defer o.recordDuration(ctx, op, time.Now())
+	ctx, span := o.start(ctx, op, sum(cmd.Commands()))
 	if o.dbStmtFunc != nil {
 		span.SetAttributes(dbstmt.String(o.dbStmtFunc(cmd.Commands())))
 	}
 
 	resp = o.client.DoStream(ctx, cmd)
 	o.end(span, resp.Error())
+	o.recordError(ctx, op, resp.Error())
 	return
 }
 
 func (o *otelclient) DoMultiStream(ctx context.Context, multi ...valkey.Completed) (resp valkey.MultiValkeyResultStream) {
-	ctx, span := o.start(ctx, multiFirst(multi), multiSum(multi))
+	op := multiFirst(multi)
+	defer o.recordDuration(ctx, op, time.Now())
+	ctx, span := o.start(ctx, op, multiSum(multi))
 	resp = o.client.DoMultiStream(ctx, multi...)
 	o.end(span, resp.Error())
+	o.recordError(ctx, op, resp.Error())
 	return
 }
 
 func (o *otelclient) DoCache(ctx context.Context, cmd valkey.Cacheable, ttl time.Duration) (resp valkey.ValkeyResult) {
-	ctx, span := o.start(ctx, first(cmd.Commands()), sum(cmd.Commands()))
+	op := first(cmd.Commands())
+	defer o.recordDuration(ctx, op, time.Now())
+	ctx, span := o.start(ctx, op, sum(cmd.Commands()))
 	if o.dbStmtFunc != nil {
 		span.SetAttributes(dbstmt.String(o.dbStmtFunc(cmd.Commands())))
 	}
@@ -131,11 +173,14 @@ func (o *otelclient) DoCache(ctx context.Context, cmd valkey.Cacheable, ttl time
 		}
 	}
 	o.end(span, resp.Error())
+	o.recordError(ctx, op, resp.Error())
 	return
 }
 
 func (o *otelclient) DoMultiCache(ctx context.Context, multi ...valkey.CacheableTTL) (resps []valkey.ValkeyResult) {
-	ctx, span := o.start(ctx, multiCacheableFirst(multi), multiCacheableSum(multi))
+	op := multiCacheableFirst(multi)
+	defer o.recordDuration(ctx, op, time.Now())
+	ctx, span := o.start(ctx, op, multiCacheableSum(multi))
 	resps = o.client.DoMultiCache(ctx, multi...)
 	for _, resp := range resps {
 		if resp.NonValkeyError() == nil {
@@ -146,17 +191,20 @@ func (o *otelclient) DoMultiCache(ctx context.Context, multi ...valkey.Cacheable
 			}
 		}
 	}
-	o.end(span, firstError(resps))
+	err := firstError(resps)
+	o.end(span, err)
+	o.recordError(ctx, op, err)
 	return
 }
 
 func (o *otelclient) Dedicated(fn func(valkey.DedicatedClient) error) (err error) {
 	return o.client.Dedicated(func(client valkey.DedicatedClient) error {
 		return fn(&dedicated{
-			client:     client,
-			tAttrs:     o.tAttrs,
-			tracer:     o.tracer,
-			dbStmtFunc: o.dbStmtFunc,
+			client:         client,
+			tAttrs:         o.tAttrs,
+			tracer:         o.tracer,
+			dbStmtFunc:     o.dbStmtFunc,
+			commandMetrics: o.commandMetrics,
 		})
 	})
 }
@@ -164,10 +212,11 @@ func (o *otelclient) Dedicated(fn func(valkey.DedicatedClient) error) (err error
 func (o *otelclient) Dedicate() (valkey.DedicatedClient, func()) {
 	client, cancel := o.client.Dedicate()
 	return &dedicated{
-		client:     client,
-		tAttrs:     o.tAttrs,
-		tracer:     o.tracer,
-		dbStmtFunc: o.dbStmtFunc,
+		client:         client,
+		tAttrs:         o.tAttrs,
+		tracer:         o.tracer,
+		dbStmtFunc:     o.dbStmtFunc,
+		commandMetrics: o.commandMetrics,
 	}, cancel
 }
 
@@ -198,6 +247,7 @@ func (o *otelclient) Nodes() map[string]valkey.Client {
 			tAttrs:          o.tAttrs,
 			histogramOption: o.histogramOption,
 			dbStmtFunc:      o.dbStmtFunc,
+			commandMetrics:  o.commandMetrics,
 		}
 	}
 	return nodes
@@ -218,6 +268,7 @@ type dedicated struct {
 	tracer     trace.Tracer
 	tAttrs     trace.SpanStartEventOption
 	dbStmtFunc StatementFunc
+	commandMetrics
 }
 
 func (d *dedicated) B() valkey.Builder {
@@ -225,20 +276,27 @@ func (d *dedicated) B() valkey.Builder {
 }
 
 func (d *dedicated) Do(ctx context.Context, cmd valkey.Completed) (resp valkey.ValkeyResult) {
-	ctx, span := d.start(ctx, first(cmd.Commands()), sum(cmd.Commands()))
+	op := first(cmd.Commands())
+	defer d.recordDuration(ctx, op, time.Now())
+	ctx, span := d.start(ctx, op, sum(cmd.Commands()))
 	if d.dbStmtFunc != nil {
 		span.SetAttributes(dbstmt.String(d.dbStmtFunc(cmd.Commands())))
 	}
 
 	resp = d.client.Do(ctx, cmd)
 	d.end(span, resp.Error())
+	d.recordError(ctx, op, resp.Error())
 	return
 }
 
 func (d *dedicated) DoMulti(ctx context.Context, multi ...valkey.Completed) (resp []valkey.ValkeyResult) {
-	ctx, span := d.start(ctx, multiFirst(multi), multiSum(multi))
+	op := multiFirst(multi)
+	defer d.recordDuration(ctx, op, time.Now())
+	ctx, span := d.start(ctx, op, multiSum(multi))
 	resp = d.client.DoMulti(ctx, multi...)
-	d.end(span, firstError(resp))
+	err := firstError(resp)
+	d.end(span, err)
+	d.recordError(ctx, op, err)
 	return
 }
 

valkeyotel/trace_test.go
@@ -54,6 +54,7 @@ func (m *mockMeter) Int64UpDownCounter(name string, options ...metricapi.Int64Up
 	}
 	return nil, nil
 }
+
 func (m *mockMeter) Float64Histogram(name string, options ...metricapi.Float64HistogramOption) (metricapi.Float64Histogram, error) {
 	if m.testName == name {
 		return nil, fmt.Errorf("%w: %s", errMocked, m.testName)
@@ -272,6 +273,23 @@ func testWithClient(t *testing.T, client valkey.Client, exp *tracetest.InMemoryE
 		client.DoMulti(ctx, client.B().Set().Key("key").Value("val").Build(), client.B().Set().Key("key").Value("val").Build())
 		validateTrace(t, exp, "SET SET", codes.Ok)
 	}
+
+	if err := mxp.Collect(ctx, &metrics); err != nil {
+		t.Fatalf("unexpected err %v", err)
+	}
+
+	validateMetrics(t, metrics, "valkey_command_errors", 6)
+
+	validateMetricNumDataPoints(t, metrics, "valkey_command_errors", 1)
+	validateMetricNumDataPoints(t, metrics, "valkey_command_duration_seconds", 1)
+
+	duration := float64HistogramMetric(metrics, "valkey_command_duration_seconds")
+	if duration == 0 {
+		t.Fatalf("valkey_command_duration_seconds: got 0, expected > 0")
+	}
+
+	validateMetricHasNoAttribute(t, metrics, "valkey_command_errors", "operation")
+	validateMetricHasNoAttribute(t, metrics, "valkey_command_duration_seconds", "operation")
 }
 
 func validateTrace(t *testing.T, exp *tracetest.InMemoryExporter, op string, code codes.Code) {
@@ -341,6 +359,7 @@ func TestWithClientSimple(t *testing.T) {
 		WithTracerProvider(tracerProvider),
 		WithMeterProvider(meterProvider),
 		WithDBStatement(dbStmtFunc),
+		WithOperationMetricAttr(),
 	)
 	defer client.Close()
 
@@ -366,22 +385,99 @@ func TestWithClientSimple(t *testing.T) {
 	if !found {
 		t.Fatalf("expected attribute 'any: label' not found in span attributes")
 	}
+
+	metrics := metricdata.ResourceMetrics{}
+	if err := mxp.Collect(context.Background(), &metrics); err != nil {
+		t.Fatalf("unexpected err %v", err)
+	}
+
+	validateMetricNumDataPoints(t, metrics, "valkey_command_duration_seconds", 1)
+	validateMetricHasAttributes(t, metrics, "valkey_command_duration_seconds", "operation")
 }
 
 func validateMetrics(t *testing.T, metrics metricdata.ResourceMetrics, name string, value int64) {
-	for _, sm := range metrics.ScopeMetrics {
-		for _, m := range sm.Metrics {
-			if m.Name == name {
-				data := m.Data.(metricdata.Sum[int64])
-				metricdatatest.AssertHasAttributes(t, data, attribute.String("any", "label"))
-				if data.DataPoints[0].Value != value {
-					t.Fatalf("unexpected metric value %v", data.DataPoints[0].Value)
+	t.Helper()
+
+	data, ok := findMetric(metrics, name).(metricdata.Sum[int64])
+	if !ok {
+		t.Fatalf("metric %v not found", name)
+	}
+
+	metricdatatest.AssertHasAttributes(t, data, attribute.String("any", "label"))
+	var sum int64
+	for _, dp := range data.DataPoints {
+		sum += dp.Value
+	}
+	if sum != value {
+		t.Fatalf("unexpected value for %v: wanted %v, got %v", name, value, sum)
+	}
+}
+
+func validateMetricHasAttributes(t *testing.T, metrics metricdata.ResourceMetrics, name string, keys ...attribute.Key) {
+	t.Helper()
+
+	switch data := findMetric(metrics, name).(type) {
+	case metricdata.Sum[int64]:
+		for _, dp := range data.DataPoints {
+			for _, key := range keys {
+				_, ok := dp.Attributes.Value(key)
+				if !ok {
+					t.Fatalf("metric %v does not have attribute %v", name, key)
 				}
-				return
 			}
 		}
+	case metricdata.Histogram[float64]:
+		for _, dp := range data.DataPoints {
+			for _, key := range keys {
+				_, ok := dp.Attributes.Value(key)
+				if !ok {
+					t.Fatalf("metric %v does not have attribute %v", name, key)
+				}
+			}
+		}
+	default:
+		t.Fatalf("unexpected metric type for %v: %T", name, data)
 	}
-	t.Fatalf("metrics not found %v", name)
+}
+
+func validateMetricHasNoAttribute(t *testing.T, metrics metricdata.ResourceMetrics, name string, key attribute.Key) {
+	t.Helper()
+
+	switch data := findMetric(metrics, name).(type) {
+	case metricdata.Sum[int64]:
+		for _, dp := range data.DataPoints {
+			_, ok := dp.Attributes.Value(key)
+			if ok {
+				t.Fatalf("metric %v has attribute %v", name, key)
+			}
+		}
+	case metricdata.Histogram[float64]:
+		for _, dp := range data.DataPoints {
+			_, ok := dp.Attributes.Value(key)
+			if ok {
+				t.Fatalf("metric %v has attribute %v", name, key)
+			}
+		}
+	default:
+		t.Fatalf("unexpected metric type for %v: %T", name, data)
+	}
+}
+
+func validateMetricNumDataPoints(t *testing.T, metrics metricdata.ResourceMetrics, name string, num int) {
+	t.Helper()
+
+	switch data := findMetric(metrics, name).(type) {
+	case metricdata.Sum[int64]:
+		if len(data.DataPoints) != num {
+			t.Fatalf("unexpected number of data points for %v: got %d, expected %d", name, len(data.DataPoints), num)
+		}
+	case metricdata.Histogram[float64]:
+		if len(data.DataPoints) != num {
+			t.Fatalf("unexpected number of data points for %v: got %d, expected %d", name, len(data.DataPoints), num)
+		}
+	default:
+		t.Fatalf("unexpected metric type for %v: %T", name, data)
+	}
 }
 
 func ExampleWithClient_openTelemetry() {