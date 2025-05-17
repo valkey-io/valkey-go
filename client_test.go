redis/rueidis

Check transaction block when using connection lifetime (#837)
* Check transaction block when using connection lifetime

* Add test of transaction block for connection expired

client_test.go
@@ -1498,6 +1498,95 @@ func TestSingleClientConnLifetime(t *testing.T) {
 		}
 	})
 
+	t.Run("DoMulti ConnLifetime Transaction Block", func(t *testing.T) {
+		client, m := setup()
+		attempts := 0
+		var orgMulti []Completed
+		m.DoMultiFn = func(multi ...Completed) *redisresults {
+			attempts++
+			switch attempts {
+			case 1: // errConnExpired at the head of processing
+				orgMulti = multi
+				return &redisresults{s: []RedisResult{newErrResult(errConnExpired), newErrResult(errConnExpired), newErrResult(errConnExpired), newErrResult(errConnExpired), newErrResult(errConnExpired), newErrResult(errConnExpired)}}
+			case 2: // errConnExpired at Multi Command
+				if len(multi) != 6 || !reflect.DeepEqual(multi[0].Commands(), orgMulti[0].Commands()) {
+					t.Fatalf("unexpected multi when errConnExpired occurred at the head of proccessing, %v", multi)
+				}
+				return &redisresults{s: []RedisResult{
+					newResult(strmsg('+', "1"), nil),
+					newErrResult(errConnExpired), newErrResult(errConnExpired), newErrResult(errConnExpired), newErrResult(errConnExpired), newErrResult(errConnExpired),
+				}}
+			case 3: // errConnExpired in the middle of transaction block
+				if len(multi) != 5 || !reflect.DeepEqual(multi[0].Commands(), orgMulti[1].Commands()) {
+					t.Fatalf("unexpected multi when errConnExpired occurred at Multi Command, %v", multi)
+				}
+				return &redisresults{s: []RedisResult{
+					newResult(strmsg('+', "OK"), nil),
+					newResult(strmsg('+', "QUEUE"), nil),
+					newErrResult(errConnExpired), newErrResult(errConnExpired), newErrResult(errConnExpired),
+				}}
+			case 4: // errConnExpired at Exec Command
+				if len(multi) != 5 || !reflect.DeepEqual(multi[0].Commands(), orgMulti[1].Commands()) {
+					t.Fatalf("unexpected multi when errConnExpired occurred in the middle of transaction block, %v", multi)
+				}
+				return &redisresults{s: []RedisResult{
+					newResult(strmsg('+', "OK"), nil),
+					newResult(strmsg('+', "QUEUE"), nil),
+					newResult(strmsg('+', "QUEUE"), nil),
+					newErrResult(errConnExpired), newErrResult(errConnExpired)}}
+			case 5: // errConnExpired at end of processing
+				if len(multi) != 5 || !reflect.DeepEqual(multi[0].Commands(), orgMulti[1].Commands()) {
+					t.Fatalf("unexpected multi when errConnExpired occurred at at Exec Command, %v", multi)
+				}
+				return &redisresults{s: []RedisResult{
+					newResult(strmsg('+', "OK"), nil),
+					newResult(strmsg('+', "QUEUE"), nil),
+					newResult(strmsg('+', "QUEUE"), nil),
+					newResult(slicemsg('*', []RedisMessage{
+						strmsg('+', "2"),
+						strmsg('+', "3"),
+					}), nil),
+					newErrResult(errConnExpired),
+				}}
+			case 6:
+				if len(multi) != 1 || !reflect.DeepEqual(multi[0].Commands(), orgMulti[5].Commands()) {
+					t.Fatalf("unexpected multi when errConnExpired occurred at end of processing, %v", multi)
+				}
+			}
+			return &redisresults{s: []RedisResult{newResult(strmsg('+', "4"), nil)}}
+		}
+		multi := []Completed{
+			client.B().Get().Key("1{t}").Build(),
+			client.B().Multi().Build(),
+			client.B().Get().Key("2{t}").Build(),
+			client.B().Get().Key("3{t}").Build(),
+			client.B().Exec().Build(),
+			client.B().Get().Key("4{t}").Build(),
+		}
+		resps := client.DoMulti(context.Background(), multi...)
+		if len(resps) != 6 {
+			t.Fatalf("unexpected response length %v", len(resps))
+		}
+		if v, err := resps[0].ToString(); err != nil || v != "1" {
+			t.Errorf("unexpected response %v %v", v, err)
+		}
+		if v, err := resps[1].ToString(); err != nil || v != "OK" {
+			t.Errorf("unexpected response %v %v", v, err)
+		}
+		if v, err := resps[2].ToString(); err != nil || v != "QUEUE" {
+			t.Errorf("unexpected response %v %v", v, err)
+		}
+		if v, err := resps[3].ToString(); err != nil || v != "QUEUE" {
+			t.Errorf("unexpected response %v %v", v, err)
+		}
+		if v, err := resps[4].AsStrSlice(); err != nil || !reflect.DeepEqual(v, []string{"2", "3"}) {
+			t.Errorf("unexpected response %v %v", v, err)
+		}
+		if v, err := resps[5].ToString(); err != nil || v != "4" {
+			t.Errorf("unexpected response %v %v", v, err)
+		}
+	})
+
 	t.Run("DoMultiCache", func(t *testing.T) {
 		client, m := setup()
 		m.DoMultiCacheFn = func(multi ...CacheableTTL) *redisresults {

valkey-io/valkey-go

Signed-off-by: Rueian <rueiancsie@gmail.com>

client_test.go
@@ -1498,6 +1498,95 @@ func TestSingleClientConnLifetime(t *testing.T) {
 		}
 	})
 
+	t.Run("DoMulti ConnLifetime Transaction Block", func(t *testing.T) {
+		client, m := setup()
+		attempts := 0
+		var orgMulti []Completed
+		m.DoMultiFn = func(multi ...Completed) *valkeyresults {
+			attempts++
+			switch attempts {
+			case 1: // errConnExpired at the head of processing
+				orgMulti = multi
+				return &valkeyresults{s: []ValkeyResult{newErrResult(errConnExpired), newErrResult(errConnExpired), newErrResult(errConnExpired), newErrResult(errConnExpired), newErrResult(errConnExpired), newErrResult(errConnExpired)}}
+			case 2: // errConnExpired at Multi Command
+				if len(multi) != 6 || !reflect.DeepEqual(multi[0].Commands(), orgMulti[0].Commands()) {
+					t.Fatalf("unexpected multi when errConnExpired occurred at the head of proccessing, %v", multi)
+				}
+				return &valkeyresults{s: []ValkeyResult{
+					newResult(strmsg('+', "1"), nil),
+					newErrResult(errConnExpired), newErrResult(errConnExpired), newErrResult(errConnExpired), newErrResult(errConnExpired), newErrResult(errConnExpired),
+				}}
+			case 3: // errConnExpired in the middle of transaction block
+				if len(multi) != 5 || !reflect.DeepEqual(multi[0].Commands(), orgMulti[1].Commands()) {
+					t.Fatalf("unexpected multi when errConnExpired occurred at Multi Command, %v", multi)
+				}
+				return &valkeyresults{s: []ValkeyResult{
+					newResult(strmsg('+', "OK"), nil),
+					newResult(strmsg('+', "QUEUE"), nil),
+					newErrResult(errConnExpired), newErrResult(errConnExpired), newErrResult(errConnExpired),
+				}}
+			case 4: // errConnExpired at Exec Command
+				if len(multi) != 5 || !reflect.DeepEqual(multi[0].Commands(), orgMulti[1].Commands()) {
+					t.Fatalf("unexpected multi when errConnExpired occurred in the middle of transaction block, %v", multi)
+				}
+				return &valkeyresults{s: []ValkeyResult{
+					newResult(strmsg('+', "OK"), nil),
+					newResult(strmsg('+', "QUEUE"), nil),
+					newResult(strmsg('+', "QUEUE"), nil),
+					newErrResult(errConnExpired), newErrResult(errConnExpired)}}
+			case 5: // errConnExpired at end of processing
+				if len(multi) != 5 || !reflect.DeepEqual(multi[0].Commands(), orgMulti[1].Commands()) {
+					t.Fatalf("unexpected multi when errConnExpired occurred at at Exec Command, %v", multi)
+				}
+				return &valkeyresults{s: []ValkeyResult{
+					newResult(strmsg('+', "OK"), nil),
+					newResult(strmsg('+', "QUEUE"), nil),
+					newResult(strmsg('+', "QUEUE"), nil),
+					newResult(slicemsg('*', []ValkeyMessage{
+						strmsg('+', "2"),
+						strmsg('+', "3"),
+					}), nil),
+					newErrResult(errConnExpired),
+				}}
+			case 6:
+				if len(multi) != 1 || !reflect.DeepEqual(multi[0].Commands(), orgMulti[5].Commands()) {
+					t.Fatalf("unexpected multi when errConnExpired occurred at end of processing, %v", multi)
+				}
+			}
+			return &valkeyresults{s: []ValkeyResult{newResult(strmsg('+', "4"), nil)}}
+		}
+		multi := []Completed{
+			client.B().Get().Key("1{t}").Build(),
+			client.B().Multi().Build(),
+			client.B().Get().Key("2{t}").Build(),
+			client.B().Get().Key("3{t}").Build(),
+			client.B().Exec().Build(),
+			client.B().Get().Key("4{t}").Build(),
+		}
+		resps := client.DoMulti(context.Background(), multi...)
+		if len(resps) != 6 {
+			t.Fatalf("unexpected response length %v", len(resps))
+		}
+		if v, err := resps[0].ToString(); err != nil || v != "1" {
+			t.Errorf("unexpected response %v %v", v, err)
+		}
+		if v, err := resps[1].ToString(); err != nil || v != "OK" {
+			t.Errorf("unexpected response %v %v", v, err)
+		}
+		if v, err := resps[2].ToString(); err != nil || v != "QUEUE" {
+			t.Errorf("unexpected response %v %v", v, err)
+		}
+		if v, err := resps[3].ToString(); err != nil || v != "QUEUE" {
+			t.Errorf("unexpected response %v %v", v, err)
+		}
+		if v, err := resps[4].AsStrSlice(); err != nil || !reflect.DeepEqual(v, []string{"2", "3"}) {
+			t.Errorf("unexpected response %v %v", v, err)
+		}
+		if v, err := resps[5].ToString(); err != nil || v != "4" {
+			t.Errorf("unexpected response %v %v", v, err)
+		}
+	})
+
 	t.Run("DoMultiCache", func(t *testing.T) {
 		client, m := setup()
 		m.DoMultiCacheFn = func(multi ...CacheableTTL) *valkeyresults {