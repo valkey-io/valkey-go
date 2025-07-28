b94a78513da689b780212fde0ea8eb131c0ec6dd
zeeshan-zomato <zeeshan.hussain@zomato.com>
2025-07-28 19:50:42 +0530

    fix(pipeline): clear placeholder pipeline error after execution (#61)
    Fixes issue where commands would retain "pipeline has not been executed"
    error even after successful execution by properly setting the error state.
    
    Signed-off-by: Zeeshan Hussain <zeeshan.hussain@zomato.com>
    Signed-off-by: SoulPancake <angbpy@gmail.com>

--- valkeycompat/pipeline.go
+++ valkeycompat/pipeline.go
@@ -3210,6 +3210,7 @@ func (c *Pipeline) Exec(ctx context.Context) ([]Cmder, error) {
 		if r.NonValkeyError() != nil {
 			err = r.NonValkeyError()
 		}
+		rets[i].SetErr(nil)
 		rets[i].from(r)
 	}
 
