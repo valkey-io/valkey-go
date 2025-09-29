redis/rueidis

feat: add new queue which is context aware (#896)
tiny improvement about [the
issue](https://github.com/redis/rueidis/issues/858)

This PR doesn't solve above issue. To solve the issue, I think rewriting
ring buffer is needed. But That's quite huge.

This PR improves two things.
1. Do not use command if context is done.
2. do not make waiting coroutine if context is done.

I think this tiny improvement is helpful for very rare situation, that
buffer is full and lots of requests are waiting other requests.

---------

Signed-off-by: Rueian <rueiancsie@gmail.com>
Co-authored-by: Rueian <rueiancsie@gmail.com>

flowbuffer.go
@@ -1,0 +1,100 @@
+package rueidis
+
+import (
+	"context"
+)
+
+type queuedCmd struct {
+	ch    chan RedisResult
+	one   Completed
+	multi []Completed
+	resps []RedisResult
+}
+
+type flowBuffer struct {
+	f chan queuedCmd
+	r chan queuedCmd
+	w chan queuedCmd
+	c *chan RedisResult
+}
+
+var _ queue = (*flowBuffer)(nil)
+
+func newFlowBuffer(factor int) *flowBuffer {
+	if factor <= 0 {
+		factor = DefaultRingScale
+	}
+	size := 2 << (factor - 1)
+
+	r := &flowBuffer{
+		f: make(chan queuedCmd, size),
+		r: make(chan queuedCmd, size),
+		w: make(chan queuedCmd, size),
+	}
+	for i := 0; i < size; i++ {
+		r.f <- queuedCmd{
+			ch: make(chan RedisResult),
+		}
+	}
+	return r
+}
+
+func (b *flowBuffer) PutOne(ctx context.Context, m Completed) (chan RedisResult, error) {
+	select {
+	case cmd := <-b.f:
+		cmd.one = m
+		b.w <- cmd
+		return cmd.ch, nil
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	}
+}
+
+func (b *flowBuffer) PutMulti(ctx context.Context, m []Completed, resps []RedisResult) (chan RedisResult, error) {
+	select {
+	case cmd := <-b.f:
+		cmd.multi, cmd.resps = m, resps
+		b.w <- cmd
+		return cmd.ch, nil
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	}
+}
+
+// NextWriteCmd should be only called by one dedicated thread
+func (b *flowBuffer) NextWriteCmd() (one Completed, multi []Completed, ch chan RedisResult) {
+	select {
+	case cmd := <-b.w:
+		one, multi, ch = cmd.one, cmd.multi, cmd.ch
+		b.r <- cmd
+	default:
+	}
+	return
+}
+
+// WaitForWrite should be only called by one dedicated thread
+func (b *flowBuffer) WaitForWrite() (one Completed, multi []Completed, ch chan RedisResult) {
+	cmd := <-b.w
+	one, multi, ch = cmd.one, cmd.multi, cmd.ch
+	b.r <- cmd
+	return
+}
+
+// NextResultCh should be only called by one dedicated thread
+func (b *flowBuffer) NextResultCh() (one Completed, multi []Completed, ch chan RedisResult, resps []RedisResult) {
+	select {
+	case cmd := <-b.r:
+		b.c = &cmd.ch
+		one, multi, ch, resps = cmd.one, cmd.multi, cmd.ch, cmd.resps
+	default:
+	}
+	return
+}
+
+// FinishResult should be only called by one dedicated thread
+func (b *flowBuffer) FinishResult() {
+	if b.c != nil {
+		b.f <- queuedCmd{ch: *b.c}
+		b.c = nil
+	}
+}

flowbuffer_test.go
@@ -1,0 +1,185 @@
+package rueidis
+
+import (
+	"context"
+	"runtime"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/redis/rueidis/internal/cmds"
+)
+
+//gocyclo:ignore
+func TestFlowBuffer(t *testing.T) {
+	defer ShouldNotLeak(SetupLeakDetection())
+	t.Run("PutOne", func(t *testing.T) {
+		buffer := newFlowBuffer(DefaultRingScale)
+		size := 5000
+		fixture := make(map[string]struct{}, size)
+		for i := 0; i < size; i++ {
+			fixture[strconv.Itoa(i)] = struct{}{}
+		}
+
+		for cmd := range fixture {
+			go buffer.PutOne(context.Background(), cmds.NewCompleted([]string{cmd}))
+		}
+
+		for len(fixture) != 0 {
+			cmd1, _, _ := buffer.NextWriteCmd()
+			if cmd1.IsEmpty() {
+				runtime.Gosched()
+				continue
+			}
+			cmd2, _, ch, _ := buffer.NextResultCh()
+			buffer.FinishResult()
+			if cmd1.Commands()[0] != cmd2.Commands()[0] {
+				t.Fatalf("cmds read by NextWriteCmd and NextResultCh is not the same one")
+			}
+			if ch == nil || len(ch) != 0 {
+				t.Fatalf("channel from NextResultCh is broken")
+			}
+			delete(fixture, cmd1.Commands()[0])
+		}
+	})
+
+	t.Run("PutMulti", func(t *testing.T) {
+		buffer := newFlowBuffer(DefaultRingScale)
+		size := 5000
+		fixture := make(map[string]struct{}, size)
+		for i := 0; i < size; i++ {
+			fixture[strconv.Itoa(i)] = struct{}{}
+		}
+
+		base := [][]string{{"a"}, {"b"}, {"c"}, {"d"}}
+		for cmd := range fixture {
+			go buffer.PutMulti(context.Background(), cmds.NewMultiCompleted(append([][]string{{cmd}}, base...)), nil)
+		}
+
+		for len(fixture) != 0 {
+			_, cmd1, _ := buffer.NextWriteCmd()
+			if cmd1 == nil {
+				runtime.Gosched()
+				continue
+			}
+			_, cmd2, ch, _ := buffer.NextResultCh()
+			buffer.FinishResult()
+			for j := 0; j < len(cmd1); j++ {
+				if cmd1[j].Commands()[0] != cmd2[j].Commands()[0] {
+					t.Fatalf("cmds read by NextWriteCmd and NextResultCh is not the same one")
+				}
+			}
+			if ch == nil || len(ch) != 0 {
+				t.Fatalf("channel from NextResultCh is broken")
+			}
+			delete(fixture, cmd1[0].Commands()[0])
+		}
+	})
+
+	t.Run("NextWriteCmd & NextResultCh", func(t *testing.T) {
+		buffer := newFlowBuffer(DefaultRingScale)
+		if one, multi, _ := buffer.NextWriteCmd(); !one.IsEmpty() || multi != nil {
+			t.Fatalf("NextWriteCmd should returns nil if empty")
+		}
+		one, multi, ch, _ := buffer.NextResultCh()
+		if !one.IsEmpty() || multi != nil || ch != nil {
+			t.Fatalf("NextResultCh should returns nil if not NextWriteCmd yet")
+		}
+
+		buffer.PutOne(context.Background(), cmds.NewCompleted([]string{"0"}))
+		if one, _, _ := buffer.NextWriteCmd(); len(one.Commands()) == 0 || one.Commands()[0] != "0" {
+			t.Fatalf("NextWriteCmd should returns next cmd")
+		}
+		one, multi, ch, _ = buffer.NextResultCh()
+		if len(one.Commands()) == 0 || one.Commands()[0] != "0" || ch == nil {
+			t.Fatalf("NextResultCh should returns next cmd after NextWriteCmd")
+		} else {
+			buffer.FinishResult()
+		}
+
+		buffer.PutMulti(context.Background(), cmds.NewMultiCompleted([][]string{{"0"}}), nil)
+		if _, multi, _ := buffer.NextWriteCmd(); len(multi) == 0 || multi[0].Commands()[0] != "0" {
+			t.Fatalf("NextWriteCmd should returns next cmd")
+		}
+		_, multi, ch, _ = buffer.NextResultCh()
+		if len(multi) == 0 || multi[0].Commands()[0] != "0" || ch == nil {
+			t.Fatalf("NextResultCh should returns next cmd after NextWriteCmd")
+		} else {
+			buffer.FinishResult()
+		}
+	})
+
+	t.Run("PutOne Wakeup WaitForWrite", func(t *testing.T) {
+		buffer := newFlowBuffer(DefaultRingScale)
+		if one, _, ch := buffer.NextWriteCmd(); ch == nil {
+			go func() {
+				time.Sleep(time.Millisecond * 100)
+				buffer.PutOne(context.Background(), cmds.PingCmd)
+			}()
+			if one, _, ch = buffer.WaitForWrite(); ch != nil && one.Commands()[0] == cmds.PingCmd.Commands()[0] {
+				return
+			}
+		}
+		t.Fatal("Should sleep")
+	})
+
+	t.Run("PutMulti Wakeup WaitForWrite", func(t *testing.T) {
+		buffer := newFlowBuffer(DefaultRingScale)
+		if _, _, ch := buffer.NextWriteCmd(); ch == nil {
+			go func() {
+				time.Sleep(time.Millisecond * 100)
+				buffer.PutMulti(context.Background(), []Completed{cmds.PingCmd}, nil)
+			}()
+			if _, multi, ch := buffer.WaitForWrite(); ch != nil && multi[0].Commands()[0] == cmds.PingCmd.Commands()[0] {
+				return
+			}
+		}
+		t.Fatal("Should sleep")
+	})
+
+	t.Run("PutOne Context Is Done", func(t *testing.T) {
+		buffer := newFlowBuffer(1)
+		for i := 0; i < (1 << 1); i++ {
+			buffer.PutOne(context.Background(), cmds.NewCompleted([]string{strconv.Itoa(i)}))
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+		defer cancel()
+
+		_, err := buffer.PutOne(ctx, cmds.NewCompleted([]string{"should_fail"}))
+		if err != context.DeadlineExceeded {
+			t.Fatalf("Expected context.DeadlineExceeded error, got %v", err)
+		}
+
+		for i := 0; i < (1 << 1); i++ {
+			buffer.NextWriteCmd()
+		}
+		for i := 0; i < (1 << 1); i++ {
+			buffer.NextResultCh()
+			buffer.FinishResult()
+		}
+	})
+
+	t.Run("PutMulti Context Is Done", func(t *testing.T) {
+		buffer := newFlowBuffer(1)
+		for i := 0; i < (1 << 1); i++ {
+			buffer.PutMulti(context.Background(), cmds.NewMultiCompleted([][]string{{strconv.Itoa(i)}}), nil)
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+		defer cancel()
+
+		_, err := buffer.PutMulti(ctx, cmds.NewMultiCompleted([][]string{{"should_fail"}}), nil)
+		if err != context.DeadlineExceeded {
+			t.Fatalf("Expected context.Canceled error, got %v", err)
+		}
+
+		for i := 0; i < (1 << 1); i++ {
+			buffer.NextWriteCmd()
+		}
+		for i := 0; i < (1 << 1); i++ {
+			buffer.NextResultCh()
+			buffer.FinishResult()
+		}
+	})
+}

ring.go
@@ -1,6 +1,7 @@
 package rueidis
 
 import (
+	"context"
 	"sync"
 	"sync/atomic"
 
@@ -8,11 +9,12 @@ import (
 )
 
 type queue interface {
-	PutOne(m Completed) chan RedisResult
-	PutMulti(m []Completed, resps []RedisResult) chan RedisResult
+	PutOne(ctx context.Context, m Completed) (chan RedisResult, error)
+	PutMulti(ctx context.Context, m []Completed, resps []RedisResult) (chan RedisResult, error)
 	NextWriteCmd() (Completed, []Completed, chan RedisResult)
 	WaitForWrite() (Completed, []Completed, chan RedisResult)
-	NextResultCh() (Completed, []Completed, chan RedisResult, []RedisResult, *sync.Cond)
+	NextResultCh() (Completed, []Completed, chan RedisResult, []RedisResult)
+	FinishResult()
 }
 
 var _ queue = (*ring)(nil)
@@ -33,6 +35,7 @@ func newRing(factor int) *ring {
 }
 
 type ring struct {
+	resc  *sync.Cond
 	store []node // store's size must be 2^N to work with the mask
 	_     cpu.CacheLinePad
 	write uint32
@@ -53,7 +56,7 @@ type node struct {
 	slept bool
 }
 
-func (r *ring) PutOne(m Completed) chan RedisResult {
+func (r *ring) PutOne(_ context.Context, m Completed) (chan RedisResult, error) {
 	n := &r.store[atomic.AddUint32(&r.write, 1)&r.mask]
 	n.c1.L.Lock()
 	for n.mark != 0 {
@@ -66,10 +69,10 @@ func (r *ring) PutOne(m Completed) chan RedisResult {
 	if s {
 		n.c2.Broadcast()
 	}
-	return n.ch
+	return n.ch, nil
 }
 
-func (r *ring) PutMulti(m []Completed, resps []RedisResult) chan RedisResult {
+func (r *ring) PutMulti(_ context.Context, m []Completed, resps []RedisResult) (chan RedisResult, error) {
 	n := &r.store[atomic.AddUint32(&r.write, 1)&r.mask]
 	n.c1.L.Lock()
 	for n.mark != 0 {
@@ -83,7 +86,7 @@ func (r *ring) PutMulti(m []Completed, resps []RedisResult) chan RedisResult {
 	if s {
 		n.c2.Broadcast()
 	}
-	return n.ch
+	return n.ch, nil
 }
 
 // NextWriteCmd should be only called by one dedicated thread
@@ -120,11 +123,11 @@ func (r *ring) WaitForWrite() (one Completed, multi []Completed, ch chan RedisRe
 }
 
 // NextResultCh should be only called by one dedicated thread
-func (r *ring) NextResultCh() (one Completed, multi []Completed, ch chan RedisResult, resps []RedisResult, cond *sync.Cond) {
+func (r *ring) NextResultCh() (one Completed, multi []Completed, ch chan RedisResult, resps []RedisResult) {
 	r.read2++
 	p := r.read2 & r.mask
 	n := &r.store[p]
-	cond = n.c1
+	r.resc = n.c1
 	n.c1.L.Lock()
 	if n.mark == 2 {
 		one, multi, ch, resps = n.one, n.multi, n.ch, n.resps
@@ -136,4 +139,13 @@ func (r *ring) NextResultCh() (one Completed, multi []Completed, ch chan RedisRe
 		r.read2--
 	}
 	return
+}
+
+// FinishResult should be only called by one dedicated thread
+func (r *ring) FinishResult() {
+	if r.resc != nil {
+		r.resc.L.Unlock()
+		r.resc.Signal()
+		r.resc = nil
+	}
 }

valkey-io/valkey-go

Signed-off-by: SoulPancake <angbpy@gmail.com>

flowbuffer.go
@@ -1,0 +1,100 @@
+package valkey
+
+import (
+	"context"
+)
+
+type queuedCmd struct {
+	ch    chan ValkeyResult
+	one   Completed
+	multi []Completed
+	resps []ValkeyResult
+}
+
+type flowBuffer struct {
+	f chan queuedCmd
+	r chan queuedCmd
+	w chan queuedCmd
+	c *chan ValkeyResult
+}
+
+var _ queue = (*flowBuffer)(nil)
+
+func newFlowBuffer(factor int) *flowBuffer {
+	if factor <= 0 {
+		factor = DefaultRingScale
+	}
+	size := 2 << (factor - 1)
+
+	r := &flowBuffer{
+		f: make(chan queuedCmd, size),
+		r: make(chan queuedCmd, size),
+		w: make(chan queuedCmd, size),
+	}
+	for i := 0; i < size; i++ {
+		r.f <- queuedCmd{
+			ch: make(chan ValkeyResult),
+		}
+	}
+	return r
+}
+
+func (b *flowBuffer) PutOne(ctx context.Context, m Completed) (chan ValkeyResult, error) {
+	select {
+	case cmd := <-b.f:
+		cmd.one = m
+		b.w <- cmd
+		return cmd.ch, nil
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	}
+}
+
+func (b *flowBuffer) PutMulti(ctx context.Context, m []Completed, resps []ValkeyResult) (chan ValkeyResult, error) {
+	select {
+	case cmd := <-b.f:
+		cmd.multi, cmd.resps = m, resps
+		b.w <- cmd
+		return cmd.ch, nil
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	}
+}
+
+// NextWriteCmd should be only called by one dedicated thread
+func (b *flowBuffer) NextWriteCmd() (one Completed, multi []Completed, ch chan ValkeyResult) {
+	select {
+	case cmd := <-b.w:
+		one, multi, ch = cmd.one, cmd.multi, cmd.ch
+		b.r <- cmd
+	default:
+	}
+	return
+}
+
+// WaitForWrite should be only called by one dedicated thread
+func (b *flowBuffer) WaitForWrite() (one Completed, multi []Completed, ch chan ValkeyResult) {
+	cmd := <-b.w
+	one, multi, ch = cmd.one, cmd.multi, cmd.ch
+	b.r <- cmd
+	return
+}
+
+// NextResultCh should be only called by one dedicated thread
+func (b *flowBuffer) NextResultCh() (one Completed, multi []Completed, ch chan ValkeyResult, resps []ValkeyResult) {
+	select {
+	case cmd := <-b.r:
+		b.c = &cmd.ch
+		one, multi, ch, resps = cmd.one, cmd.multi, cmd.ch, cmd.resps
+	default:
+	}
+	return
+}
+
+// FinishResult should be only called by one dedicated thread
+func (b *flowBuffer) FinishResult() {
+	if b.c != nil {
+		b.f <- queuedCmd{ch: *b.c}
+		b.c = nil
+	}
+}

flowbuffer_test.go
@@ -1,0 +1,185 @@
+package valkey
+
+import (
+	"context"
+	"runtime"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/valkey-io/valkey-go/internal/cmds"
+)
+
+//gocyclo:ignore
+func TestFlowBuffer(t *testing.T) {
+	defer ShouldNotLeak(SetupLeakDetection())
+	t.Run("PutOne", func(t *testing.T) {
+		buffer := newFlowBuffer(DefaultRingScale)
+		size := 5000
+		fixture := make(map[string]struct{}, size)
+		for i := 0; i < size; i++ {
+			fixture[strconv.Itoa(i)] = struct{}{}
+		}
+
+		for cmd := range fixture {
+			go buffer.PutOne(context.Background(), cmds.NewCompleted([]string{cmd}))
+		}
+
+		for len(fixture) != 0 {
+			cmd1, _, _ := buffer.NextWriteCmd()
+			if cmd1.IsEmpty() {
+				runtime.Gosched()
+				continue
+			}
+			cmd2, _, ch, _ := buffer.NextResultCh()
+			buffer.FinishResult()
+			if cmd1.Commands()[0] != cmd2.Commands()[0] {
+				t.Fatalf("cmds read by NextWriteCmd and NextResultCh is not the same one")
+			}
+			if ch == nil || len(ch) != 0 {
+				t.Fatalf("channel from NextResultCh is broken")
+			}
+			delete(fixture, cmd1.Commands()[0])
+		}
+	})
+
+	t.Run("PutMulti", func(t *testing.T) {
+		buffer := newFlowBuffer(DefaultRingScale)
+		size := 5000
+		fixture := make(map[string]struct{}, size)
+		for i := 0; i < size; i++ {
+			fixture[strconv.Itoa(i)] = struct{}{}
+		}
+
+		base := [][]string{{"a"}, {"b"}, {"c"}, {"d"}}
+		for cmd := range fixture {
+			go buffer.PutMulti(context.Background(), cmds.NewMultiCompleted(append([][]string{{cmd}}, base...)), nil)
+		}
+
+		for len(fixture) != 0 {
+			_, cmd1, _ := buffer.NextWriteCmd()
+			if cmd1 == nil {
+				runtime.Gosched()
+				continue
+			}
+			_, cmd2, ch, _ := buffer.NextResultCh()
+			buffer.FinishResult()
+			for j := 0; j < len(cmd1); j++ {
+				if cmd1[j].Commands()[0] != cmd2[j].Commands()[0] {
+					t.Fatalf("cmds read by NextWriteCmd and NextResultCh is not the same one")
+				}
+			}
+			if ch == nil || len(ch) != 0 {
+				t.Fatalf("channel from NextResultCh is broken")
+			}
+			delete(fixture, cmd1[0].Commands()[0])
+		}
+	})
+
+	t.Run("NextWriteCmd & NextResultCh", func(t *testing.T) {
+		buffer := newFlowBuffer(DefaultRingScale)
+		if one, multi, _ := buffer.NextWriteCmd(); !one.IsEmpty() || multi != nil {
+			t.Fatalf("NextWriteCmd should returns nil if empty")
+		}
+		one, multi, ch, _ := buffer.NextResultCh()
+		if !one.IsEmpty() || multi != nil || ch != nil {
+			t.Fatalf("NextResultCh should returns nil if not NextWriteCmd yet")
+		}
+
+		buffer.PutOne(context.Background(), cmds.NewCompleted([]string{"0"}))
+		if one, _, _ := buffer.NextWriteCmd(); len(one.Commands()) == 0 || one.Commands()[0] != "0" {
+			t.Fatalf("NextWriteCmd should returns next cmd")
+		}
+		one, multi, ch, _ = buffer.NextResultCh()
+		if len(one.Commands()) == 0 || one.Commands()[0] != "0" || ch == nil {
+			t.Fatalf("NextResultCh should returns next cmd after NextWriteCmd")
+		} else {
+			buffer.FinishResult()
+		}
+
+		buffer.PutMulti(context.Background(), cmds.NewMultiCompleted([][]string{{"0"}}), nil)
+		if _, multi, _ := buffer.NextWriteCmd(); len(multi) == 0 || multi[0].Commands()[0] != "0" {
+			t.Fatalf("NextWriteCmd should returns next cmd")
+		}
+		_, multi, ch, _ = buffer.NextResultCh()
+		if len(multi) == 0 || multi[0].Commands()[0] != "0" || ch == nil {
+			t.Fatalf("NextResultCh should returns next cmd after NextWriteCmd")
+		} else {
+			buffer.FinishResult()
+		}
+	})
+
+	t.Run("PutOne Wakeup WaitForWrite", func(t *testing.T) {
+		buffer := newFlowBuffer(DefaultRingScale)
+		if one, _, ch := buffer.NextWriteCmd(); ch == nil {
+			go func() {
+				time.Sleep(time.Millisecond * 100)
+				buffer.PutOne(context.Background(), cmds.PingCmd)
+			}()
+			if one, _, ch = buffer.WaitForWrite(); ch != nil && one.Commands()[0] == cmds.PingCmd.Commands()[0] {
+				return
+			}
+		}
+		t.Fatal("Should sleep")
+	})
+
+	t.Run("PutMulti Wakeup WaitForWrite", func(t *testing.T) {
+		buffer := newFlowBuffer(DefaultRingScale)
+		if _, _, ch := buffer.NextWriteCmd(); ch == nil {
+			go func() {
+				time.Sleep(time.Millisecond * 100)
+				buffer.PutMulti(context.Background(), []Completed{cmds.PingCmd}, nil)
+			}()
+			if _, multi, ch := buffer.WaitForWrite(); ch != nil && multi[0].Commands()[0] == cmds.PingCmd.Commands()[0] {
+				return
+			}
+		}
+		t.Fatal("Should sleep")
+	})
+
+	t.Run("PutOne Context Is Done", func(t *testing.T) {
+		buffer := newFlowBuffer(1)
+		for i := 0; i < (1 << 1); i++ {
+			buffer.PutOne(context.Background(), cmds.NewCompleted([]string{strconv.Itoa(i)}))
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+		defer cancel()
+
+		_, err := buffer.PutOne(ctx, cmds.NewCompleted([]string{"should_fail"}))
+		if err != context.DeadlineExceeded {
+			t.Fatalf("Expected context.DeadlineExceeded error, got %v", err)
+		}
+
+		for i := 0; i < (1 << 1); i++ {
+			buffer.NextWriteCmd()
+		}
+		for i := 0; i < (1 << 1); i++ {
+			buffer.NextResultCh()
+			buffer.FinishResult()
+		}
+	})
+
+	t.Run("PutMulti Context Is Done", func(t *testing.T) {
+		buffer := newFlowBuffer(1)
+		for i := 0; i < (1 << 1); i++ {
+			buffer.PutMulti(context.Background(), cmds.NewMultiCompleted([][]string{{strconv.Itoa(i)}}), nil)
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+		defer cancel()
+
+		_, err := buffer.PutMulti(ctx, cmds.NewMultiCompleted([][]string{{"should_fail"}}), nil)
+		if err != context.DeadlineExceeded {
+			t.Fatalf("Expected context.Canceled error, got %v", err)
+		}
+
+		for i := 0; i < (1 << 1); i++ {
+			buffer.NextWriteCmd()
+		}
+		for i := 0; i < (1 << 1); i++ {
+			buffer.NextResultCh()
+			buffer.FinishResult()
+		}
+	})
+}

ring.go
@@ -1,6 +1,7 @@
 package valkey
 
 import (
+	"context"
 	"sync"
 	"sync/atomic"
 
@@ -8,11 +9,12 @@ import (
 )
 
 type queue interface {
-	PutOne(m Completed) chan ValkeyResult
-	PutMulti(m []Completed, resps []ValkeyResult) chan ValkeyResult
+	PutOne(ctx context.Context, m Completed) (chan ValkeyResult, error)
+	PutMulti(ctx context.Context, m []Completed, resps []ValkeyResult) (chan ValkeyResult, error)
 	NextWriteCmd() (Completed, []Completed, chan ValkeyResult)
 	WaitForWrite() (Completed, []Completed, chan ValkeyResult)
-	NextResultCh() (Completed, []Completed, chan ValkeyResult, []ValkeyResult, *sync.Cond)
+	NextResultCh() (Completed, []Completed, chan ValkeyResult, []ValkeyResult)
+	FinishResult()
 }
 
 var _ queue = (*ring)(nil)
@@ -33,6 +35,7 @@ func newRing(factor int) *ring {
 }
 
 type ring struct {
+	resc  *sync.Cond
 	store []node // store's size must be 2^N to work with the mask
 	_     cpu.CacheLinePad
 	write uint32
@@ -53,7 +56,7 @@ type node struct {
 	slept bool
 }
 
-func (r *ring) PutOne(m Completed) chan ValkeyResult {
+func (r *ring) PutOne(_ context.Context, m Completed) (chan ValkeyResult, error) {
 	n := &r.store[atomic.AddUint32(&r.write, 1)&r.mask]
 	n.c1.L.Lock()
 	for n.mark != 0 {
@@ -66,10 +69,10 @@ func (r *ring) PutOne(m Completed) chan ValkeyResult {
 	if s {
 		n.c2.Broadcast()
 	}
-	return n.ch
+	return n.ch, nil
 }
 
-func (r *ring) PutMulti(m []Completed, resps []ValkeyResult) chan ValkeyResult {
+func (r *ring) PutMulti(_ context.Context, m []Completed, resps []ValkeyResult) (chan ValkeyResult, error) {
 	n := &r.store[atomic.AddUint32(&r.write, 1)&r.mask]
 	n.c1.L.Lock()
 	for n.mark != 0 {
@@ -83,7 +86,7 @@ func (r *ring) PutMulti(m []Completed, resps []ValkeyResult) chan ValkeyResult {
 	if s {
 		n.c2.Broadcast()
 	}
-	return n.ch
+	return n.ch, nil
 }
 
 // NextWriteCmd should be only called by one dedicated thread
@@ -120,11 +123,11 @@ func (r *ring) WaitForWrite() (one Completed, multi []Completed, ch chan ValkeyR
 }
 
 // NextResultCh should be only called by one dedicated thread
-func (r *ring) NextResultCh() (one Completed, multi []Completed, ch chan ValkeyResult, resps []ValkeyResult, cond *sync.Cond) {
+func (r *ring) NextResultCh() (one Completed, multi []Completed, ch chan ValkeyResult, resps []ValkeyResult) {
 	r.read2++
 	p := r.read2 & r.mask
 	n := &r.store[p]
-	cond = n.c1
+	r.resc = n.c1
 	n.c1.L.Lock()
 	if n.mark == 2 {
 		one, multi, ch, resps = n.one, n.multi, n.ch, n.resps
@@ -136,4 +139,13 @@ func (r *ring) NextResultCh() (one Completed, multi []Completed, ch chan ValkeyR
 		r.read2--
 	}
 	return
+}
+
+// FinishResult should be only called by one dedicated thread
+func (r *ring) FinishResult() {
+	if r.resc != nil {
+		r.resc.L.Unlock()
+		r.resc.Signal()
+		r.resc = nil
+	}
 }