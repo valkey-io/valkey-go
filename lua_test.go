redis/rueidis

FIPS-compliant SHA-1 loading option for Lua scripts without performance penalty (#914)
This PR introduces a new `WithLoadSha1()` option for `NewLuaScript` and
`NewLuaScriptReadOnly` constructors,
enabling SHA-1 hash loading from Redis instead of client-side
calculation. This provides FIPS compliance
while maintaining EVALSHA performance benefits.

**Problem:**

- Client-side SHA-1 calculation causes panics in FIPS mode
(`fips140=only`)
- Current workaround: `NewLuaScriptNoSha()` always uses EVAL, sending
full script every time. It comes with performance cost: slower than
EVALSHA - ~36% slower in benchmarks here, +47.84% reported in
https://github.com/redis/rueidis/pull/836.

**Proposed solution:**

Allow SHA-1 to be obtained from Redis via `SCRIPT LOAD` response which
[returns calculated
hash](https://redis.io/docs/latest/commands/script-load/#return-information).
This maintains EVALSHA performance without client-side usage of sha1
package. Delegates compliance problems to Redis server only (most likely
approved already).

Benchmark run over local Redis 7.4.0 which show that `WithLoadSha1()`
performs on par with default `NewLuaScript()` while
`NewLuaScriptNoSha()` is significantly slower:

```
name                            time/op
LuaScript_Exec/Default-8        5.59µs ± 1%
LuaScript_Exec/LoadSha1-8       5.60µs ± 1%    (Same as Default, FIPS-compliant)
LuaScript_Exec/NoSha-8          7.61µs ± 1%    (36% slower than LoadSha1)

LuaScript_ExecMulti/Default-8    163µs ± 1%
LuaScript_ExecMulti/LoadSha1-8   163µs ± 2%    (Same as Default, FIPS-compliant)
LuaScript_ExecMulti/NoSha-8      101µs ± 5%

name                            alloc/op
LuaScript_Exec/Default-8          264B ± 0%
LuaScript_Exec/LoadSha1-8         264B ± 0%
LuaScript_Exec/NoSha-8            265B ± 0%

LuaScript_ExecMulti/Default-8   1.91kB ± 0%
LuaScript_ExecMulti/LoadSha1-8  1.92kB ± 0%
LuaScript_ExecMulti/NoSha-8     1.30kB ± 0%

name                            allocs/op
LuaScript_Exec/Default-8    

lua_test.go
@@ -7,6 +7,7 @@ import (
 	"math/rand"
 	"reflect"
 	"strconv"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -404,6 +405,379 @@ func (c *client) Close() {
 	}
 }
 
+func TestNewLuaScriptWithLoadSha1(t *testing.T) {
+	defer ShouldNotLeak(SetupLeakDetection())
+	body := strconv.Itoa(rand.Int())
+	sum := sha1.Sum([]byte(body))
+	sha := hex.EncodeToString(sum[:])
+
+	k := []string{"1", "2"}
+	a := []string{"3", "4"}
+
+	scriptLoadCalled := false
+	evalshaInvoked := false
+
+	c := &client{
+		BFn: func() Builder {
+			return cmds.NewBuilder(cmds.NoSlot)
+		},
+		DoFn: func(ctx context.Context, cmd Completed) (resp RedisResult) {
+			commands := cmd.Commands()
+			if reflect.DeepEqual(commands, []string{"SCRIPT", "LOAD", body}) {
+				scriptLoadCalled = true
+				return newResult(strmsg('+', sha), nil)
+			}
+			if reflect.DeepEqual(commands, []string{"EVALSHA", sha, "2", "1", "2", "3", "4"}) {
+				evalshaInvoked = true
+				return newResult(strmsg('+', "OK"), nil)
+			}
+			if reflect.DeepEqual(commands, []string{"EVAL", body, "2", "1", "2", "3", "4"}) {
+				t.Fatal("EVAL must not be called when load succeeds")
+			}
+			return newResult(strmsg('+', "unexpected"), nil)
+		},
+	}
+
+	script := NewLuaScript(body, WithLoadSha1())
+
+	if v, err := script.Exec(context.Background(), c, k, a).ToString(); err != nil || v != "OK" {
+		t.Fatalf("ret mismatch")
+	}
+
+	if !scriptLoadCalled {
+		t.Fatal("SCRIPT LOAD should have been called")
+	}
+	if !evalshaInvoked {
+		t.Fatal("EVALSHA should have been called")
+	}
+}
+
+func TestNewLuaScriptReadOnlyWithLoadSha1(t *testing.T) {
+	defer ShouldNotLeak(SetupLeakDetection())
+	body := strconv.Itoa(rand.Int())
+	sum := sha1.Sum([]byte(body))
+	sha := hex.EncodeToString(sum[:])
+
+	k := []string{"1", "2"}
+	a := []string{"3", "4"}
+
+	scriptLoadCalled := false
+	evalshaRoInvoked := false
+
+	c := &client{
+		BFn: func() Builder {
+			return cmds.NewBuilder(cmds.NoSlot)
+		},
+		DoFn: func(ctx context.Context, cmd Completed) (resp RedisResult) {
+			commands := cmd.Commands()
+			if reflect.DeepEqual(commands, []string{"SCRIPT", "LOAD", body}) {
+				scriptLoadCalled = true
+				return newResult(strmsg('+', sha), nil)
+			}
+			if reflect.DeepEqual(commands, []string{"EVALSHA_RO", sha, "2", "1", "2", "3", "4"}) {
+				evalshaRoInvoked = true
+				return newResult(strmsg('+', "OK"), nil)
+			}
+			if reflect.DeepEqual(commands, []string{"EVAL_RO", body, "2", "1", "2", "3", "4"}) {
+				t.Fatal("EVAL_RO must not be called when load succeeds")
+			}
+			return newResult(strmsg('+', "unexpected"), nil)
+		},
+	}
+
+	script := NewLuaScriptReadOnly(body, WithLoadSha1())
+
+	if v, err := script.Exec(context.Background(), c, k, a).ToString(); err != nil || v != "OK" {
+		t.Fatalf("ret mismatch")
+	}
+
+	if !scriptLoadCalled {
+		t.Fatal("SCRIPT LOAD should have been called")
+	}
+	if !evalshaRoInvoked {
+		t.Fatal("EVALSHA_RO should have been called")
+	}
+}
+
+func TestNewLuaScriptWithLoadSha1Concurrent(t *testing.T) {
+	defer ShouldNotLeak(SetupLeakDetection())
+	body := strconv.Itoa(rand.Int())
+	sum := sha1.Sum([]byte(body))
+	sha := hex.EncodeToString(sum[:])
+
+	k := []string{"1", "2"}
+	a := []string{"3", "4"}
+
+	var scriptLoadCount atomic.Int64
+
+	c := &client{
+		BFn: func() Builder {
+			return cmds.NewBuilder(cmds.NoSlot)
+		},
+		DoFn: func(ctx context.Context, cmd Completed) (resp RedisResult) {
+			commands := cmd.Commands()
+			if reflect.DeepEqual(commands, []string{"SCRIPT", "LOAD", body}) {
+				scriptLoadCount.Add(1)
+				return newResult(strmsg('+', sha), nil)
+			}
+			if reflect.DeepEqual(commands, []string{"EVALSHA", sha, "2", "1", "2", "3", "4"}) {
+				return newResult(strmsg('+', "OK"), nil)
+			}
+			return newResult(strmsg('+', "unexpected"), nil)
+		},
+	}
+
+	script := NewLuaScript(body, WithLoadSha1())
+
+	// Execute concurrently to verify singleflight works correctly
+	done := make(chan bool)
+	for i := 0; i < 10; i++ {
+		go func() {
+			script.Exec(context.Background(), c, k, a)
+			done <- true
+		}()
+	}
+
+	for i := 0; i < 10; i++ {
+		<-done
+	}
+
+	// SCRIPT LOAD should only be called once due to singleflight
+	if count := scriptLoadCount.Load(); count != 1 {
+		t.Fatalf("SCRIPT LOAD should be called exactly once, but was called %d times", count)
+	}
+}
+
+func TestNewLuaScriptWithLoadSha1ExecMulti(t *testing.T) {
+	defer ShouldNotLeak(SetupLeakDetection())
+	body := strconv.Itoa(rand.Int())
+	sum := sha1.Sum([]byte(body))
+	sha := hex.EncodeToString(sum[:])
+
+	k := []string{"1", "2"}
+	a := []string{"3", "4"}
+
+	scriptLoadCalled := false
+
+	c := &client{
+		BFn: func() Builder {
+			return cmds.NewBuilder(cmds.NoSlot)
+		},
+		DoFn: func(ctx context.Context, cmd Completed) (resp RedisResult) {
+			commands := cmd.Commands()
+			if reflect.DeepEqual(commands, []string{"SCRIPT", "LOAD", body}) {
+				scriptLoadCalled = true
+				return newResult(strmsg('+', sha), nil)
+			}
+			return newResult(strmsg('+', "OK"), nil)
+		},
+		DoMultiFn: func(ctx context.Context, multi ...Completed) (resp []RedisResult) {
+			for _, cmd := range multi {
+				if reflect.DeepEqual(cmd.Commands(), []string{"EVALSHA", sha, "2", "1", "2", "3", "4"}) {
+					resp = append(resp, newResult(strmsg('+', "OK"), nil))
+				} else if reflect.DeepEqual(cmd.Commands(), []string{"EVAL", body, "2", "1", "2", "3", "4"}) {
+					t.Fatal("EVAL should not be called when load succeeds")
+				}
+			}
+			return resp
+		},
+	}
+
+	script := NewLuaScript(body, WithLoadSha1())
+	if v, err := script.ExecMulti(context.Background(), c, LuaExec{Keys: k, Args: a})[0].ToString(); err != nil || v != "OK" {
+		t.Fatalf("ret mismatch")
+	}
+
+	if !scriptLoadCalled {
+		t.Fatal("SCRIPT LOAD should have been called")
+	}
+}
+
+func BenchmarkLuaScript_Exec(b *testing.B) {
+	ctx := context.Background()
+	c, err := NewClient(ClientOption{InitAddress: []string{"127.0.0.1:6379"}})
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer c.Close()
+
+	// Benchmark configuration
+	const (
+		numKeys   = 5
+		keyPrefix = "BenchmarkLuaScript_key"
+	)
+
+	// More complex Lua script with math operations, conversions, and multiple keys
+	script := `
+		local sum = 0
+		local results = {}
+
+		-- Read multiple keys and perform calculations
+		for i = 1, #KEYS do
+			local val = redis.call('GET', KEYS[i])
+			if val then
+				local num = tonumber(val)
+				if num then
+					sum = sum + num
+					table.insert(results, tostring(num * 2))
+				else
+					table.insert(results, val)
+				end
+			end
+		end
+
+		-- Apply some math operations
+		local multiplier = tonumber(ARGV[1]) or 1
+		local threshold = tonumber(ARGV[2]) or 100
+		local final_result = math.floor(sum * multiplier)
+
+		-- Conditional logic
+		if final_result > threshold then
+			table.insert(results, 'high:' .. tostring(final_result))
+		else
+			table.insert(results, 'low:' .. tostring(final_result))
+		end
+
+		return results
+	`
+
+	// Setup: create test keys with numeric values
+	k := make([]string, numKeys)
+	for i := 0; i < numKeys; i++ {
+		key := keyPrefix + strconv.Itoa(i+1)
+		k[i] = key
+		if err := c.Do(ctx, c.B().Set().Key(key).Value(strconv.Itoa((i+1)*10)).Build()).Error(); err != nil {
+			b.Fatal(err)
+		}
+	}
+	a := []string{"1.5", "200"}
+
+	// Table-driven sub-benchmarks
+	cases := []struct {
+		name   string
+		script *Lua
+	}{
+		{"Default", NewLuaScript(script)},
+		{"LoadSha1", NewLuaScript(script, WithLoadSha1())},
+		{"NoSha", NewLuaScriptNoSha(script)},
+	}
+
+	for _, tc := range cases {
+		b.Run(tc.name, func(b *testing.B) {
+			b.SetParallelism(128)
+			b.ResetTimer()
+			b.RunParallel(func(pb *testing.PB) {
+				for pb.Next() {
+					if err := tc.script.Exec(ctx, c, k, a).Error(); err != nil {
+						b.Errorf("unexpected error: %v", err)
+					}
+				}
+			})
+		})
+	}
+}
+
+func BenchmarkLuaScript_ExecMulti(b *testing.B) {
+	ctx := context.Background()
+	c, err := NewClient(ClientOption{InitAddress: []string{"127.0.0.1:6379"}})
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer c.Close()
+
+	// Benchmark configuration
+	const (
+		numExecs    = 5 // Number of executions in the batch
+		keysPerExec = 3 // Number of keys per execution
+		keyPrefix   = "BenchmarkLuaScriptExecMulti_key"
+	)
+	totalKeys := numExecs * keysPerExec
+
+	// More complex Lua script with math operations, conversions, and multiple keys
+	script := `
+		local sum = 0
+		local results = {}
+
+		-- Read multiple keys and perform calculations
+		for i = 1, #KEYS do
+			local val = redis.call('GET', KEYS[i])
+			if val then
+				local num = tonumber(val)
+				if num then
+					sum = sum + num
+					table.insert(results, tostring(num * 2))
+				else
+					table.insert(results, val)
+				end
+			end
+		end
+
+		-- Apply some math operations
+		local multiplier = tonumber(ARGV[1]) or 1
+		local threshold = tonumber(ARGV[2]) or 100
+		local final_result = math.floor(sum * multiplier)
+
+		-- Conditional logic
+		if final_result > threshold then
+			table.insert(results, 'high:' .. tostring(final_result))
+		else
+			table.insert(results, 'low:' .. tostring(final_result))
+		end
+
+		return results
+	`
+
+	// Setup: create test keys with numeric values
+	for i := 1; i <= totalKeys; i++ {
+		key := keyPrefix + strconv.Itoa(i)
+		if err := c.Do(ctx, c.B().Set().Key(key).Value(strconv.Itoa(i*10)).Build()).Error(); err != nil {
+			b.Fatal(err)
+		}
+	}
+
+	// Programmatically build execution batch
+	execs := make([]LuaExec, 0, numExecs)
+	multipliers := []string{"1.2", "2.0", "0.5", "1.8", "3.0"}
+	thresholds := []string{"150", "200", "50", "300", "500"}
+
+	for i := 0; i < numExecs; i++ {
+		keys := make([]string, keysPerExec)
+		for j := 0; j < keysPerExec; j++ {
+			keyNum := i*keysPerExec + j + 1
+			keys[j] = keyPrefix + strconv.Itoa(keyNum)
+		}
+		execs = append(execs, LuaExec{
+			Keys: keys,
+			Args: []string{multipliers[i%len(multipliers)], thresholds[i%len(thresholds)]},
+		})
+	}
+
+	// Table-driven sub-benchmarks
+	cases := []struct {
+		name   string
+		script *Lua
+	}{
+		{"Default", NewLuaScript(script)},
+		{"LoadSha1", NewLuaScript(script, WithLoadSha1())},
+		{"NoSha", NewLuaScriptNoSha(script)},
+	}
+
+	for _, tc := range cases {
+		b.Run(tc.name, func(b *testing.B) {
+			b.SetParallelism(128)
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				results := tc.script.ExecMulti(ctx, c, execs...)
+				for _, r := range results {
+					if err := r.Error(); err != nil {
+						b.Errorf("unexpected error: %v", err)
+					}
+				}
+			}
+		})
+	}
+}
+
 func ExampleLua_exec() {
 	client, err := NewClient(ClientOption{InitAddress: []string{"127.0.0.1:6379"}})
 	if err != nil {
@@ -415,5 +789,5 @@ func ExampleLua_exec() {
 
 	script := NewLuaScript("return {KEYS[1],KEYS[2],ARGV[1],ARGV[2]}")
 
-	script.Exec(ctx, client, []string{"k1", "k2"}, []string{"a1", "a2"}).ToArray()
+	_, _ = script.Exec(ctx, client, []string{"k1", "k2"}, []string{"a1", "a2"}).ToArray()
 }

valkey-io/valkey-go

Signed-off-by: Rueian <rueiancsie@gmail.com>

lua_test.go
@@ -7,6 +7,7 @@ import (
 	"math/rand"
 	"reflect"
 	"strconv"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -404,6 +405,379 @@ func (c *client) Close() {
 	}
 }
 
+func TestNewLuaScriptWithLoadSha1(t *testing.T) {
+	defer ShouldNotLeak(SetupLeakDetection())
+	body := strconv.Itoa(rand.Int())
+	sum := sha1.Sum([]byte(body))
+	sha := hex.EncodeToString(sum[:])
+
+	k := []string{"1", "2"}
+	a := []string{"3", "4"}
+
+	scriptLoadCalled := false
+	evalshaInvoked := false
+
+	c := &client{
+		BFn: func() Builder {
+			return cmds.NewBuilder(cmds.NoSlot)
+		},
+		DoFn: func(ctx context.Context, cmd Completed) (resp ValkeyResult) {
+			commands := cmd.Commands()
+			if reflect.DeepEqual(commands, []string{"SCRIPT", "LOAD", body}) {
+				scriptLoadCalled = true
+				return newResult(strmsg('+', sha), nil)
+			}
+			if reflect.DeepEqual(commands, []string{"EVALSHA", sha, "2", "1", "2", "3", "4"}) {
+				evalshaInvoked = true
+				return newResult(strmsg('+', "OK"), nil)
+			}
+			if reflect.DeepEqual(commands, []string{"EVAL", body, "2", "1", "2", "3", "4"}) {
+				t.Fatal("EVAL must not be called when load succeeds")
+			}
+			return newResult(strmsg('+', "unexpected"), nil)
+		},
+	}
+
+	script := NewLuaScript(body, WithLoadSha1())
+
+	if v, err := script.Exec(context.Background(), c, k, a).ToString(); err != nil || v != "OK" {
+		t.Fatalf("ret mismatch")
+	}
+
+	if !scriptLoadCalled {
+		t.Fatal("SCRIPT LOAD should have been called")
+	}
+	if !evalshaInvoked {
+		t.Fatal("EVALSHA should have been called")
+	}
+}
+
+func TestNewLuaScriptReadOnlyWithLoadSha1(t *testing.T) {
+	defer ShouldNotLeak(SetupLeakDetection())
+	body := strconv.Itoa(rand.Int())
+	sum := sha1.Sum([]byte(body))
+	sha := hex.EncodeToString(sum[:])
+
+	k := []string{"1", "2"}
+	a := []string{"3", "4"}
+
+	scriptLoadCalled := false
+	evalshaRoInvoked := false
+
+	c := &client{
+		BFn: func() Builder {
+			return cmds.NewBuilder(cmds.NoSlot)
+		},
+		DoFn: func(ctx context.Context, cmd Completed) (resp ValkeyResult) {
+			commands := cmd.Commands()
+			if reflect.DeepEqual(commands, []string{"SCRIPT", "LOAD", body}) {
+				scriptLoadCalled = true
+				return newResult(strmsg('+', sha), nil)
+			}
+			if reflect.DeepEqual(commands, []string{"EVALSHA_RO", sha, "2", "1", "2", "3", "4"}) {
+				evalshaRoInvoked = true
+				return newResult(strmsg('+', "OK"), nil)
+			}
+			if reflect.DeepEqual(commands, []string{"EVAL_RO", body, "2", "1", "2", "3", "4"}) {
+				t.Fatal("EVAL_RO must not be called when load succeeds")
+			}
+			return newResult(strmsg('+', "unexpected"), nil)
+		},
+	}
+
+	script := NewLuaScriptReadOnly(body, WithLoadSha1())
+
+	if v, err := script.Exec(context.Background(), c, k, a).ToString(); err != nil || v != "OK" {
+		t.Fatalf("ret mismatch")
+	}
+
+	if !scriptLoadCalled {
+		t.Fatal("SCRIPT LOAD should have been called")
+	}
+	if !evalshaRoInvoked {
+		t.Fatal("EVALSHA_RO should have been called")
+	}
+}
+
+func TestNewLuaScriptWithLoadSha1Concurrent(t *testing.T) {
+	defer ShouldNotLeak(SetupLeakDetection())
+	body := strconv.Itoa(rand.Int())
+	sum := sha1.Sum([]byte(body))
+	sha := hex.EncodeToString(sum[:])
+
+	k := []string{"1", "2"}
+	a := []string{"3", "4"}
+
+	var scriptLoadCount atomic.Int64
+
+	c := &client{
+		BFn: func() Builder {
+			return cmds.NewBuilder(cmds.NoSlot)
+		},
+		DoFn: func(ctx context.Context, cmd Completed) (resp ValkeyResult) {
+			commands := cmd.Commands()
+			if reflect.DeepEqual(commands, []string{"SCRIPT", "LOAD", body}) {
+				scriptLoadCount.Add(1)
+				return newResult(strmsg('+', sha), nil)
+			}
+			if reflect.DeepEqual(commands, []string{"EVALSHA", sha, "2", "1", "2", "3", "4"}) {
+				return newResult(strmsg('+', "OK"), nil)
+			}
+			return newResult(strmsg('+', "unexpected"), nil)
+		},
+	}
+
+	script := NewLuaScript(body, WithLoadSha1())
+
+	// Execute concurrently to verify singleflight works correctly
+	done := make(chan bool)
+	for i := 0; i < 10; i++ {
+		go func() {
+			script.Exec(context.Background(), c, k, a)
+			done <- true
+		}()
+	}
+
+	for i := 0; i < 10; i++ {
+		<-done
+	}
+
+	// SCRIPT LOAD should only be called once due to singleflight
+	if count := scriptLoadCount.Load(); count != 1 {
+		t.Fatalf("SCRIPT LOAD should be called exactly once, but was called %d times", count)
+	}
+}
+
+func TestNewLuaScriptWithLoadSha1ExecMulti(t *testing.T) {
+	defer ShouldNotLeak(SetupLeakDetection())
+	body := strconv.Itoa(rand.Int())
+	sum := sha1.Sum([]byte(body))
+	sha := hex.EncodeToString(sum[:])
+
+	k := []string{"1", "2"}
+	a := []string{"3", "4"}
+
+	scriptLoadCalled := false
+
+	c := &client{
+		BFn: func() Builder {
+			return cmds.NewBuilder(cmds.NoSlot)
+		},
+		DoFn: func(ctx context.Context, cmd Completed) (resp ValkeyResult) {
+			commands := cmd.Commands()
+			if reflect.DeepEqual(commands, []string{"SCRIPT", "LOAD", body}) {
+				scriptLoadCalled = true
+				return newResult(strmsg('+', sha), nil)
+			}
+			return newResult(strmsg('+', "OK"), nil)
+		},
+		DoMultiFn: func(ctx context.Context, multi ...Completed) (resp []ValkeyResult) {
+			for _, cmd := range multi {
+				if reflect.DeepEqual(cmd.Commands(), []string{"EVALSHA", sha, "2", "1", "2", "3", "4"}) {
+					resp = append(resp, newResult(strmsg('+', "OK"), nil))
+				} else if reflect.DeepEqual(cmd.Commands(), []string{"EVAL", body, "2", "1", "2", "3", "4"}) {
+					t.Fatal("EVAL should not be called when load succeeds")
+				}
+			}
+			return resp
+		},
+	}
+
+	script := NewLuaScript(body, WithLoadSha1())
+	if v, err := script.ExecMulti(context.Background(), c, LuaExec{Keys: k, Args: a})[0].ToString(); err != nil || v != "OK" {
+		t.Fatalf("ret mismatch")
+	}
+
+	if !scriptLoadCalled {
+		t.Fatal("SCRIPT LOAD should have been called")
+	}
+}
+
+func BenchmarkLuaScript_Exec(b *testing.B) {
+	ctx := context.Background()
+	c, err := NewClient(ClientOption{InitAddress: []string{"127.0.0.1:6379"}})
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer c.Close()
+
+	// Benchmark configuration
+	const (
+		numKeys   = 5
+		keyPrefix = "BenchmarkLuaScript_key"
+	)
+
+	// More complex Lua script with math operations, conversions, and multiple keys
+	script := `
+		local sum = 0
+		local results = {}
+
+		-- Read multiple keys and perform calculations
+		for i = 1, #KEYS do
+			local val = redis.call('GET', KEYS[i])
+			if val then
+				local num = tonumber(val)
+				if num then
+					sum = sum + num
+					table.insert(results, tostring(num * 2))
+				else
+					table.insert(results, val)
+				end
+			end
+		end
+
+		-- Apply some math operations
+		local multiplier = tonumber(ARGV[1]) or 1
+		local threshold = tonumber(ARGV[2]) or 100
+		local final_result = math.floor(sum * multiplier)
+
+		-- Conditional logic
+		if final_result > threshold then
+			table.insert(results, 'high:' .. tostring(final_result))
+		else
+			table.insert(results, 'low:' .. tostring(final_result))
+		end
+
+		return results
+	`
+
+	// Setup: create test keys with numeric values
+	k := make([]string, numKeys)
+	for i := 0; i < numKeys; i++ {
+		key := keyPrefix + strconv.Itoa(i+1)
+		k[i] = key
+		if err := c.Do(ctx, c.B().Set().Key(key).Value(strconv.Itoa((i+1)*10)).Build()).Error(); err != nil {
+			b.Fatal(err)
+		}
+	}
+	a := []string{"1.5", "200"}
+
+	// Table-driven sub-benchmarks
+	cases := []struct {
+		name   string
+		script *Lua
+	}{
+		{"Default", NewLuaScript(script)},
+		{"LoadSha1", NewLuaScript(script, WithLoadSha1())},
+		{"NoSha", NewLuaScriptNoSha(script)},
+	}
+
+	for _, tc := range cases {
+		b.Run(tc.name, func(b *testing.B) {
+			b.SetParallelism(128)
+			b.ResetTimer()
+			b.RunParallel(func(pb *testing.PB) {
+				for pb.Next() {
+					if err := tc.script.Exec(ctx, c, k, a).Error(); err != nil {
+						b.Errorf("unexpected error: %v", err)
+					}
+				}
+			})
+		})
+	}
+}
+
+func BenchmarkLuaScript_ExecMulti(b *testing.B) {
+	ctx := context.Background()
+	c, err := NewClient(ClientOption{InitAddress: []string{"127.0.0.1:6379"}})
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer c.Close()
+
+	// Benchmark configuration
+	const (
+		numExecs    = 5 // Number of executions in the batch
+		keysPerExec = 3 // Number of keys per execution
+		keyPrefix   = "BenchmarkLuaScriptExecMulti_key"
+	)
+	totalKeys := numExecs * keysPerExec
+
+	// More complex Lua script with math operations, conversions, and multiple keys
+	script := `
+		local sum = 0
+		local results = {}
+
+		-- Read multiple keys and perform calculations
+		for i = 1, #KEYS do
+			local val = redis.call('GET', KEYS[i])
+			if val then
+				local num = tonumber(val)
+				if num then
+					sum = sum + num
+					table.insert(results, tostring(num * 2))
+				else
+					table.insert(results, val)
+				end
+			end
+		end
+
+		-- Apply some math operations
+		local multiplier = tonumber(ARGV[1]) or 1
+		local threshold = tonumber(ARGV[2]) or 100
+		local final_result = math.floor(sum * multiplier)
+
+		-- Conditional logic
+		if final_result > threshold then
+			table.insert(results, 'high:' .. tostring(final_result))
+		else
+			table.insert(results, 'low:' .. tostring(final_result))
+		end
+
+		return results
+	`
+
+	// Setup: create test keys with numeric values
+	for i := 1; i <= totalKeys; i++ {
+		key := keyPrefix + strconv.Itoa(i)
+		if err := c.Do(ctx, c.B().Set().Key(key).Value(strconv.Itoa(i*10)).Build()).Error(); err != nil {
+			b.Fatal(err)
+		}
+	}
+
+	// Programmatically build execution batch
+	execs := make([]LuaExec, 0, numExecs)
+	multipliers := []string{"1.2", "2.0", "0.5", "1.8", "3.0"}
+	thresholds := []string{"150", "200", "50", "300", "500"}
+
+	for i := 0; i < numExecs; i++ {
+		keys := make([]string, keysPerExec)
+		for j := 0; j < keysPerExec; j++ {
+			keyNum := i*keysPerExec + j + 1
+			keys[j] = keyPrefix + strconv.Itoa(keyNum)
+		}
+		execs = append(execs, LuaExec{
+			Keys: keys,
+			Args: []string{multipliers[i%len(multipliers)], thresholds[i%len(thresholds)]},
+		})
+	}
+
+	// Table-driven sub-benchmarks
+	cases := []struct {
+		name   string
+		script *Lua
+	}{
+		{"Default", NewLuaScript(script)},
+		{"LoadSha1", NewLuaScript(script, WithLoadSha1())},
+		{"NoSha", NewLuaScriptNoSha(script)},
+	}
+
+	for _, tc := range cases {
+		b.Run(tc.name, func(b *testing.B) {
+			b.SetParallelism(128)
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				results := tc.script.ExecMulti(ctx, c, execs...)
+				for _, r := range results {
+					if err := r.Error(); err != nil {
+						b.Errorf("unexpected error: %v", err)
+					}
+				}
+			}
+		})
+	}
+}
+
 func ExampleLua_exec() {
 	client, err := NewClient(ClientOption{InitAddress: []string{"127.0.0.1:6379"}})
 	if err != nil {
@@ -415,5 +789,5 @@ func ExampleLua_exec() {
 
 	script := NewLuaScript("return {KEYS[1],KEYS[2],ARGV[1],ARGV[2]}")
 
-	script.Exec(ctx, client, []string{"k1", "k2"}, []string{"a1", "a2"}).ToArray()
+	_, _ = script.Exec(ctx, client, []string{"k1", "k2"}, []string{"a1", "a2"}).ToArray()
 }