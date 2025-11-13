3f49180019f7a27a34d4db81c5f65390292030b8
Swapnil Chadotra <schadotr@asu.edu>
2025-11-12 22:51:58 -0700

    Fixing gen files

--- internal/cmds/gen_connection_test.go
+++ internal/cmds/gen_connection_test.go
@@ -1462,7 +1462,6 @@ func TestCommand_InitSlot_connection(t *testing.T) {
 	t.Run("11", func(t *testing.T) { connection11(s) })
 	t.Run("12", func(t *testing.T) { connection12(s) })
 	t.Run("13", func(t *testing.T) { connection13(s) })
-	t.Run("14", func(t *testing.T) { connection14(s) })
 }
 
 func TestCommand_NoSlot_connection(t *testing.T) {
@@ -1481,5 +1480,4 @@ func TestCommand_NoSlot_connection(t *testing.T) {
 	t.Run("11", func(t *testing.T) { connection11(s) })
 	t.Run("12", func(t *testing.T) { connection12(s) })
 	t.Run("13", func(t *testing.T) { connection13(s) })
-	t.Run("14", func(t *testing.T) { connection14(s) })
 }
