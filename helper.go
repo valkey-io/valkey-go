redis/rueidis

feat : Implement slot-based MGET batching for cluster clients (#908)
For #844

---------

Signed-off-by: Rueian <rueiancsie@gmail.com>
Co-authored-by: Rueian <rueiancsie@gmail.com>

helper.go
@@ -50,12 +50,7 @@ func MGet(client Client, ctx context.Context, keys []string) (ret map[string]Red
 		return clientMGet(client, ctx, client.B().Mget().Key(keys...).Build(), keys)
 	}
 
-	cmds := mgetcmdsp.Get(len(keys), len(keys))
-	defer mgetcmdsp.Put(cmds)
-	for i := range cmds.s {
-		cmds.s[i] = client.B().Get().Key(keys[i]).Build()
-	}
-	return doMultiGet(client, ctx, cmds.s, keys)
+	return clusterMGet(client, ctx, keys)
 }
 
 // MSet is a helper that consults the redis directly with multiple keys by grouping keys within the same slot into MSETs or multiple SETs
@@ -139,12 +134,7 @@ func JsonMGet(client Client, ctx context.Context, keys []string, path string) (r
 		return clientMGet(client, ctx, client.B().JsonMget().Key(keys...).Path(path).Build(), keys)
 	}
 
-	cmds := mgetcmdsp.Get(len(keys), len(keys))
-	defer mgetcmdsp.Put(cmds)
-	for i := range cmds.s {
-		cmds.s[i] = client.B().JsonGet().Key(keys[i]).Path(path).Build()
-	}
-	return doMultiGet(client, ctx, cmds.s, keys)
+	return clusterJsonMGet(client, ctx, keys, path)
 }
 
 // JsonMSet is a helper that consults redis directly with multiple keys by grouping keys within the same slot into JSON.MSETs or multiple JSON.SETs
@@ -245,19 +235,6 @@ func doMultiCache(cc Client, ctx context.Context, cmds []CacheableTTL, keys []st
 	return ret, nil
 }
 
-func doMultiGet(cc Client, ctx context.Context, cmds []Completed, keys []string) (ret map[string]RedisMessage, err error) {
-	ret = make(map[string]RedisMessage, len(keys))
-	resps := cc.DoMulti(ctx, cmds...)
-	defer resultsp.Put(&redisresults{s: resps})
-	for i, resp := range resps {
-		if err := resp.NonRedisError(); err != nil {
-			return nil, err
-		}
-		ret[keys[i]] = resp.val
-	}
-	return ret, nil
-}
-
 func doMultiSet(cc Client, ctx context.Context, cmds []Completed) (ret map[string]error) {
 	ret = make(map[string]error, len(cmds))
 	resps := cc.DoMulti(ctx, cmds...)
@@ -275,6 +252,94 @@ func arrayToKV(m map[string]RedisMessage, arr []RedisMessage, keys []string) map
 		m[keys[i]] = resp
 	}
 	return m
+}
+
+func clusterMGet(client Client, ctx context.Context, keys []string) (ret map[string]RedisMessage, err error) {
+	ret = make(map[string]RedisMessage, len(keys))
+	if len(keys) == 0 {
+		return ret, nil
+	}
+
+	// Map slot -> index in cmds.s
+	hint := len(keys) / 2
+	slotIdx := make(map[uint16]int, hint)
+	cmds := mgetcmdsp.Get(0, hint)
+	defer mgetcmdsp.Put(cmds)
+
+	for _, key := range keys {
+		slot := intl.Slot(key)
+		idx, ok := slotIdx[slot]
+		if !ok {
+			slotIdx[slot] = len(cmds.s)
+			cmds.s = append(cmds.s, client.B().Mget().Key(key).Build().Pin())
+			continue
+		}
+		intl.AppendCompleted(cmds.s[idx], key)
+	}
+
+	resps := client.DoMulti(ctx, cmds.s...)
+	defer resultsp.Put(&redisresults{s: resps})
+
+	for i, resp := range resps {
+		arr, err := resp.ToArray()
+		if err != nil {
+			return nil, err
+		}
+		for j, val := range arr {
+			ret[cmds.s[i].Commands()[j+1]] = val
+		}
+	}
+
+	for i := range cmds.s {
+		intl.PutCompletedForce(cmds.s[i])
+	}
+	return ret, nil
+}
+
+func clusterJsonMGet(client Client, ctx context.Context, keys []string, path string) (ret map[string]RedisMessage, err error) {
+	ret = make(map[string]RedisMessage, len(keys))
+	if len(keys) == 0 {
+		return ret, nil
+	}
+
+	// Map slot -> index in cmds.s
+	hint := len(keys) / 2
+	slotIdx := make(map[uint16]int, hint)
+	cmds := mgetcmdsp.Get(0, hint)
+	defer mgetcmdsp.Put(cmds)
+
+	for _, key := range keys {
+		slot := intl.Slot(key)
+		idx, ok := slotIdx[slot]
+		if !ok {
+			slotIdx[slot] = len(cmds.s)
+			cmds.s = append(cmds.s, client.B().Arbitrary("JSON.MGET").Keys(key).MultiGet().Pin())
+			continue
+		}
+		intl.AppendCompleted(cmds.s[idx], key)
+	}
+
+	for _, c := range cmds.s {
+		intl.AppendCompleted(c, path)
+	}
+
+	resps := client.DoMulti(ctx, cmds.s...)
+	defer resultsp.Put(&redisresults{s: resps})
+
+	for i, resp := range resps {
+		arr, err := resp.ToArray()
+		if err != nil {
+			return nil, err
+		}
+		for j, val := range arr {
+			ret[cmds.s[i].Commands()[j+1]] = val
+		}
+	}
+
+	for i := range cmds.s {
+		intl.PutCompletedForce(cmds.s[i])
+	}
+	return ret, nil
 }
 
 // ErrMSetNXNotSet is used in the MSetNX helper when the underlying MSETNX response is 0.

helper_test.go
@@ -179,18 +179,22 @@ func TestMGetCache(t *testing.T) {
 			t.Fatalf("unexpected err %v", err)
 		}
 		t.Run("Delegate DisabledCache DoCache", func(t *testing.T) {
-			keys := make([]string, 100)
-			for i := range keys {
-				keys[i] = strconv.Itoa(i)
-			}
+			keys := []string{"{slot1}a", "{slot1}b", "{slot2}a", "{slot2}b"}
 			m.DoMultiFn = func(cmd ...Completed) *redisresults {
 				result := make([]RedisResult, len(cmd))
-				for i, key := range keys {
-					if !reflect.DeepEqual(cmd[i].Commands(), []string{"GET", key}) {
-						t.Fatalf("unexpected command %v", cmd)
+				for i, c := range cmd {
+					// Each command should be MGET with keys from the same slot
+					commands := c.Commands()
+					if commands[0] != "MGET" {
+						t.Fatalf("expected MGET command, got %v", commands)
 						return nil
 					}
-					result[i] = newResult(strmsg('+', key), nil)
+					// Build response array with values matching the keys
+					values := make([]RedisMessage, len(commands)-1)
+					for j := 1; j < len(commands); j++ {
+						values[j-1] = strmsg('+', commands[j])
+					}
+					result[i] = newResult(slicemsg('*', values), nil)
 				}
 				return &redisresults{s: result}
 			}
@@ -200,7 +204,7 @@ func TestMGetCache(t *testing.T) {
 			}
 			for _, key := range keys {
 				if vKey, ok := v[key]; !ok || vKey.string() != key {
-					t.Fatalf("unexpected response %v", v)
+					t.Fatalf("unexpected response for key %s: %v", key, v)
 				}
 			}
 		})
@@ -358,18 +362,22 @@ func TestMGet(t *testing.T) {
 			t.Fatalf("unexpected err %v", err)
 		}
 		t.Run("Delegate Do", func(t *testing.T) {
-			keys := make([]string, 100)
-			for i := range keys {
-				keys[i] = strconv.Itoa(i)
-			}
+			keys := []string{"{slot1}a", "{slot1}b", "{slot2}a", "{slot2}b"}
 			m.DoMultiFn = func(cmd ...Completed) *redisresults {
 				result := make([]RedisResult, len(cmd))
-				for i, key := range keys {
-					if !reflect.DeepEqual(cmd[i].Commands(), []string{"GET", key}) {
-						t.Fatalf("unexpected command %v", cmd)
+				for i, c := range cmd {
+					// Each command should be MGET with keys from the same slot
+					commands := c.Commands()
+					if commands[0] != "MGET" {
+						t.Fatalf("expected MGET command, got %v", commands)
 						return nil
 					}
-					result[i] = newResult(strmsg('+', key), nil)
+					// Build response array with values matching the keys
+					values := make([]RedisMessage, len(commands)-1)
+					for j := 1; j < len(commands); j++ {
+						values[j-1] = strmsg('+', commands[j])
+					}
+					result[i] = newResult(slicemsg('*', values), nil)
 				}
 				return &redisresults{s: result}
 			}
@@ -379,7 +387,7 @@ func TestMGet(t *testing.T) {
 			}
 			for _, key := range keys {
 				if vKey, ok := v[key]; !ok || vKey.string() != key {
-					t.Fatalf("unexpected response %v", v)
+					t.Fatalf("unexpected response for key %s: %v", key, v)
 				}
 			}
 		})
@@ -1162,18 +1170,26 @@ func TestJsonMGet(t *testing.T) {
 			t.Fatalf("unexpected err %v", err)
 		}
 		t.Run("Delegate Do", func(t *testing.T) {
-			keys := make([]string, 100)
-			for i := range keys {
-				keys[i] = strconv.Itoa(i)
-			}
+			keys := []string{"{slot1}a", "{slot1}b", "{slot2}a", "{slot2}b"}
 			m.DoMultiFn = func(cmd ...Completed) *redisresults {
 				result := make([]RedisResult, len(cmd))
-				for i, key := range keys {
-					if !reflect.DeepEqual(cmd[i].Commands(), []string{"JSON.GET", key, "$"}) {
-						t.Fatalf("unexpected command %v", cmd)
+				for i, c := range cmd {
+					// Each command should be JSON.MGET with keys from the same slot and path at the end
+					commands := c.Commands()
+					if commands[0] != "JSON.MGET" {
+						t.Fatalf("expected JSON.MGET command, got %v", commands)
 						return nil
 					}
-					result[i] = newResult(strmsg('+', key), nil)
+					if commands[len(commands)-1] != "$" {
+						t.Fatalf("expected $ as last parameter, got %v", commands)
+						return nil
+					}
+					// Build response array with values matching the keys (exclude the path)
+					values := make([]RedisMessage, len(commands)-2)
+					for j := 1; j < len(commands)-1; j++ {
+						values[j-1] = strmsg('+', commands[j])
+					}
+					result[i] = newResult(slicemsg('*', values), nil)
 				}
 				return &redisresults{s: result}
 			}
@@ -1183,7 +1199,7 @@ func TestJsonMGet(t *testing.T) {
 			}
 			for _, key := range keys {
 				if vKey, ok := v[key]; !ok || vKey.string() != key {
-					t.Fatalf("unexpected response %v", v)
+					t.Fatalf("unexpected response for key %s: %v", key, v)
 				}
 			}
 		})

valkey-io/valkey-go

helper.go
@@ -50,12 +50,7 @@ func MGet(client Client, ctx context.Context, keys []string) (ret map[string]Val
 		return clientMGet(client, ctx, client.B().Mget().Key(keys...).Build(), keys)
 	}
 
-	cmds := mgetcmdsp.Get(len(keys), len(keys))
-	defer mgetcmdsp.Put(cmds)
-	for i := range cmds.s {
-		cmds.s[i] = client.B().Get().Key(keys[i]).Build()
-	}
-	return doMultiGet(client, ctx, cmds.s, keys)
+	return clusterMGet(client, ctx, keys)
 }
 
 // MSet is a helper that consults the valkey directly with multiple keys by grouping keys within the same slot into MSETs or multiple SETs
@@ -139,12 +134,7 @@ func JsonMGet(client Client, ctx context.Context, keys []string, path string) (r
 		return clientMGet(client, ctx, client.B().JsonMget().Key(keys...).Path(path).Build(), keys)
 	}
 
-	cmds := mgetcmdsp.Get(len(keys), len(keys))
-	defer mgetcmdsp.Put(cmds)
-	for i := range cmds.s {
-		cmds.s[i] = client.B().JsonGet().Key(keys[i]).Path(path).Build()
-	}
-	return doMultiGet(client, ctx, cmds.s, keys)
+	return clusterJsonMGet(client, ctx, keys, path)
 }
 
 // JsonMSet is a helper that consults valkey directly with multiple keys by grouping keys within the same slot into JSON.MSETs or multiple JSON.SETs
@@ -245,19 +235,6 @@ func doMultiCache(cc Client, ctx context.Context, cmds []CacheableTTL, keys []st
 	return ret, nil
 }
 
-func doMultiGet(cc Client, ctx context.Context, cmds []Completed, keys []string) (ret map[string]ValkeyMessage, err error) {
-	ret = make(map[string]ValkeyMessage, len(keys))
-	resps := cc.DoMulti(ctx, cmds...)
-	defer resultsp.Put(&valkeyresults{s: resps})
-	for i, resp := range resps {
-		if err := resp.NonValkeyError(); err != nil {
-			return nil, err
-		}
-		ret[keys[i]] = resp.val
-	}
-	return ret, nil
-}
-
 func doMultiSet(cc Client, ctx context.Context, cmds []Completed) (ret map[string]error) {
 	ret = make(map[string]error, len(cmds))
 	resps := cc.DoMulti(ctx, cmds...)
@@ -275,6 +252,94 @@ func arrayToKV(m map[string]ValkeyMessage, arr []ValkeyMessage, keys []string) m
 		m[keys[i]] = resp
 	}
 	return m
+}
+
+func clusterMGet(client Client, ctx context.Context, keys []string) (ret map[string]ValkeyMessage, err error) {
+	ret = make(map[string]ValkeyMessage, len(keys))
+	if len(keys) == 0 {
+		return ret, nil
+	}
+
+	// Map slot -> index in cmds.s
+	hint := len(keys) / 2
+	slotIdx := make(map[uint16]int, hint)
+	cmds := mgetcmdsp.Get(0, hint)
+	defer mgetcmdsp.Put(cmds)
+
+	for _, key := range keys {
+		slot := intl.Slot(key)
+		idx, ok := slotIdx[slot]
+		if !ok {
+			slotIdx[slot] = len(cmds.s)
+			cmds.s = append(cmds.s, client.B().Mget().Key(key).Build().Pin())
+			continue
+		}
+		intl.AppendCompleted(cmds.s[idx], key)
+	}
+
+	resps := client.DoMulti(ctx, cmds.s...)
+	defer resultsp.Put(&valkeyresults{s: resps})
+
+	for i, resp := range resps {
+		arr, err := resp.ToArray()
+		if err != nil {
+			return nil, err
+		}
+		for j, val := range arr {
+			ret[cmds.s[i].Commands()[j+1]] = val
+		}
+	}
+
+	for i := range cmds.s {
+		intl.PutCompletedForce(cmds.s[i])
+	}
+	return ret, nil
+}
+
+func clusterJsonMGet(client Client, ctx context.Context, keys []string, path string) (ret map[string]ValkeyMessage, err error) {
+	ret = make(map[string]ValkeyMessage, len(keys))
+	if len(keys) == 0 {
+		return ret, nil
+	}
+
+	// Map slot -> index in cmds.s
+	hint := len(keys) / 2
+	slotIdx := make(map[uint16]int, hint)
+	cmds := mgetcmdsp.Get(0, hint)
+	defer mgetcmdsp.Put(cmds)
+
+	for _, key := range keys {
+		slot := intl.Slot(key)
+		idx, ok := slotIdx[slot]
+		if !ok {
+			slotIdx[slot] = len(cmds.s)
+			cmds.s = append(cmds.s, client.B().Arbitrary("JSON.MGET").Keys(key).MultiGet().Pin())
+			continue
+		}
+		intl.AppendCompleted(cmds.s[idx], key)
+	}
+
+	for _, c := range cmds.s {
+		intl.AppendCompleted(c, path)
+	}
+
+	resps := client.DoMulti(ctx, cmds.s...)
+	defer resultsp.Put(&valkeyresults{s: resps})
+
+	for i, resp := range resps {
+		arr, err := resp.ToArray()
+		if err != nil {
+			return nil, err
+		}
+		for j, val := range arr {
+			ret[cmds.s[i].Commands()[j+1]] = val
+		}
+	}
+
+	for i := range cmds.s {
+		intl.PutCompletedForce(cmds.s[i])
+	}
+	return ret, nil
 }
 
 // ErrMSetNXNotSet is used in the MSetNX helper when the underlying MSETNX response is 0.

helper_test.go
@@ -179,18 +179,22 @@ func TestMGetCache(t *testing.T) {
 			t.Fatalf("unexpected err %v", err)
 		}
 		t.Run("Delegate DisabledCache DoCache", func(t *testing.T) {
-			keys := make([]string, 100)
-			for i := range keys {
-				keys[i] = strconv.Itoa(i)
-			}
+			keys := []string{"{slot1}a", "{slot1}b", "{slot2}a", "{slot2}b"}
 			m.DoMultiFn = func(cmd ...Completed) *valkeyresults {
 				result := make([]ValkeyResult, len(cmd))
-				for i, key := range keys {
-					if !reflect.DeepEqual(cmd[i].Commands(), []string{"GET", key}) {
-						t.Fatalf("unexpected command %v", cmd)
+				for i, c := range cmd {
+					// Each command should be MGET with keys from the same slot
+					commands := c.Commands()
+					if commands[0] != "MGET" {
+						t.Fatalf("expected MGET command, got %v", commands)
 						return nil
 					}
-					result[i] = newResult(strmsg('+', key), nil)
+					// Build response array with values matching the keys
+					values := make([]ValkeyMessage, len(commands)-1)
+					for j := 1; j < len(commands); j++ {
+						values[j-1] = strmsg('+', commands[j])
+					}
+					result[i] = newResult(slicemsg('*', values), nil)
 				}
 				return &valkeyresults{s: result}
 			}
@@ -200,7 +204,7 @@ func TestMGetCache(t *testing.T) {
 			}
 			for _, key := range keys {
 				if vKey, ok := v[key]; !ok || vKey.string() != key {
-					t.Fatalf("unexpected response %v", v)
+					t.Fatalf("unexpected response for key %s: %v", key, v)
 				}
 			}
 		})
@@ -358,18 +362,22 @@ func TestMGet(t *testing.T) {
 			t.Fatalf("unexpected err %v", err)
 		}
 		t.Run("Delegate Do", func(t *testing.T) {
-			keys := make([]string, 100)
-			for i := range keys {
-				keys[i] = strconv.Itoa(i)
-			}
+			keys := []string{"{slot1}a", "{slot1}b", "{slot2}a", "{slot2}b"}
 			m.DoMultiFn = func(cmd ...Completed) *valkeyresults {
 				result := make([]ValkeyResult, len(cmd))
-				for i, key := range keys {
-					if !reflect.DeepEqual(cmd[i].Commands(), []string{"GET", key}) {
-						t.Fatalf("unexpected command %v", cmd)
+				for i, c := range cmd {
+					// Each command should be MGET with keys from the same slot
+					commands := c.Commands()
+					if commands[0] != "MGET" {
+						t.Fatalf("expected MGET command, got %v", commands)
 						return nil
 					}
-					result[i] = newResult(strmsg('+', key), nil)
+					// Build response array with values matching the keys
+					values := make([]ValkeyMessage, len(commands)-1)
+					for j := 1; j < len(commands); j++ {
+						values[j-1] = strmsg('+', commands[j])
+					}
+					result[i] = newResult(slicemsg('*', values), nil)
 				}
 				return &valkeyresults{s: result}
 			}
@@ -379,7 +387,7 @@ func TestMGet(t *testing.T) {
 			}
 			for _, key := range keys {
 				if vKey, ok := v[key]; !ok || vKey.string() != key {
-					t.Fatalf("unexpected response %v", v)
+					t.Fatalf("unexpected response for key %s: %v", key, v)
 				}
 			}
 		})
@@ -1162,18 +1170,26 @@ func TestJsonMGet(t *testing.T) {
 			t.Fatalf("unexpected err %v", err)
 		}
 		t.Run("Delegate Do", func(t *testing.T) {
-			keys := make([]string, 100)
-			for i := range keys {
-				keys[i] = strconv.Itoa(i)
-			}
+			keys := []string{"{slot1}a", "{slot1}b", "{slot2}a", "{slot2}b"}
 			m.DoMultiFn = func(cmd ...Completed) *valkeyresults {
 				result := make([]ValkeyResult, len(cmd))
-				for i, key := range keys {
-					if !reflect.DeepEqual(cmd[i].Commands(), []string{"JSON.GET", key, "$"}) {
-						t.Fatalf("unexpected command %v", cmd)
+				for i, c := range cmd {
+					// Each command should be JSON.MGET with keys from the same slot and path at the end
+					commands := c.Commands()
+					if commands[0] != "JSON.MGET" {
+						t.Fatalf("expected JSON.MGET command, got %v", commands)
 						return nil
 					}
-					result[i] = newResult(strmsg('+', key), nil)
+					if commands[len(commands)-1] != "$" {
+						t.Fatalf("expected $ as last parameter, got %v", commands)
+						return nil
+					}
+					// Build response array with values matching the keys (exclude the path)
+					values := make([]ValkeyMessage, len(commands)-2)
+					for j := 1; j < len(commands)-1; j++ {
+						values[j-1] = strmsg('+', commands[j])
+					}
+					result[i] = newResult(slicemsg('*', values), nil)
 				}
 				return &valkeyresults{s: result}
 			}
@@ -1183,7 +1199,7 @@ func TestJsonMGet(t *testing.T) {
 			}
 			for _, key := range keys {
 				if vKey, ok := v[key]; !ok || vKey.string() != key {
-					t.Fatalf("unexpected response %v", v)
+					t.Fatalf("unexpected response for key %s: %v", key, v)
 				}
 			}
 		})