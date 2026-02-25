redis/rueidis

Add Redis 8.6+ to CI/test setup (#962)
close #961

This PR adds a Redis 8.6+ service to the compose setup so tests that
rely on newer Redis Streams behavior can run against it.

Signed-off-by: jinbum-kim <jinbum9958@gmail.com>

rueidiscompat/adapter_test.go
@@ -57,18 +57,20 @@ func TestAdapter(t *testing.T) {
 }
 
 var (
-	err                error
-	ctx                context.Context
-	clientresp2        rueidis.Client
-	clientsearchresp2  rueidis.Client
-	clusterresp2       rueidis.Client
-	clientresp3        rueidis.Client
-	clusterresp3       rueidis.Client
-	adapterresp2       Cmdable
-	adaptersearchresp2 Cmdable
-	adaptercluster2    Cmdable
-	adapterresp3       Cmdable
-	adaptercluster3    Cmdable
+	err                 error
+	ctx                 context.Context
+	clientresp2         rueidis.Client
+	clientsearchresp2   rueidis.Client
+	clusterresp2        rueidis.Client
+	clientresp3         rueidis.Client
+	clusterresp3        rueidis.Client
+	clientresp3redis86  rueidis.Client
+	adapterresp2        Cmdable
+	adaptersearchresp2  Cmdable
+	adaptercluster2     Cmdable
+	adapterresp3        Cmdable
+	adaptercluster3     Cmdable
+	adapterresp3redis86 Cmdable
 )
 
 var _ = BeforeSuite(func() {
@@ -85,6 +87,12 @@ var _ = BeforeSuite(func() {
 	Expect(err).NotTo(HaveOccurred())
 	adapterresp3 = NewAdapter(clientresp3)
 	adaptercluster3 = NewAdapter(clusterresp3)
+	clientresp3redis86, err = rueidis.NewClient(rueidis.ClientOption{
+		InitAddress: []string{"127.0.0.1:6382"},
+		ClientName:  "rueidis",
+	})
+	Expect(err).NotTo(HaveOccurred())
+	adapterresp3redis86 = NewAdapter(clientresp3redis86)
 	clientresp2, err = rueidis.NewClient(rueidis.ClientOption{
 		InitAddress:  []string{"127.0.0.1:6356"},
 		ClientName:   "rueidis",
@@ -113,6 +121,11 @@ var _ = AfterSuite(func() {
 	Expect(adapterresp3.FlushDB(ctx).Err()).NotTo(HaveOccurred())
 	Expect(adapterresp3.Quit(ctx).Err()).NotTo(HaveOccurred())
 	clientresp3.Close()
+
+	Expect(adapterresp3redis86.FlushDB(ctx).Err()).NotTo(HaveOccurred())
+	Expect(adapterresp3redis86.Quit(ctx).Err()).NotTo(HaveOccurred())
+	clientresp3redis86.Close()
+
 	Expect(adapterresp2.FlushDB(ctx).Err()).NotTo(HaveOccurred())
 	Expect(adapterresp2.Quit(ctx).Err()).NotTo(HaveOccurred())
 	clientresp2.Close()

valkey-io/valkey-go

Signed-off-by: Rueian <rueiancsie@gmail.com>

valkeycompat/adapter_test.go
@@ -57,18 +57,20 @@ func TestAdapter(t *testing.T) {
 }
 
 var (
-	err                error
-	ctx                context.Context
-	clientresp2        valkey.Client
-	clientsearchresp2  valkey.Client
-	clusterresp2       valkey.Client
-	clientresp3        valkey.Client
-	clusterresp3       valkey.Client
-	adapterresp2       Cmdable
-	adaptersearchresp2 Cmdable
-	adaptercluster2    Cmdable
-	adapterresp3       Cmdable
-	adaptercluster3    Cmdable
+	err                 error
+	ctx                 context.Context
+	clientresp2         valkey.Client
+	clientsearchresp2   valkey.Client
+	clusterresp2        valkey.Client
+	clientresp3         valkey.Client
+	clusterresp3        valkey.Client
+	clientresp3redis86  valkey.Client
+	adapterresp2        Cmdable
+	adaptersearchresp2  Cmdable
+	adaptercluster2     Cmdable
+	adapterresp3        Cmdable
+	adaptercluster3     Cmdable
+	adapterresp3redis86 Cmdable
 )
 
 var _ = BeforeSuite(func() {
@@ -85,6 +87,12 @@ var _ = BeforeSuite(func() {
 	Expect(err).NotTo(HaveOccurred())
 	adapterresp3 = NewAdapter(clientresp3)
 	adaptercluster3 = NewAdapter(clusterresp3)
+	clientresp3redis86, err = valkey.NewClient(valkey.ClientOption{
+		InitAddress: []string{"127.0.0.1:6382"},
+		ClientName:  "valkey",
+	})
+	Expect(err).NotTo(HaveOccurred())
+	adapterresp3redis86 = NewAdapter(clientresp3redis86)
 	clientresp2, err = valkey.NewClient(valkey.ClientOption{
 		InitAddress:  []string{"127.0.0.1:6356"},
 		ClientName:   "valkey",
@@ -113,6 +121,11 @@ var _ = AfterSuite(func() {
 	Expect(adapterresp3.FlushDB(ctx).Err()).NotTo(HaveOccurred())
 	Expect(adapterresp3.Quit(ctx).Err()).NotTo(HaveOccurred())
 	clientresp3.Close()
+
+	Expect(adapterresp3redis86.FlushDB(ctx).Err()).NotTo(HaveOccurred())
+	Expect(adapterresp3redis86.Quit(ctx).Err()).NotTo(HaveOccurred())
+	clientresp3redis86.Close()
+
 	Expect(adapterresp2.FlushDB(ctx).Err()).NotTo(HaveOccurred())
 	Expect(adapterresp2.Quit(ctx).Err()).NotTo(HaveOccurred())
 	clientresp2.Close()