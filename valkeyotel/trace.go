b7c8e2e9a5bf4fdef66586df8a14e953df1b7efa
Nelz <nelz@blooket.com>
2026-09-26 10:41:51 -0700

    emit trace attributes for cache hits (#189)
    * emit trace attributes for cache hits
    
    Signed-off-by: Nelz Carpentier <nelz@blooket.com>
    
    * Update valkeyotel/trace.go
    
    Co-authored-by: Rueian <rueiancsie@gmail.com>
    Signed-off-by: Nelz <nelz@blooket.com>
    
    * accumulate hit/miss counts
    
    Signed-off-by: Nelz Carpentier <nelz@blooket.com>
    
    * filter errors from hit/miss telemetry
    
    Signed-off-by: Nelz Carpentier <nelz@blooket.com>
    
    * no hit/miss increment on error
    
    Signed-off-by: Nelz Carpentier <nelz@blooket.com>
    
    ---------
    
    Signed-off-by: Nelz Carpentier <nelz@blooket.com>
    Signed-off-by: Nelz <nelz@blooket.com>
    Co-authored-by: Rueian <rueiancsie@gmail.com>

--- valkeyotel/trace.go
+++ valkeyotel/trace.go
@@ -15,10 +15,12 @@ import (
 )
 
 var (
-	name   = "github.com/valkey-io/valkey-go"
-	kind   = trace.WithSpanKind(trace.SpanKindClient)
-	dbattr = attribute.String("db.system", "valkey")
-	dbstmt = attribute.Key("db.statement")
+	name      = "github.com/valkey-io/valkey-go"
+	kind      = trace.WithSpanKind(trace.SpanKindClient)
+	dbattr    = attribute.String("db.system", "valkey")
+	dbstmt    = attribute.Key("db.statement")
+	cacheHit  = attribute.Key("db.cache.hit")
+	cacheMiss = attribute.Key("db.cache.miss")
 )
 
 type contextKey struct{}
@@ -310,6 +312,15 @@ func (o *otelclient) DoCache(ctx context.Context, cmd valkey.Cacheable, ttl time
 
 	resp = o.client.DoCache(ctx, cmd, ttl)
 	o.recordCacheHitMiss(ctx, resp)
+	hitCount, missCount := 0, 0
+	if resp.NonValkeyError() == nil {
+		if resp.IsCacheHit() {
+			hitCount++
+		} else {
+			missCount++
+		}
+	}
+	span.SetAttributes(cacheHit.Int(hitCount), cacheMiss.Int(missCount))
 	o.end(span, resp.Error())
 	o.recordError(ctx, op, resp.Error())
 	return
@@ -321,9 +332,19 @@ func (o *otelclient) DoMultiCache(ctx context.Context, multi ...valkey.Cacheable
 
 	ctx, span := o.start(ctx, op, multiCacheableSum(multi))
 	resps = o.client.DoMultiCache(ctx, multi...)
+
+	hitCount, missCount := 0, 0
 	for _, resp := range resps {
 		o.recordCacheHitMiss(ctx, resp)
+		if resp.NonValkeyError() == nil {
+			if resp.IsCacheHit() {
+				hitCount++
+			} else {
+				missCount++
+			}
+		}
 	}
+	span.SetAttributes(cacheHit.Int(hitCount), cacheMiss.Int(missCount))
 	err := firstError(resps)
 	o.end(span, err)
 	o.recordError(ctx, op, err)
